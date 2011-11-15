98eb0bcb8ad0f0d4f532f5182853b3b636fa6adf
akau_mogi <akau_mogi@0dc4ddf3-c69b-4c57-96b0-f340c5324e41>
2011-11-15 00:35:51 +0000

    refs #2226 車載器アプリ
    git-svn-id: http://10.0.0.2/repos/ondemand/trunk@59 0dc4ddf3-c69b-4c57-96b0-f340c5324e41

---
+++ src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceActivity.java
@@ -0,0 +1,13 @@
+package com.kogasoftware.odt.invehicledevice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InVehicleDeviceActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
