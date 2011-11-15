8654de390ea83dbef4cdf868f06045baa0a27621
akau_mogi <akau_mogi@0dc4ddf3-c69b-4c57-96b0-f340c5324e41>
2011-11-15 01:30:38 +0000

    refs #2226 HTMLモックをAndroidで表示できるようにした
    git-svn-id: http://10.0.0.2/repos/ondemand/trunk@64 0dc4ddf3-c69b-4c57-96b0-f340c5324e41

--- src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceActivity.java
+++ src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceActivity.java
@@ -2,12 +2,23 @@ package com.kogasoftware.odt.invehicledevice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class InVehicleDeviceActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		WebView webView = (WebView) findViewById(R.id.webView);
+		webView.setWebViewClient(new WebViewClient());
+		webView.setWebChromeClient(new WebChromeClient());
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webView.loadUrl("file:///android_asset/default.html");
+	}
 }
