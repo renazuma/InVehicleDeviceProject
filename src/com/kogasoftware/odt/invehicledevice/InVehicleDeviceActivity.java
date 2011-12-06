bbf225d8499c6b9a6f4935a3cee1d0c676209fa2
akau_mogi <akau_mogi@0dc4ddf3-c69b-4c57-96b0-f340c5324e41>
2011-12-06 00:44:30 +0000

    refs #2163 車載器がしゃべる機能のプロトタイプ
    git-svn-id: http://10.0.0.2/repos/ondemand/trunk@165 0dc4ddf3-c69b-4c57-96b0-f340c5324e41

--- src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceActivity.java
+++ src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceActivity.java
@@ -1,11 +1,55 @@
 package com.kogasoftware.odt.invehicledevice;
 
+import java.util.Locale;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+class SpeakAlertThread extends Thread {
+	private final TextToSpeech tts;
+
+	public SpeakAlertThread(Context context) {
+		tts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+			@Override
+			public void onInit(int status) {
+				if (status != TextToSpeech.SUCCESS) {
+					return;
+				}
+				Locale locale = Locale.JAPAN;
+				if (tts.isLanguageAvailable(locale) < TextToSpeech.LANG_AVAILABLE) {
+					return;
+				}
+				tts.setLanguage(locale);
+			}
+		});
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				Thread.sleep(5000);
+				if (tts.isSpeaking()) {
+					// 読み上げ中なら止める
+					tts.stop();
+				}
+				// 読み上げ開始
+				// tts.speak("こんにちは", TextToSpeech.QUEUE_FLUSH, null);
+				tts.speak("Hello Android Launch", TextToSpeech.QUEUE_FLUSH, null);
+				
+			}
+		} catch (InterruptedException e) {
+		} finally {
+			tts.shutdown();
+		}
+	}
+}
 
 public class InVehicleDeviceActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -20,5 +64,7 @@ public class InVehicleDeviceActivity extends Activity {
 		WebSettings webSettings = webView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 		webView.loadUrl("file:///android_asset/default.html");
+
+		new SpeakAlertThread(this).start();
 	}
 }
