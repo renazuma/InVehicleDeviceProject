e90c4af0b16e5ff24f2cfc9745f2239c42e539d4
akau_mogi <akau_mogi@0dc4ddf3-c69b-4c57-96b0-f340c5324e41>
2011-12-09 04:27:52 +0000

    refs #2510 遷移作成。文言と音声はまだ
    git-svn-id: http://10.0.0.2/repos/ondemand/trunk@199 0dc4ddf3-c69b-4c57-96b0-f340c5324e41

--- assets/default.js
+++ assets/default.js
@@ -1,217 +1,238 @@
-      var TRANSITION_DURATION = 200;
-      var STATUS_DRIVING = 0;
-      var STATUS_STATION = 1;
-      var STATUS_PAUSE = 2;
-      var STATUS_STOP = 3;
-      var status = STATUS_DRIVING;
-      var page = 0;
-      var radio = 0;
-      
-      $(window).resize(function() {
-          // スマフォっぽい比率で固定
-          $("body").css("font-size", $("body").width() / 7 + "%");
-      });
-      $(document).ready(function() {
-          $(window).resize();
-          start();
-          setInterval(function() {
-              $("#drive_screen_image").attr("src", "d" + page + ".png");
-              $("#radio_image").attr("src", "radio" + radio + ".png");
-              page = (page + 1) % 4;
-              radio = (radio + 1) % 4;
-          }, 5000);
+var TRANSITION_DURATION = 200;
+var STATUS_DRIVING = 0;
+var STATUS_STATION = 1;
+var STATUS_PAUSE = 2;
+var STATUS_STOP = 3;
+var status = STATUS_DRIVING;
+var page = 0;
+var radio = 0;
 
-          $("#inbound_reserve_list td").click(function(e) {
-              $("#inbound_reserve_list td").not(this).parent().css("background-color", "transparent");
-              $(this).parent().css("background-color", "lightblue");
-              $("#inbound_reserve_button").css({"color": "black", "background-color": "tomato"});
-          });
+$(window).resize(function() {
+    // スマフォっぽい比率で固定
+    $("body").css("font-size", $("body").width() / 7 + "%");
+});
+$(document).ready(function() {
+    $(window).resize();
+    start();
+    setInterval(function() {
+        $("#drive_screen_image").attr("src", "d" + page + ".png");
+        $("#radio_image").attr("src", "radio" + radio + ".png");
+        page = (page + 1) % 4;
+        radio = (radio + 1) % 4;
+    }, 5000);
 
-          $(".station-cell").click(function(e) {
-              $(this).toggleClass("station-cell-selected");
-              if ($(".station-cell").not(".station-cell-selected").length > 0) {
-                  return;
-              }
-              showCheckStartOverlay();
-          });
-      });
+    $("#inbound_reserve_list td").click(function(e) {
+        $("#inbound_reserve_list td").not(this).parent().css("background-color", "transparent");
+        $(this).parent().css("background-color", "lightblue");
+        $("#inbound_reserve_button").css({"color": "black", "background-color": "tomato"});
+    });
 
-      function showDefaultScreen() {
-          var selector = "";
-          if (status == STATUS_DRIVING) {
-              selector = "#drive_screen";
-          } else if (status == STATUS_STATION) {
-              selector = "#station_screen";
-          }
-          $(".screen").not(selector).hide(TRANSITION_DURATION);
-          $(selector).show(TRANSITION_DURATION);
-          $(".button_layout").css("background-color", "transparent");
-      }
-      
-      function toggleScreen(targetId) {
-          var result = true;
-          var showId = targetId;
-          if ($("#" + targetId).filter(":hidden").length == 0) {
-              if (status == STATUS_DRIVING) {
-                  showId = "drive_screen";
-              } else if (status == STATUS_STATION) {
-                  showId = "station_screen";
-              }
-              result = false;
-          }
-          $(".screen").not("#" + showId).hide(TRANSITION_DURATION);
-          $("#" + showId).show(TRANSITION_DURATION);
-          return result;
-      }
+    $(".station-cell").click(function(e) {
+        $(this).toggleClass("station-cell-selected");
+        if ($(".station-cell").not(".station-cell-selected").length > 0) {
+            return;
+        }
+        showCheckStartOverlay();
+    });
+});
 
-      function toggleMapScreen() {
-          var selector = "#map_button_layout";
-          $(".button_layout").not(selector).css("background-color", "transparent");
-          var bg = "";
-          if (toggleScreen("map_screen")) {
-              if (status == STATUS_DRIVING) {
-                  bg = "gsp.png";
-              } else if (status == STATUS_STATION) {
-                  bg = "bsp.png";
-              }
-          } else {
-              if (status == STATUS_DRIVING) {
-                  bg = "gs.png";
-              } else if (status == STATUS_STATION) {
-                  bg = "bs.png";
-              }
-          }
-          $("#map_button_bg").attr("src", bg);
+function showDefaultScreen() {
+    var selector = "";
+    if (status == STATUS_DRIVING) {
+        selector = "#drive_screen";
+    } else if (status == STATUS_STATION) {
+        selector = "#station_screen";
+    }
+    $(".screen").not(selector).hide(TRANSITION_DURATION);
+    $(selector).show(TRANSITION_DURATION);
+    $(".button_layout").css("background-color", "transparent");
+}
 
-          if (status == STATUS_DRIVING) {
-              $("#schedule_button_bg").attr("src", "gs.png");
-          } else if (status == STATUS_STATION) {
-              $("#schedule_button_bg").attr("src", "bs.png");
-          }
-      }
+function toggleScreen(targetId) {
+    var result = true;
+    var showId = targetId;
+    if ($("#" + targetId).filter(":hidden").length == 0) {
+        if (status == STATUS_DRIVING) {
+            showId = "drive_screen";
+        } else if (status == STATUS_STATION) {
+            showId = "station_screen";
+        }
+        result = false;
+    }
+    $(".screen").not("#" + showId).hide(TRANSITION_DURATION);
+    $("#" + showId).show(TRANSITION_DURATION);
+    return result;
+}
 
-      function toggleScheduleScreen() {
-          var selector = "#schedule_button_layout";
-          $(".button_layout").not(selector).css("background-color", "transparent");
-          var bg = "";
-          if (toggleScreen("schedule_screen")) {
-              if (status == STATUS_DRIVING) {
-                  bg = "gsp.png";
-              } else if (status == STATUS_STATION) {
-                  bg = "bsp.png";
-              }
-          } else {
-              if (status == STATUS_DRIVING) {
-                  bg = "gs.png";
-              } else if (status == STATUS_STATION) {
-                  bg = "bs.png";
-              }
-          }
-          $("#schedule_button_bg").attr("src", bg);
+function showScheduleScreen() {
+    if ($("#schedule_screen").filter(":hidden").length > 0) {
+        toggleScheduleScreen();
+    }
+}
 
-          if (status == STATUS_DRIVING) {
-              $("#map_button_bg").attr("src", "gs.png");
-          } else if (status == STATUS_STATION) {
-              $("#map_button_bg").attr("src", "bs.png");
-          }
-      }
+function toggleMapScreen() {
+    var selector = "#map_button_layout";
+    $(".button_layout").not(selector).css("background-color", "transparent");
+    var bg = "";
+    if (toggleScreen("map_screen")) {
+        if (status == STATUS_DRIVING) {
+            bg = "gsp.png";
+        } else if (status == STATUS_STATION) {
+            bg = "bsp.png";
+        }
+    } else {
+        if (status == STATUS_DRIVING) {
+            bg = "gs.png";
+        } else if (status == STATUS_STATION) {
+            bg = "bs.png";
+        }
+    }
+    $("#map_button_bg").attr("src", bg);
 
-      function showCheckStartOverlay() {
-          $("#check_start_overlay").show(TRANSITION_DURATION);
-      }
+    if (status == STATUS_DRIVING) {
+        $("#schedule_button_bg").attr("src", "gs.png");
+    } else if (status == STATUS_STATION) {
+        $("#schedule_button_bg").attr("src", "bs.png");
+    }
+}
 
-      function showCheckStopOverlay() {
-          $("#check_stop_overlay").show(TRANSITION_DURATION);
-      }
+function toggleScheduleScreen() {
+    var selector = "#schedule_button_layout";
+    $(".button_layout").not(selector).css("background-color", "transparent");
+    var bg = "";
+    if (toggleScreen("schedule_screen")) {
+        if (status == STATUS_DRIVING) {
+            bg = "gsp.png";
+        } else if (status == STATUS_STATION) {
+            bg = "bsp.png";
+        }
+    } else {
+        if (status == STATUS_DRIVING) {
+            bg = "gs.png";
+        } else if (status == STATUS_STATION) {
+            bg = "bs.png";
+        }
+    }
+    $("#schedule_button_bg").attr("src", bg);
 
-      function showMemoOverlay() {
-          $("#memo_overlay").show(TRANSITION_DURATION);
-      }
+    if (status == STATUS_DRIVING) {
+        $("#map_button_bg").attr("src", "gs.png");
+    } else if (status == STATUS_STATION) {
+        $("#map_button_bg").attr("src", "bs.png");
+    }
+}
 
-      function showOperatorMessageOverlay() {
-          clearMessageNotify();
-          $("#operator_message_overlay").show(TRANSITION_DURATION);
-      }
+function showCheckStartOverlay() {
+    $("#check_start_overlay").show(TRANSITION_DURATION);
+}
 
-      function showStopOverlay() {
-          $("#stop_overlay").show(TRANSITION_DURATION);
-      }
+function showCheckStopOverlay() {
+    $("#check_stop_overlay").show(TRANSITION_DURATION);
+}
 
-      function showPauseOverlay() {
-          $("#pause_overlay").show(TRANSITION_DURATION);
-      }
+function showMemoOverlay() {
+    $("#memo_overlay").show(TRANSITION_DURATION);
+}
 
-      function showAdminOverlay() {
-          $("#admin_overlay").show(TRANSITION_DURATION);
-      }
+function showOperatorMessageOverlay() {
+    clearMessageNotify();
+    $("#operator_message_overlay").show(TRANSITION_DURATION);
+}
 
-      function showInboundOverlay() {
-          $("#inbound_overlay").show(TRANSITION_DURATION);
-      }
+function showStopOverlay() {
+    $("#stop_overlay").show(TRANSITION_DURATION);
+}
 
-      function start() {
-          closeOverlay();
-          status = STATUS_DRIVING;
-          $(".frame").css("background-color", "lightgreen");
-          $("#admin_button_bg").attr("src", "gs.png");
-          $("#map_button_bg").attr("src", "gs.png");
-          $("#schedule_button_bg").attr("src", "gb.png");
-          $("#start_button_bg").attr("src", "gb.png");
-          $("#goal_button_bg").attr("src", "gb.png");
+function showPauseOverlay() {
+    $("#pause_overlay").show(TRANSITION_DURATION);
+}
 
-          $("#start_button_layout").hide();
-          $("#goal_button_layout").show();
-          $("#status_text").text("走行中");
-          showDefaultScreen();
-          $(".station-cell").removeClass("station-cell-selected");
-      }
+function showAdminOverlay() {
+    $("#admin_overlay").show(TRANSITION_DURATION);
+}
 
-      function goal() {
-          status = STATUS_STATION;
-          $(".frame").css("background-color", "lightblue");
-          $("#admin_button_bg").attr("src", "bs.png");
-          $("#map_button_bg").attr("src", "bs.png");
-          $("#schedule_button_bg").attr("src", "bb.png");
-          $("#start_button_bg").attr("src", "bb.png");
-          $("#goal_button_bg").attr("src", "bb.png");
+function showInboundOverlay() {
+    $("#inbound_overlay").show(TRANSITION_DURATION);
+}
 
-          $("#start_button_layout").show();
-          $("#goal_button_layout").hide();
-          $("#status_text").text("停車中");
-          showDefaultScreen();
-      }
+function start() {
+    closeOverlay();
+    status = STATUS_DRIVING;
+    $(".frame").css("background-color", "lightgreen");
+    $("#admin_button_bg").attr("src", "gs.png");
+    $("#map_button_bg").attr("src", "gs.png");
+    $("#schedule_button_bg").attr("src", "gb.png");
+    $("#start_button_bg").attr("src", "gb.png");
+    $("#goal_button_bg").attr("src", "gb.png");
 
-      function closeOverlay() {
-          $(".overlay").hide(TRANSITION_DURATION);
-      }
+    $("#start_button_layout").hide();
+    $("#goal_button_layout").show();
+    $("#status_text").text("走行中");
+    showDefaultScreen();
+    $(".station-cell").removeClass("station-cell-selected");
+}
 
-      function up() {
-          $("div.scroll").animate({scrollTop: $("div.scroll").scrollTop() - 300}, 'fast');
-      }
+function goal() {
+    status = STATUS_STATION;
+    $(".frame").css("background-color", "lightblue");
+    $("#admin_button_bg").attr("src", "bs.png");
+    $("#map_button_bg").attr("src", "bs.png");
+    $("#schedule_button_bg").attr("src", "bb.png");
+    $("#start_button_bg").attr("src", "bb.png");
+    $("#goal_button_bg").attr("src", "bb.png");
 
-      function down() {
-          $("div.scroll").animate({scrollTop: $("div.scroll").scrollTop() + 300}, 'fast');
-      }
+    $("#start_button_layout").show();
+    $("#goal_button_layout").hide();
+    $("#status_text").text("停車中");
+    showDefaultScreen();
+}
 
-      var messageNotifyTimerId = 0;
-      var messageNotifyBlink = 0;
-      
-      function startMessageNotify() {
-          clearMessageNotify();
-          var messageNotifyTimerId = 0;
-          messageNotifyTimerId = setInterval(function() {
-              $("#message_notify").toggle();
-              messageNotifyBlink++;
-              if (messageNotifyBlink < 6) {
-                  return;
-              }
-              $("#message_notify").hide();
-              clearInterval(messageNotifyTimerId);
-              messageNotifyBlink = 0;
-              $("#operator_message_overlay").show(TRANSITION_DURATION);
-          }, 350);
-      }
+function closeOverlay() {
+    $(".overlay").hide(TRANSITION_DURATION);
+}
 
-      function clearMessageNotify() {
-      }
+function up() {
+    $("div.scroll").animate({scrollTop: $("div.scroll").scrollTop() - 300}, 'fast');
+}
+
+function down() {
+    $("div.scroll").animate({scrollTop: $("div.scroll").scrollTop() + 300}, 'fast');
+}
+
+var notifyBlink = 0;
+
+function startMessageNotify() {
+    clearMessageNotify();
+    var messageNotifyTimerId = 0;
+    messageNotifyTimerId = setInterval(function() {
+        $("#message_notify").toggle();
+        notifyBlink++;
+        if (notifyBlink < 6) {
+            return;
+        }
+        $("#message_notify").hide();
+        clearInterval(messageNotifyTimerId);
+        notifyBlink = 0;
+        $("#operator_message_overlay").show(TRANSITION_DURATION);
+    }, 350);
+}
+
+function startScheduleChangedNotify() {
+    clearMessageNotify();
+    var messageNotifyTimerId = 0;
+    messageNotifyTimerId = setInterval(function() {
+        $("#message_notify").toggle();
+        notifyBlink++;
+        if (notifyBlink < 6) {
+            return;
+        }
+        $("#message_notify").hide();
+        clearInterval(messageNotifyTimerId);
+        notifyBlink = 0;
+        $("#schedule_changed_overlay").show(TRANSITION_DURATION);
+    }, 350);
+}
+
+function clearMessageNotify() {
+}
 
