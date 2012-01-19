7faa66685b6f645410d6128b3b53aa942f10e0c0
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-01-19 11:51:16 +0900

    refs #2815 webview削除

--- src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceActivity.java
+++ src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceActivity.java
@@ -4,65 +4,25 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Toast;
-
-class WebViewSpeaker {
-	private final BlockingQueue<String> texts;
-
-	public WebViewSpeaker(BlockingQueue<String> texts) {
-		this.texts = texts;
-	}
-
-	public void speak(String text) {
-		texts.add(text);
-	}
-}
-
-class WebViewMapLauncher {
-	private final Context context;
-
-	public WebViewMapLauncher(Context context) {
-		this.context = context;
-	}
-
-	public void launch() {
-	}
-}
 
 public class InVehicleDeviceActivity extends Activity {
 	Thread speakAlertThread = new Thread();
 	private final BlockingQueue<String> texts = new LinkedBlockingQueue<String>();
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.invehicledevice);
-
-		WebView webView = (WebView) findViewById(R.id.webView);
-		webView.setWebViewClient(new WebViewClient());
-		webView.setWebChromeClient(new WebChromeClient());
-		WebSettings webSettings = webView.getSettings();
-		webSettings.setJavaScriptEnabled(true);
-		webView.addJavascriptInterface(new WebViewSpeaker(texts), "speaker");
-		webView.addJavascriptInterface(new WebViewMapLauncher(this),
-				"mapLauncher");
-		webView.loadUrl("file:///android_asset/default.html");
-
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		speakAlertThread.interrupt();
 	}
 
 	@Override
