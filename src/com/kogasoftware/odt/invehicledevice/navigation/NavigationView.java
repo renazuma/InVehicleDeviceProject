c9d23ea19ecfeef1dc1d360f8b2f6bf2f143def3
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-02-02 12:37:37 +0900

    segv解決

--- src/com/kogasoftware/odt/invehicledevice/navigation/NavigationView.java
+++ src/com/kogasoftware/odt/invehicledevice/navigation/NavigationView.java
@@ -6,7 +6,6 @@ import javax.microedition.khronos.opengles.GL10;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -51,10 +50,14 @@ public class NavigationView extends FrameLayout {
 	private static final String T = LogTag.get(NavigationView.class);
 	// private final MapViewRedirector mapViewRedirector;
 	private final MapView mapView;
-	private final LocationManager locationManager;
-	private final MyLocationListener listener;
+	// private final LocationManager locationManager;
+	// private final MyLocationListener listener;
+	private final GLSurfaceView glSurfaceView;
+
+	final private MapSynchronizer mapSynchronizer;
 
 	// private final MapRenderer mapRenderer;
+
 	// private final OrientationSensor orientationSensor;
 	// private final MapOnTouchListener mapOnTouchListener;
 
@@ -62,26 +65,20 @@ public class NavigationView extends FrameLayout {
 		return mapView;
 	}
 
-	final private GLSurfaceView glSurfaceView;
-	final private MapSynchronizer mapSynchronizer;
-
 	public NavigationView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
 		mapSynchronizer = new MapSynchronizer();
 
-		locationManager = (LocationManager) context
-				.getSystemService(Context.LOCATION_SERVICE);
+		// locationManager = (LocationManager) context
+		// .getSystemService(Context.LOCATION_SERVICE);
 		mapView = new MapView(context,
 				"0_ZIi_adDM8WHxCX0OJTfcXhHO8jOsYOjLF7xow");
 		// mapOnTouchListener = new MapOnTouchListener(mapView);
-		glSurfaceView = new GLSurfaceView(context.getApplicationContext());
 		// mapViewRedirector = new MapViewRedirector(context, null);
 		// mapView.setOnTouchListener(mapOnTouchListener);
 		MapRenderer mapRenderer = new MapRenderer(
 				context.getApplicationContext(), mapSynchronizer, mapView);
-		// glSurfaceView.setRenderer(mapRenderer);
-		glSurfaceView.setRenderer(new MyRenderer());
 
 		// mapRenderer = new MapRenderer(context.getApplicationContext(),
 		// bitmapSynchronizer, mapView);
@@ -106,31 +103,35 @@ public class NavigationView extends FrameLayout {
 		// };
 		// orientationSensor.create();
 		//
-		listener = new MyLocationListener();
+		// listener = new MyLocationListener();
 
-		// addView(glSurfaceView, new FrameLayout.LayoutParams(
+		// addView(mapViewRedirector, new FrameLayout.LayoutParams(512, 512));
+		// addView(mapViewRedirector, new FrameLayout.LayoutParams(512, 512));
+		// addView(glSurfaceView, new NavigationView.LayoutParams(
 		// FrameLayout.LayoutParams.FILL_PARENT,
 		// FrameLayout.LayoutParams.FILL_PARENT));
-		// addView(mapViewRedirector, new FrameLayout.LayoutParams(512, 512));
-		// addView(mapViewRedirector, new FrameLayout.LayoutParams(512, 512));
-		addView(glSurfaceView, new FrameLayout.LayoutParams(
-				FrameLayout.LayoutParams.FILL_PARENT,
-				FrameLayout.LayoutParams.FILL_PARENT));
+
+		// glSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
+		glSurfaceView = new GLSurfaceView(context.getApplicationContext());
+		// glSurfaceView.setRenderer(new MyRenderer());
+		glSurfaceView.setRenderer(mapRenderer);
 	}
 
 	@Override
 	protected void onAttachedToWindow() {
 		super.onAttachedToWindow();
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				2000, 0, listener);
-
+		// locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+		// 2000, 0, listener);
+		addView(glSurfaceView, new NavigationView.LayoutParams(
+				NavigationView.LayoutParams.FILL_PARENT,
+				NavigationView.LayoutParams.FILL_PARENT));
 	}
 
 	@Override
 	protected void onDetachedFromWindow() {
-		locationManager.removeUpdates(listener);
+		// locationManager.removeUpdates(listener);
 		// removeView(mapViewRedirector);
-		// removeView(glSurfaceView);
+		removeView(glSurfaceView);
 		// orientationSensor.destroy();
 		// removeAllViews();
 		// this.removeView(mapViewRedirector);
@@ -138,12 +139,12 @@ public class NavigationView extends FrameLayout {
 	}
 
 	public void onResumeActivity() {
-		mapSynchronizer.onResume();
+		// mapSynchronizer.onResume();
 		glSurfaceView.onResume();
 	}
 
 	public void onPauseActivity() {
 		glSurfaceView.onPause();
-		mapSynchronizer.onPause();
+		// mapSynchronizer.onPause();
 	}
 }
