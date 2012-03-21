8f97466c78a317d6f4c30099a9f596921a33f05d
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-03-21 17:34:23 +0900

    通知画面をダミーのモデルを利用して表示

--- src/com/kogasoftware/odt/invehicledevice/modal/NotificationModal.java
+++ src/com/kogasoftware/odt/invehicledevice/modal/NotificationModal.java
@@ -1,14 +1,23 @@
 package com.kogasoftware.odt.invehicledevice.modal;
 
 import com.kogasoftware.odt.invehicledevice.R;
+import com.kogasoftware.odt.webapi.model.VehicleNotification;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.TextView;
 
 
 public class NotificationModal extends Modal {
 	public NotificationModal(Context context, AttributeSet attrs) {
 		super(context, attrs, R.layout.notification_modal);
 
+		VehicleNotification vehicleNotification = new VehicleNotification();
+		vehicleNotification.setBody("鈴木さんは乗車されましたか？");
+		// 
+		TextView bodyTextView = (TextView)findViewById(R.id.vehicle_notification_body);
+		if (vehicleNotification.getBody().isPresent()) {
+			bodyTextView.setText(vehicleNotification.getBody().get());
+		}
 	}
 }
