95d69ddfc5ad989fb0ff27c3201f3f552573a49c
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-03-28 15:25:59 +0900

    make android lint happy

--- src/jp/tomorrowkey/android/vtextviewer/VTextView.java
+++ src/jp/tomorrowkey/android/vtextviewer/VTextView.java
@@ -10,30 +10,22 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class VTextView extends View {
-
 	private static final int TOP_SPACE = 0;
-
 	private static final int BOTTOM_SPACE = 18;
-
 	private static final int FONT_SIZE = 60;
-
-	private Typeface mFace;
-
-	private Paint mPaint;
-
+	private Typeface typeFace = Typeface.defaultFromStyle(Typeface.NORMAL);
+	private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 	private String text = "";
-
-	private int width;
-
-	private int height;
+	private int width = 0;
+	private int height = 0;
+	private Bitmap bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+	private Canvas canvas = new Canvas(bitmap);
 
 	public VTextView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		mFace = Typeface.defaultFromStyle(Typeface.NORMAL);
-		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mPaint.setTextSize(FONT_SIZE);
-		mPaint.setColor(Color.BLACK);
-		mPaint.setTypeface(mFace);
+		paint.setTextSize(FONT_SIZE);
+		paint.setColor(Color.BLACK);
+		paint.setTypeface(typeFace);
 	}
 
 	public void setText(String text) {
@@ -47,20 +39,20 @@ public class VTextView extends View {
 
 		width = getWidth();
 		height = getHeight();
+
+		bitmap.recycle();
+		bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		canvas = new Canvas(bitmap);
 	}
 
 	@Override
 	public void onDraw(Canvas targetCanvas) {
-		float fontSpacing = mPaint.getFontSpacing() * 0.8f;
+		float fontSpacing = paint.getFontSpacing() * 0.8f;
 		float lineSpacing = fontSpacing;
 		float x = width - lineSpacing;
 		float beginX = x;
 		float y = TOP_SPACE + fontSpacing;
 		String[] s = text.split("");
-
-		Bitmap bitmap = Bitmap.createBitmap(width, height,
-				Bitmap.Config.ARGB_8888);
-		Canvas canvas = new Canvas(bitmap);
 
 		boolean newLine = false;
 		for (int i = 1; i < s.length; i++) {
@@ -73,13 +65,13 @@ public class VTextView extends View {
 			CharSetting setting = CharSetting.getSetting(s[i]);
 			if (setting == null) {
 				// 文字設定がない場合、そのまま描画
-				canvas.drawText(s[i], x, y, mPaint);
+				canvas.drawText(s[i], x, y, paint);
 			} else {
 				// 文字設定が見つかったので、設定に従い描画
 				canvas.save();
 				canvas.rotate(setting.angle, x, y);
 				canvas.drawText(s[i], x + fontSpacing * setting.x, y
-						+ fontSpacing * setting.y, mPaint);
+						+ fontSpacing * setting.y, paint);
 				canvas.restore();
 			}
 
@@ -95,7 +87,6 @@ public class VTextView extends View {
 
 		// 中央揃え
 		float x2 = -(width - beginX + x - fontSpacing) / 2;
-		targetCanvas.drawBitmap(bitmap, x2, 0, mPaint);
-		bitmap.recycle();
+		targetCanvas.drawBitmap(bitmap, x2, 0, paint);
 	}
 }
