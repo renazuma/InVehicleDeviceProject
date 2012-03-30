0b234eeaae423eb16da7539cf0acfa86a0a67f80
Hisako Uchida <hisako_uchida@kogasoftware.com>
2012-03-30 17:51:52 +0900

    車載器テストコード　復路予約画面テストコード表示

--- tests/src/com/kogasoftware/odt/invehicledevice/test/ReturnPathTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/ReturnPathTestCase.java
@@ -16,13 +16,14 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 
 	private Solo solo;
 
-	public void dataset(Integer i) {
+	public void dataset(Integer iCount,Integer userId,
+			Integer departurePlatformId, Integer arrivalPlatformId) {
 
 		DataSourceFactory.newInstance();
 		MockDataSourceTest mdst = new MockDataSourceTest();
 
 		mdst.setReservation(6);
-		mdst.setReservationCandidate(i,1,1,1);
+		mdst.setReservationCandidate(iCount,userId,departurePlatformId,arrivalPlatformId);
 
 		DataSourceFactory.setInstance(mdst);
 
@@ -45,7 +46,8 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 	}
 
 	public void test00_データ初期設定() {
-		dataset(6);
+		// TODO userId,departurePlatformId,arrivalPlatformId部分は後で茂木さんの実装が出来たら実装する
+		dataset(6,1,1,1);
 	}
 
 	public void test01_復路画面で戻るボタンを押すと復路画面が非表示() {
@@ -61,36 +63,48 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 
 	}
 
-	public void test03_予約候補表示後で予約選択前は予約ボタン使用不可() {
+	public void test03_復路画面で予約候補を検索を押すと予約候補表示() {
+
+		assertFalse(solo.searchText("乗車時刻"));
 
 		solo.clickOnButton("予約候補を検索");
+		assertEquals(View.VISIBLE, solo.getView(R.id.return_path_modal)
+				.getVisibility());
+
+		assertTrue(solo.searchText("乗車時刻"));
+		assertTrue(solo.searchText("13時"));
+		assertTrue(solo.searchText("34分"));
+
+	}
+
+	public void test04_予約候補表示後で予約選択前は予約ボタン使用不可() {
+
+		test03_復路画面で予約候補を検索を押すと予約候補表示();
+
+		Button reserveButton = (Button) getActivity().findViewById(R.id.return_path_button);
+
+		assertFalse(reserveButton.isEnabled());
+
+	}
+
+	public void test05_復路画面で予約候補をタッチすると予約ボタンが使用可能になる() {
+
+		test03_復路画面で予約候補を検索を押すと予約候補表示();
+
+		solo.clickOnText("15時");
 
 		Button reserveButton = (Button) getActivity().findViewById(R.id.return_path_button);
 
 		assertTrue(reserveButton.isEnabled());
 
-	}
-
-	public void test02_復路画面で予約候補を検索を押すと予約候補表示() {
-
-		assertFalse(solo.searchText("乗車時刻"));
-
-		assertEquals(View.VISIBLE, solo.getView(R.id.return_path_modal)
-				.getVisibility());
-
-		solo.clickOnButton("予約候補を検索");
-
-		assertTrue(solo.searchText("乗車時刻"));
-
-		assertEquals(View.VISIBLE, solo.getView(R.id.return_path_button)
-				.getVisibility());
 
 	}
 
+	public void test06_復路画面で予約を押すと待機中画面へ戻る() {
 
-	public void test03_復路画面で予約を押すと待機中画面へ戻る() {
+		test03_復路画面で予約候補を検索を押すと予約候補表示();
 
-		test02_復路画面で予約候補を検索を押すと予約候補表示();
+		solo.clickOnText("15時");
 
 		solo.clickOnButton("予約する");
 		assertEquals(View.VISIBLE, solo.getView(R.id.waiting_layout)
@@ -98,33 +112,7 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 
 	}
 
-	public void test04_下スクロール() {
-
-		test02_復路画面で予約候補を検索を押すと予約候補表示();
-
-		assertTrue(solo.searchText("予約名字a", 0,false));
-
-		solo.clickOnButton("下へ移動");
-
-		assertFalse(solo.searchText("名字a", 0,false));
-
-	}
-
-	public void test05_上スクロール() {
-
-		test04_下スクロール();
-
-		solo.clickOnButton("上へ移動");
-
-		assertTrue(solo.searchText("名字a", 0,false));
-
-	}
-
-
-	/*
-	public void test16復路画面の予約条件時間変更() {
-
-		test03復路ボタンを押すと復路画面が表示();
+	public void test07_復路画面の予約条件時間変更() {
 
 		solo.clickOnText("9");
 
@@ -134,9 +122,7 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 
 	}
 
-	public void test17復路画面の予約条件分変更() {
-
-		test03復路ボタンを押すと復路画面が表示();
+	public void test08_復路画面の予約条件分変更() {
 
 		solo.clickOnText("0",2);
 
@@ -146,9 +132,7 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 
 	}
 
-	public void test18復路画面の予約条件乗車変更() {
-
-		test03復路ボタンを押すと復路画面が表示();
+	public void test09_復路画面の予約条件乗車変更() {
 
 		solo.clickOnText("乗車");
 
@@ -158,22 +142,44 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 
 	}
 
-	public void test19データ初期設定1件() {
-		dataset(1);
+	public void test10_下スクロール() {
+
+		// TODO
+		assertTrue(solo.searchText("13時", 0,false));
+
+		solo.clickOnButton("下へ移動");
+
+		assertFalse(solo.searchText("13時", 0,false));
+
 	}
 
-	public void test20件数が少ないため上へ移動ボタンが存在しない() {
+	public void test11_上スクロール() {
+
+		test10_下スクロール();
+
+		solo.clickOnButton("上へ移動");
+
+		assertTrue(solo.searchText("13時", 0,false));
+
+	}
+
+	public void test12_データ初期設定1件() {
+
+		// TODO userId,departurePlatformId,arrivalPlatformId部分は後で茂木さんの実装が出来たら実装する
+		dataset(1,1,1,1);
+	}
+
+	public void test13_件数が少ないため上へ移動ボタンが存在しない() {
 
 		assertFalse(solo.searchButton("上へ移動"));
 
 	}
 
-	public void test21件数が少ないため下へ移動ボタンが存在しない() {
+	public void test14_件数が少ないため下へ移動ボタンが存在しない() {
 
 		assertFalse(solo.searchButton("下へ移動"));
 
 	}
-*/
 
 	@Override
 	public void tearDown() throws Exception {
