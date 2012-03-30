160db3cc8c4f8ca563a91aeac7c4d7eac640275d
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-03-30 16:23:02 +0900

    予約画面に待機UI追加

--- src/com/kogasoftware/odt/invehicledevice/modal/ReturnPathModal.java
+++ src/com/kogasoftware/odt/invehicledevice/modal/ReturnPathModal.java
@@ -3,7 +3,10 @@ package com.kogasoftware.odt.invehicledevice.modal;
 import java.util.LinkedList;
 import java.util.List;
 
-import android.util.Log;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,15 +23,60 @@ import com.kogasoftware.odt.webapi.model.Reservation;
 import com.kogasoftware.odt.webapi.model.ReservationCandidate;
 import com.kogasoftware.odt.webapi.model.User;
 
+class MyAsyncTask extends AsyncTask<Void, Void, String> {
+	final String TAG = MyAsyncTask.class.getSimpleName();
+
+	@Override
+	protected String doInBackground(Void... args) {
+		return "";
+	}
+}
+
 public class ReturnPathModal extends Modal {
 	private static final String TAG = ReturnPathModal.class.getSimpleName();
 	private final InVehicleDeviceActivity inVehicleDeviceActivity;
+	private ProgressDialog searchingDialog;
+	private ProgressDialog sendingDialog;
 	private Reservation currentReservation = new Reservation();
 
 	public ReturnPathModal(InVehicleDeviceActivity inVehicleDeviceActivity) {
 		super(inVehicleDeviceActivity, R.layout.return_path_modal);
 		setId(R.id.return_path_modal);
 		this.inVehicleDeviceActivity = inVehicleDeviceActivity;
+
+		searchingDialog = new ProgressDialog(getContext());
+		searchingDialog.setMessage("予約情報を受信しています");
+		searchingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		searchingDialog.setOnDismissListener(new OnDismissListener() {
+			@Override
+			public void onDismiss(DialogInterface dialog) {
+			}
+		});
+
+		sendingDialog = new ProgressDialog(getContext());
+		sendingDialog.setMessage("予約情報を送信しています");
+		sendingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		sendingDialog.setOnDismissListener(new OnDismissListener() {
+			@Override
+			public void onDismiss(DialogInterface dialog) {
+			}
+		});
+	}
+
+	@Override
+	protected void onAttachedToWindow() {
+		super.onAttachedToWindow();
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+		if (searchingDialog.isShowing()) {
+			searchingDialog.cancel();
+		}
+		if (sendingDialog.isShowing()) {
+			sendingDialog.dismiss();
+		}
 	}
 
 	public void show(Reservation reservation) {
@@ -65,17 +113,42 @@ public class ReturnPathModal extends Modal {
 				getContext(), android.R.layout.simple_spinner_item, inOrOut);
 		inOrOutSpinner.setAdapter(inOrOutAdapter);
 
+		final ListView reservationCandidateListView = (ListView) findViewById(R.id.reservation_candidates_list_view);
+		ReservationCandidateArrayAdapter adapter = new ReservationCandidateArrayAdapter(
+				inVehicleDeviceActivity,
+				R.layout.reservation_candidate_list_row, new LinkedList<ReservationCandidate>());
+		reservationCandidateListView.setAdapter(adapter);
+
 		final Button doReservationButton = (Button) findViewById(R.id.do_reservation_button);
+		doReservationButton.setVisibility(View.INVISIBLE);
 		doReservationButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				try {
-					inVehicleDeviceActivity.getDataSource().postReservation(0);
-				} catch (WebAPIException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					Log.e(TAG, e.toString(), e);
-				}
+				sendingDialog.show();
+				AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+					@Override
+					protected Void doInBackground(Void... params) {
+						try {
+							inVehicleDeviceActivity.getDataSource()
+							.postReservation(0);
+							return null;
+						} catch (WebAPIException e) {
+							e.printStackTrace();
+						}
+						cancel(true);
+						return null;
+					}
+
+					@Override
+					protected void onPostExecute(Void result) {
+						sendingDialog.dismiss();
+						if (this.isCancelled()) {
+							return;
+						}
+						hide();
+					}
+				};
+				task.execute();
 			}
 		});
 
@@ -83,22 +156,36 @@ public class ReturnPathModal extends Modal {
 		searchReturnPathButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				List<ReservationCandidate> l = new LinkedList<ReservationCandidate>();
-				try {
-					l = inVehicleDeviceActivity.getDataSource()
-							.postReservationCandidates(0, 0, 0);
-				} catch (WebAPIException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					Log.e(TAG, e.toString(), e);
-				}
+				searchingDialog.show();
+				AsyncTask<Void, Void, List<ReservationCandidate>> task = new AsyncTask<Void, Void, List<ReservationCandidate>>() {
+					@Override
+					protected List<ReservationCandidate> doInBackground(
+							Void... params) {
+						try {
+							return inVehicleDeviceActivity.getDataSource()
+									.postReservationCandidates(0, 0, 0);
+						} catch (WebAPIException e) {
+							e.printStackTrace();
+						}
+						cancel(true);
+						return new LinkedList<ReservationCandidate>();
+					}
 
-				ListView reservationCandidateListView = (ListView) findViewById(R.id.reservation_candidates_list_view);
-				ReservationCandidateArrayAdapter adapter = new ReservationCandidateArrayAdapter(
-						inVehicleDeviceActivity,
-						R.layout.reservation_candidate_list_row, l);
-				reservationCandidateListView.setAdapter(adapter);
-				doReservationButton.setVisibility(View.VISIBLE);
+					@Override
+					protected void onPostExecute(
+							List<ReservationCandidate> result) {
+						searchingDialog.dismiss();
+						if (this.isCancelled()) {
+							return;
+						}
+						ReservationCandidateArrayAdapter adapter = new ReservationCandidateArrayAdapter(
+								inVehicleDeviceActivity,
+								R.layout.reservation_candidate_list_row, result);
+						reservationCandidateListView.setAdapter(adapter);
+						doReservationButton.setVisibility(View.VISIBLE);
+					}
+				};
+				task.execute();
 			}
 		});
 
