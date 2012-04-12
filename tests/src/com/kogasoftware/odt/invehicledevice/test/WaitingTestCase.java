458d315f6db7956d13cbaa394432645aaac646fa
Hisako Uchida <hisako_uchida@kogasoftware.com>
2012-04-12 14:35:30 +0900

    refs #3933 車載器テストコード　茂木さん最新プロダクトコード取得の為一旦コミット

--- tests/src/com/kogasoftware/odt/invehicledevice/test/WaitingTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/WaitingTestCase.java
@@ -38,15 +38,16 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 		solo = new Solo(getInstrumentation(), getActivity());
 
 		// デフォルトで停車中にする
-		solo.clickOnButton("到着しました");
-
+		if (solo.searchButton("到着しました")) {
+			solo.clickOnButton("到着しました");
+		}
 	}
 
 	public void test00_データ初期設定() {
 		dataset(6);
 	}
 
-	public void test01_メモボタンを押すとメモ画面が表示() {
+/*	public void test01_メモボタンを押すとメモ画面が表示() {
 		assertEquals(View.GONE, solo.getView(R.id.memo_modal).getVisibility());
 		solo.clickOnView(solo.getView(R.id.memo_button));
 		assertEquals(View.VISIBLE, solo.getView(R.id.memo_modal)
@@ -183,10 +184,10 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 		assertFalse(solo.searchButton("下へ移動"));
 
 	}
-
+*/
 	public void test18_乗客の新規追加ボタンが存在する() {
 
-		assertTrue(solo.searchButton("予約の新規追加"));
+		assertTrue(solo.searchText("予約の新規追加"));
 
 	}
 
@@ -195,6 +196,15 @@ ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 		assertTrue(solo.searchText("乗車中の乗客全員を表示"));
 		assertTrue(solo.searchText("未来の乗客を表示"));
 		assertTrue(solo.searchText("過去の乗降場で未乗車の乗客を表示"));
+
+	}
+
+	public void test20_乗車中の乗客全員を表示オンオフ() {
+
+		assertTrue(solo.searchText("乗車中の乗客全員を表示"));
+
+		solo.clickOnText("乗車中の乗客全員を表示");
+
 
 	}
 
