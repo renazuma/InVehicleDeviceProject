f3388207d96d65f05c5dca15a11299a9a62d5f4b
HIDAKA Takahiro <takahr@gmail.com>
2012-04-12 19:31:57 +0900

    refs #3964 VehicleNotifications を取得・更新するテスト用API実装

--- tests/src/com/kogasoftware/odt/webapi/test/WebTestAPI.java
+++ tests/src/com/kogasoftware/odt/webapi/test/WebTestAPI.java
@@ -1,12 +1,18 @@
 package com.kogasoftware.odt.webapi.test;
 
+import java.util.List;
+
+import org.json.JSONException;
+
 import com.kogasoftware.odt.webapi.WebAPI;
 import com.kogasoftware.odt.webapi.WebAPIException;
+import com.kogasoftware.odt.webapi.model.VehicleNotification;
 
 public class WebTestAPI extends WebAPI {
 
 	protected static final String TEST_SERVER_HOST = "http://192.168.104.63:3333";
-	protected static final String PATH_CLEAN = "/clean";
+	protected static final String TEST_PATH_CLEAN = "/clean";
+	protected static final String TEST_PATH_NOTIFICATIONS = "/vehicle_notifications";
 
 	@Override
 	protected String getServerHost() {
@@ -20,7 +26,7 @@ public class WebTestAPI extends WebAPI {
 	 * @throws WebAPIException
 	 */
 	public int cleanDatabase(WebAPICallback<Void> callback) throws WebAPIException {
-		return post(PATH_CLEAN, null, callback, new ResponseConverter<Void>() {
+		return post(TEST_PATH_CLEAN, null, callback, new ResponseConverter<Void>() {
 			@Override
 			public Void convert(byte[] rawResponse)
 					throws Exception {
@@ -28,5 +34,70 @@ public class WebTestAPI extends WebAPI {
 			}
 		});
 	}	
-	
+
+	/**
+	 * 車載器への通知をすべて取得
+	 * @param callback
+	 * @return reqkey
+	 * @throws WebAPIException
+	 */
+	public int getAllVehicleNotifications(WebAPICallback<List<VehicleNotification>> callback) throws WebAPIException {
+		return get(TEST_PATH_NOTIFICATIONS, callback, new ResponseConverter<List<VehicleNotification>>() {
+			@Override
+			public List<VehicleNotification> convert(byte[] rawResponse)
+					throws Exception {
+				return VehicleNotification.parseList(parseJSONArray(rawResponse));
+			}
+		});
+	}
+
+	/**
+	 * 車載器への通知をひとつ取得
+	 * @param callback
+	 * @return reqkey
+	 * @throws WebAPIException
+	 */
+	public int getVehicleNotification(int id, WebAPICallback<VehicleNotification> callback) throws WebAPIException {
+		return get(TEST_PATH_NOTIFICATIONS + "/" + id, callback, new ResponseConverter<VehicleNotification>() {
+			@Override
+			public VehicleNotification convert(byte[] rawResponse)
+					throws Exception {
+				return VehicleNotification.parse(parseJSONObject(rawResponse)).orNull();
+			}
+		});
+	}
+
+	/**
+	 * 車載器への通知を生成
+	 * @param callback
+	 * @return reqkey
+	 * @throws WebAPIException
+	 * @throws JSONException 
+	 */
+	public int createVehicleNotification(VehicleNotification obj, WebAPICallback<VehicleNotification> callback) throws WebAPIException, JSONException {
+		return post(TEST_PATH_NOTIFICATIONS, obj.toJSONObject(), callback, new ResponseConverter<VehicleNotification>() {
+			@Override
+			public VehicleNotification convert(byte[] rawResponse)
+					throws Exception {
+				return VehicleNotification.parse(parseJSONObject(rawResponse)).orNull();
+			}
+		});
+	}
+
+	/**
+	 * 車載器への通知をひとつ削除
+	 * @param callback
+	 * @return reqkey
+	 * @throws WebAPIException
+	 */
+	public int deleteVehicleNotification(int id, WebAPICallback<Void> callback) throws WebAPIException {
+		return delete(TEST_PATH_NOTIFICATIONS + "/" + id, callback, new ResponseConverter<Void>() {
+			@Override
+			public Void convert(byte[] rawResponse)
+					throws Exception {
+				return null;
+			}
+		});
+	}
+
 }
--- tests/src/com/kogasoftware/odt/webapi/test/WebTestAPITestCase.java
+++ tests/src/com/kogasoftware/odt/webapi/test/WebTestAPITestCase.java
@@ -1,9 +1,11 @@
 package com.kogasoftware.odt.webapi.test;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import com.kogasoftware.odt.webapi.WebAPI.WebAPICallback;
 import com.kogasoftware.odt.webapi.WebAPIException;
+import com.kogasoftware.odt.webapi.model.VehicleNotification;
 
 import android.test.ActivityInstrumentationTestCase2;
 
@@ -13,14 +15,22 @@ public class WebTestAPITestCase extends
 	public WebTestAPITestCase() {
 		super("com.kogasoftware.odt.webapi.test", DummyActivity.class);
 	}
-	
-	public void testTruncateVehicleNotifications() throws Exception {
-		WebTestAPI api = new WebTestAPI();
-		final CountDownLatch latch = new CountDownLatch(1);
 
+	CountDownLatch latch;
+	boolean succeed = false;
+	int resultCode;
+	List<VehicleNotification> vehicleNotifications;
+	VehicleNotification vehicleNotification;
+	
+	public void testCleanDatabase() throws Exception {
+		WebTestAPI api = new WebTestAPI();
+		latch = new CountDownLatch(1);
+		succeed = false;
+		
 		api.cleanDatabase(new WebAPICallback<Void>() {
 			@Override
 			public void onSucceed(int reqkey, int statusCode, Void result) {
+				succeed = true;
 				latch.countDown();
 			}
 			
@@ -36,6 +46,182 @@ public class WebTestAPITestCase extends
 		});
 
 		latch.await();
+		
+		assertTrue(succeed);
+	}
+	
+	public void testVehicleNotifications() throws Exception {
+		WebTestAPI api = new WebTestAPI();
+
+		// オブジェクトをひとつ生成
+		VehicleNotification obj = new VehicleNotification();
+		obj.setInVehicleDeviceId(1);
+		obj.setOperatorId(20);
+		obj.setBody("車載器への通知1です");
+
+		latch = new CountDownLatch(1);
+		succeed = false;
+		resultCode = -1;
+		api.createVehicleNotification(obj, new WebAPICallback<VehicleNotification>() {
+
+			@Override
+			public void onSucceed(int reqkey, int statusCode,
+					VehicleNotification result) {
+				succeed = true;
+				resultCode = statusCode;
+				latch.countDown();
+			}
+
+			@Override
+			public void onFailed(int reqkey, int statusCode) {
+				resultCode = statusCode;
+				latch.countDown();
+			}
+
+			@Override
+			public void onException(int reqkey, WebAPIException ex) {
+				latch.countDown();
+			}
+		});
+		
+		latch.await();
+		assertTrue(succeed);
+		
+		// オブジェクトをひとつ生成
+		obj = new VehicleNotification();
+		obj.setInVehicleDeviceId(1);
+		obj.setOperatorId(20);
+		obj.setBody("車載器への通知2です");
+		
+		latch = new CountDownLatch(1);
+		succeed = false;
+		api.createVehicleNotification(obj, new WebAPICallback<VehicleNotification>() {
+
+			@Override
+			public void onSucceed(int reqkey, int statusCode,
+					VehicleNotification result) {
+				succeed = true;
+				latch.countDown();
+			}
+
+			@Override
+			public void onFailed(int reqkey, int statusCode) {
+				latch.countDown();
+			}
+
+			@Override
+			public void onException(int reqkey, WebAPIException ex) {
+				latch.countDown();
+			}
+		});
+		
+		latch.await();
+		assertTrue(succeed);
+
+		// オブジェクト数を確認
+		latch = new CountDownLatch(1);
+		succeed = false;
+		api.getAllVehicleNotifications(new WebAPICallback<List<VehicleNotification>>() {
+			
+			@Override
+			public void onSucceed(int reqkey, int statusCode,
+					List<VehicleNotification> result) {
+				vehicleNotifications = result;
+				latch.countDown();
+			}
+			
+			@Override
+			public void onFailed(int reqkey, int statusCode) {
+				latch.countDown();
+			}
+			
+			@Override
+			public void onException(int reqkey, WebAPIException ex) {
+				latch.countDown();
+			}
+		});
+
+		latch.await();
+		assertEquals(2, vehicleNotifications.size());
+		
+		// 2つめのオブジェクト取得
+		latch = new CountDownLatch(1);
+		succeed = false;
+		api.getVehicleNotification(vehicleNotifications.get(1).getId(), new WebAPICallback<VehicleNotification>() {
+
+			@Override
+			public void onSucceed(int reqkey, int statusCode,
+					VehicleNotification result) {
+				vehicleNotification = result;
+				latch.countDown();
+			}
+
+			@Override
+			public void onFailed(int reqkey, int statusCode) {
+				latch.countDown();
+			}
+
+			@Override
+			public void onException(int reqkey, WebAPIException ex) {
+				latch.countDown();
+			}
+			
+		});
+		
+		latch.await();
+		assertEquals("車載器への通知2です", vehicleNotification.getBody().orNull());
+		
+		// オブジェクトをひとつ削除
+		latch = new CountDownLatch(1);
+		succeed = false;
+		api.deleteVehicleNotification(vehicleNotifications.get(1).getId(), new WebAPICallback<Void>() {
+
+			@Override
+			public void onSucceed(int reqkey, int statusCode, Void result) {
+				succeed = true;
+				latch.countDown();
+			}
+
+			@Override
+			public void onFailed(int reqkey, int statusCode) {
+				latch.countDown();
+			}
+
+			@Override
+			public void onException(int reqkey, WebAPIException ex) {
+				latch.countDown();
+			}
+			
+		});
+		
+		latch.await();
+		assertTrue(succeed);
+		
+		// オブジェクト数を確認
+		latch = new CountDownLatch(1);
+		succeed = false;
+		api.getAllVehicleNotifications(new WebAPICallback<List<VehicleNotification>>() {
+			
+			@Override
+			public void onSucceed(int reqkey, int statusCode,
+					List<VehicleNotification> result) {
+				vehicleNotifications = result;
+				latch.countDown();
+			}
+			
+			@Override
+			public void onFailed(int reqkey, int statusCode) {
+				latch.countDown();
+			}
+			
+			@Override
+			public void onException(int reqkey, WebAPIException ex) {
+				latch.countDown();
+			}
+		});
+
+		latch.await();
+		assertEquals(1, vehicleNotifications.size());
 	}
 
 }
