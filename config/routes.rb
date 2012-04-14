ff8a255be686d480d3553f1240c5767d6758a950
HIDAKA Takahiro <takahr@gmail.com>
2012-04-14 18:21:04 +0900

    refs #3964 オペレータ操作API実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ WebAPITest::Application.routes.draw do
   post 'clean' => 'db#clean'
   resources :vehicle_notifications
   resources :in_vehicle_devices
+  resources :operators
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
