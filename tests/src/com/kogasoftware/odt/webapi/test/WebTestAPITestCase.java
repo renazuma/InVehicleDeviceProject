3daf08d28277b27036ed11dfb655af086b3e9919
HIDAKA Takahiro <takahr@gmail.com>
2012-04-14 15:09:49 +0900

    refs #3964 テストケースを SyncCall を使うように修正

--- tests/src/com/kogasoftware/odt/webapi/test/WebTestAPITestCase.java
+++ tests/src/com/kogasoftware/odt/webapi/test/WebTestAPITestCase.java
@@ -8,6 +8,7 @@ import com.kogasoftware.odt.webapi.WebAPIException;
 import com.kogasoftware.odt.webapi.model.VehicleNotification;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
 public class WebTestAPITestCase extends
 		ActivityInstrumentationTestCase2<DummyActivity> {
@@ -16,16 +17,10 @@ public class WebTestAPITestCase extends
 		super("com.kogasoftware.odt.webapi.test", DummyActivity.class);
 	}
 
-	CountDownLatch latch;
 	boolean succeed = false;
-	int resultCode;
-	List<VehicleNotification> vehicleNotifications;
-	VehicleNotification vehicleNotification;
-	
 	public void testCleanDatabase() throws Exception {
 		WebTestAPI api = new WebTestAPI();
-		latch = new CountDownLatch(1);
-		succeed = false;
+		final CountDownLatch latch = new CountDownLatch(1);
 		
 		api.cleanDatabase(new WebAPICallback<Void>() {
 			@Override
@@ -35,7 +30,7 @@ public class WebTestAPITestCase extends
 			}
 			
 			@Override
-			public void onFailed(int reqkey, int statusCode) {
+			public void onFailed(int reqkey, int statusCode, String response) {
 				latch.countDown();
 			}
 			
@@ -51,177 +46,76 @@ public class WebTestAPITestCase extends
 	}
 	
 	public void testVehicleNotifications() throws Exception {
-		WebTestAPI api = new WebTestAPI();
+		final WebTestAPI api = new WebTestAPI();
 
 		// オブジェクトをひとつ生成
-		VehicleNotification obj = new VehicleNotification();
-		obj.setInVehicleDeviceId(1);
-		obj.setOperatorId(20);
-		obj.setBody("車載器への通知1です");
-
-		latch = new CountDownLatch(1);
-		succeed = false;
-		resultCode = -1;
-		api.createVehicleNotification(obj, new WebAPICallback<VehicleNotification>() {
-
+		SyncCall<VehicleNotification> sc = new SyncCall<VehicleNotification>() {
 			@Override
-			public void onSucceed(int reqkey, int statusCode,
-					VehicleNotification result) {
-				succeed = true;
-				resultCode = statusCode;
-				latch.countDown();
-			}
+			public int run() throws Exception {
+				VehicleNotification obj = new VehicleNotification();
+				obj.setInVehicleDeviceId(1);
+				obj.setOperatorId(20);
+				obj.setBody("車載器への通知1です");
 
-			@Override
-			public void onFailed(int reqkey, int statusCode) {
-				resultCode = statusCode;
-				latch.countDown();
+				return api.createVehicleNotification(obj, this);
 			}
-
-			@Override
-			public void onException(int reqkey, WebAPIException ex) {
-				latch.countDown();
-			}
-		});
-		
-		latch.await();
-		assertTrue(succeed);
+		};
+		Log.d("VehicleNotification", sc.getResponseString());
+		assertEquals(SyncCall.SUCCEED, sc.getCallback());
 		
 		// オブジェクトをひとつ生成
-		obj = new VehicleNotification();
-		obj.setInVehicleDeviceId(1);
-		obj.setOperatorId(20);
-		obj.setBody("車載器への通知2です");
-		
-		latch = new CountDownLatch(1);
-		succeed = false;
-		api.createVehicleNotification(obj, new WebAPICallback<VehicleNotification>() {
-
+		sc = new SyncCall<VehicleNotification>() {
 			@Override
-			public void onSucceed(int reqkey, int statusCode,
-					VehicleNotification result) {
-				succeed = true;
-				latch.countDown();
-			}
+			public int run() throws Exception {
+				VehicleNotification obj = new VehicleNotification();
+				obj.setInVehicleDeviceId(1);
+				obj.setOperatorId(20);
+				obj.setBody("車載器への通知2です");
 
-			@Override
-			public void onFailed(int reqkey, int statusCode) {
-				latch.countDown();
+				return api.createVehicleNotification(obj, this);
 			}
-
-			@Override
-			public void onException(int reqkey, WebAPIException ex) {
-				latch.countDown();
-			}
-		});
-		
-		latch.await();
-		assertTrue(succeed);
+		};		
+		assertEquals(SyncCall.SUCCEED, sc.getCallback());
 
 		// オブジェクト数を確認
-		latch = new CountDownLatch(1);
-		succeed = false;
-		api.getAllVehicleNotifications(new WebAPICallback<List<VehicleNotification>>() {
-			
+		SyncCall<List<VehicleNotification>>scl = new SyncCall<List<VehicleNotification>>() {
 			@Override
-			public void onSucceed(int reqkey, int statusCode,
-					List<VehicleNotification> result) {
-				vehicleNotifications = result;
-				latch.countDown();
+			public int run() throws Exception {
+				return api.getAllVehicleNotifications(this);
 			}
-			
-			@Override
-			public void onFailed(int reqkey, int statusCode) {
-				latch.countDown();
-			}
-			
-			@Override
-			public void onException(int reqkey, WebAPIException ex) {
-				latch.countDown();
-			}
-		});
-
-		latch.await();
-		assertEquals(2, vehicleNotifications.size());
+		};		
+		assertEquals(SyncCall.SUCCEED, scl.getCallback());
+		assertEquals(2, scl.getResult().size());
 		
+		final int id = scl.getResult().get(1).getId();
 		// 2つめのオブジェクト取得
-		latch = new CountDownLatch(1);
-		succeed = false;
-		api.getVehicleNotification(vehicleNotifications.get(1).getId(), new WebAPICallback<VehicleNotification>() {
-
+		sc = new SyncCall<VehicleNotification>() {
 			@Override
-			public void onSucceed(int reqkey, int statusCode,
-					VehicleNotification result) {
-				vehicleNotification = result;
-				latch.countDown();
+			public int run() throws Exception {
+				return api.getVehicleNotification(id, this);
 			}
+		};		
+		assertEquals("車載器への通知2です", sc.getResult().getBody().orNull());
 
-			@Override
-			public void onFailed(int reqkey, int statusCode) {
-				latch.countDown();
-			}
-
-			@Override
-			public void onException(int reqkey, WebAPIException ex) {
-				latch.countDown();
-			}
-			
-		});
-		
-		latch.await();
-		assertEquals("車載器への通知2です", vehicleNotification.getBody().orNull());
-		
+		final int id2 = sc.getResult().getId();
 		// オブジェクトをひとつ削除
-		latch = new CountDownLatch(1);
-		succeed = false;
-		api.deleteVehicleNotification(vehicleNotifications.get(1).getId(), new WebAPICallback<Void>() {
-
+		SyncCall<Void> scv = new SyncCall<Void>() {
 			@Override
-			public void onSucceed(int reqkey, int statusCode, Void result) {
-				succeed = true;
-				latch.countDown();
+			public int run() throws Exception {
+				return api.deleteVehicleNotification(id2, this);
 			}
-
-			@Override
-			public void onFailed(int reqkey, int statusCode) {
-				latch.countDown();
-			}
-
-			@Override
-			public void onException(int reqkey, WebAPIException ex) {
-				latch.countDown();
-			}
-			
-		});
-		
-		latch.await();
-		assertTrue(succeed);
+		};
+		assertEquals(SyncCall.SUCCEED, scv.getCallback());
 		
 		// オブジェクト数を確認
-		latch = new CountDownLatch(1);
-		succeed = false;
-		api.getAllVehicleNotifications(new WebAPICallback<List<VehicleNotification>>() {
-			
+		scl = new SyncCall<List<VehicleNotification>>() {
 			@Override
-			public void onSucceed(int reqkey, int statusCode,
-					List<VehicleNotification> result) {
-				vehicleNotifications = result;
-				latch.countDown();
+			public int run() throws Exception {
+				return api.getAllVehicleNotifications(this);
 			}
-			
-			@Override
-			public void onFailed(int reqkey, int statusCode) {
-				latch.countDown();
-			}
-			
-			@Override
-			public void onException(int reqkey, WebAPIException ex) {
-				latch.countDown();
-			}
-		});
-
-		latch.await();
-		assertEquals(1, vehicleNotifications.size());
+		};		
+		assertEquals(SyncCall.SUCCEED, scl.getCallback());
+		assertEquals(1, scl.getResult().size());
 	}
 
 }
