40f76470847d2a1c5122036ed781a420ad095d01
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-04-17 21:04:03 +0900

    認証情報変更時は、内部データをクリアする

--- src/com/kogasoftware/odt/invehicledevice/SavePreferencesActivity.java
+++ src/com/kogasoftware/odt/invehicledevice/SavePreferencesActivity.java
@@ -19,9 +19,11 @@ public class SavePreferencesActivity extends Activity {
 				.getDefaultSharedPreferences(this);
 		Bundle bundle = intent.getExtras();
 		SharedPreferences.Editor editor = preferences.edit();
-		editor.putString("url", Strings.nullToEmpty(bundle.getString("url")));
+		editor.putString("url", Strings.nullToEmpty(bundle.getString("url"))); // TODO
+																				// 定数文字列?
 		editor.putString("token",
 				Strings.nullToEmpty(bundle.getString("token")));
+		editor.putBoolean("update", true);
 		editor.commit();
 		Toast.makeText(this, "設定を保存しました", Toast.LENGTH_LONG).show();
 		finish();
