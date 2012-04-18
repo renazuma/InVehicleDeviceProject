a32301feb09eb3ad9924ada699fd886f2e377fd2
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-04-18 20:59:27 +0900

    温度センサーの読みを一時的にLogに出すようにした

--- src/com/kogasoftware/odt/invehicledevice/logic/TemperatureSensorEventListener.java
+++ src/com/kogasoftware/odt/invehicledevice/logic/TemperatureSensorEventListener.java
@@ -3,9 +3,12 @@ package com.kogasoftware.odt.invehicledevice.logic;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.util.Log;
 
 public class TemperatureSensorEventListener extends LogicUser implements
 		SensorEventListener {
+	private static final String TAG = TemperatureSensorEventListener.class
+			.getSimpleName();
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -13,6 +16,15 @@ public class TemperatureSensorEventListener extends LogicUser implements
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-	}
+		if (!getLogic().isPresent()) {
+			return;
+		}
+		final Logic logic = getLogic().get();
 
+		if (event.sensor.getType() != Sensor.TYPE_TEMPERATURE) {
+			return;
+		}
+		Float celsius = event.values[0];
+		Log.i(TAG, "temperature changed=" + celsius);
+	}
 }
