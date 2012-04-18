ab40b6da5e75e705f9d130d6ce9f7c2d9e31bae7
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-04-18 11:57:28 +0900

    CIスクリプト修正

--- ci.sh
+++ ci.sh
@@ -7,8 +7,6 @@ ant $ant_arg findbugs
 #ant $ant_arg sonar
 
 cd tests
-ant $ant_arg uninstall findbugs
-ant $ant_arg all clean emma instrument install
 
 tar xf $HOME/userContent/OpenJTalk-sdcard.tar.xz
 p=com.kogasoftware.odt.invehicledevice
@@ -16,7 +14,8 @@ d=/mnt/sdcard/Android/data/$p/files/open_jtalk
 #adb $adb_arg shell mkdir $d
 adb $adb_arg push open_jtalk $d
 
-ant $ant_arg test-and-pull-results
+ant $ant_arg uninstall findbugs
+ant $ant_arg all clean emma debug install test-and-pull-results
 
 cd ..
 ant $ant_arg clean release
