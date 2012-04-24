6aa0f4efe88a061f8ccba2a73aea2b7e1cfcf1cc
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-04-24 14:28:53 +0900

    アンインストールを、依存ファイルアップロード前にした

--- ci.sh
+++ ci.sh
@@ -7,6 +7,7 @@ ant $ant_arg findbugs
 #ant $ant_arg sonar
 
 cd tests
+ant $ant_arg uninstall
 
 tar xf $HOME/userContent/OpenJTalk-sdcard.tar.xz
 p=com.kogasoftware.odt.invehicledevice
@@ -14,7 +15,7 @@ d=/mnt/sdcard/Android/data/$p/files/open_jtalk
 #adb $adb_arg shell mkdir $d
 adb $adb_arg push open_jtalk $d
 
-ant $ant_arg uninstall findbugs
+ant $ant_arg findbugs
 ant $ant_arg all clean emma debug install test-and-pull-results
 
 cd ..
