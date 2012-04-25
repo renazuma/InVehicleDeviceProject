e547f872e433371845a9cae39dadc0f580fbed31
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-04-25 15:35:18 +0900

    テスト環境修正

--- ci.sh
+++ ci.sh
@@ -3,12 +3,4 @@
 . AndroidCommon/ci_setup.sh $*
 
 cd `dirname $0`
-ant $ant_arg findbugs
-#ant $ant_arg sonar
-ant $ant_arg uninstall clean
-cd tests
-ant $ant_arg uninstall findbugs
-ant $ant_arg all clean emma debug install test-and-pull-results
-
-cd ..
-ant $ant_arg clean release
+ant $ant_arg ci
