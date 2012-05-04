6a8985d71eb221d9792b868525d2c2e8c31fe95d
Isamu Mogi <test@example.com>
2012-05-05 00:56:44 +0900

    送信対象に優先順位をつけられるようにした

--- src/com/kogasoftware/odt/invehicledevice/logic/event/UiEventBus.java
+++ src/com/kogasoftware/odt/invehicledevice/logic/event/UiEventBus.java
@@ -1,5 +1,10 @@
 package com.kogasoftware.odt.invehicledevice.logic.event;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -11,17 +16,28 @@ import android.util.Log;
 import com.google.common.eventbus.EventBus;
 
 /**
- * com.google.common.eventbus.EventBusクラスに、UIスレッド上でハンドラを実行する機能を追加
+ * com.google.common.eventbus.EventBusクラスに以下の機能を追加
+ * 
+ * - UIスレッド上でイベント処理を実行する.<br />
+ * - HighPriorityアノテーション付きのインスタンスは、他のオブジェクトよりも先にイベント処理する.<br />
+ * - 登録されている全てのオブジェクトを破棄する.<br />
  */
-public class UiEventBus extends EventBus {
+public class UiEventBus {
+	@Target(ElementType.TYPE)
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface HighPriority {
+	}
+
 	private static final String TAG = UiEventBus.class.getSimpleName();
 	private final List<Object> registeredObjects = new LinkedList<Object>();
 	private final AtomicBoolean disposed = new AtomicBoolean(false);
 	private final Object registerAndDisposeLock = new Object(); // registeredObjectsと実際にEventBusにregisterされたオブジェクトの整合性のためのロック
 	private final Handler uiHandler;
+	private final EventBus highPriorityEventBus = new EventBus();
+	private final EventBus lowPriorityEventBus = new EventBus();
 
 	/**
-	 * 実行対象ハンドラをMainLooperのHandlerを指定
+	 * 実行対象ハンドラとしてMainLooperのHandlerを指定
 	 */
 	public UiEventBus() {
 		this(new Handler(Looper.getMainLooper()));
@@ -52,17 +68,29 @@ public class UiEventBus extends EventBus {
 		}
 	}
 
+	private final EventBus getEventBusForObject(Object object) {
+		Class<?> c = object.getClass();
+		for (Annotation annotation : object.getClass().getAnnotations()) {
+			annotation.toString();
+		}
+		if (object.getClass().isAnnotationPresent(HighPriority.class)) {
+			return highPriorityEventBus;
+		} else {
+			return lowPriorityEventBus;
+		}
+	}
+
 	/**
-	 * UIのスレッドでObjectをポストするように修正
+	 * UIのスレッドでObjectをポストする
 	 */
-	@Override
 	public void post(final Object object) {
 		if (disposed.get()) {
 			return;
 		}
 		if (uiHandler.getLooper().getThread().getId() == Thread.currentThread()
 				.getId()) {
-			super.post(object);
+			highPriorityEventBus.post(object);
+			lowPriorityEventBus.post(object);
 			return;
 		}
 		uiHandler.post(new Runnable() {
@@ -74,27 +102,25 @@ public class UiEventBus extends EventBus {
 	}
 
 	/**
-	 * 登録と同時に、一括削除用に内部で独自にオブジェクトを保持しておくようにした
+	 * 登録と同時に、一括削除用に内部で独自にオブジェクトを保持
 	 */
-	@Override
 	public void register(Object object) {
 		synchronized (registerAndDisposeLock) {
 			if (disposed.get()) {
 				return;
 			}
 			registeredObjects.add(object);
-			super.register(object);
+			getEventBusForObject(object).register(object);
 		}
 	}
 
 	/**
-	 * 内部で独自に保持しているオブジェクトを削除する
+	 * 内部で独自に保持しているオブジェクトを削除
 	 */
-	@Override
 	public void unregister(Object object) {
 		synchronized (registerAndDisposeLock) {
 			registeredObjects.remove(object);
-			super.unregister(object);
+			getEventBusForObject(object).unregister(object);
 		}
 	}
 }
--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/logic/event/UiEventBusTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/logic/event/UiEventBusTestCase.java
@@ -5,18 +5,15 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 
-import android.os.Handler;
-
-import com.google.common.base.Preconditions;
 import com.google.common.eventbus.Subscribe;
 import com.kogasoftware.odt.invehicledevice.logic.event.UiEventBus;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
 
 public class UiEventBusTestCase extends EmptyActivityInstrumentationTestCase2 {
-	interface Test {
-		void test(Object object);
+	static class Test {
+		void test(Object object) {
+		}
 	}
 
 	UiEventBus ueb;
@@ -24,22 +21,6 @@ public class UiEventBusTestCase extends EmptyActivityInstrumentationTestCase2 {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-
-		final CountDownLatch cdl = new CountDownLatch(1);
-		final AtomicReference<Handler> ar = new AtomicReference<Handler>();
-		Handler h;
-		getActivity().runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				ar.set(new Handler());
-				cdl.countDown();
-			}
-		});
-		cdl.await();
-		h = ar.get();
-		Preconditions.checkNotNull(h);
-
-		// ueb = new UiEventBus(h);
 		ueb = new UiEventBus(getActivityHandler());
 	}
 
@@ -92,6 +73,116 @@ public class UiEventBusTestCase extends EmptyActivityInstrumentationTestCase2 {
 		assertFalse(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
 		assertEquals(ai1.get(), 1);
 		assertEquals(ai2.get(), 0);
+	}
+
+	/**
+	 * HighPriorityがついているオブジェクトは先にイベントを受信
+	 */
+	public void testHighPriority() throws Exception {
+		final AtomicInteger ai = new AtomicInteger(0);
+		final Semaphore s1 = new Semaphore(0);
+		final Semaphore s2 = new Semaphore(0);
+		final Semaphore s3 = new Semaphore(0);
+		final Semaphore s4 = new Semaphore(0);
+
+		@UiEventBus.HighPriority
+		class HighTest1 {
+			@Subscribe
+			public void test(Object object) {
+				ai.compareAndSet(0, 1);
+				s1.release();
+			}
+		}
+		HighTest1 t1 = new HighTest1();
+
+		Test t2 = new Test() {
+			@Subscribe
+			@Override
+			public void test(Object object) {
+				ai.compareAndSet(1, 2);
+				s2.release();
+			}
+		};
+
+		@UiEventBus.HighPriority
+		class HighTest3 {
+			@Subscribe
+			public void test(Object object) {
+				ai.compareAndSet(0, 3);
+				ai.compareAndSet(1, 3);
+				s3.release();
+			}
+		}
+		HighTest3 t3 = new HighTest3();
+
+		Test t4 = new Test() {
+			@Subscribe
+			@Override
+			public void test(Object object) {
+				ai.compareAndSet(3, 4);
+				s4.release();
+			}
+		};
+
+		assertTrue(HighTest1.class
+				.isAnnotationPresent(UiEventBus.HighPriority.class));
+		assertTrue((new HighTest3()).getClass().isAnnotationPresent(
+				UiEventBus.HighPriority.class));
+		assertTrue(t1.getClass().isAnnotationPresent(
+				UiEventBus.HighPriority.class));
+		assertFalse(t2.getClass().isAnnotationPresent(
+				UiEventBus.HighPriority.class));
+		assertTrue(t3.getClass().isAnnotationPresent(
+				UiEventBus.HighPriority.class));
+		assertFalse(t4.getClass().isAnnotationPresent(
+				UiEventBus.HighPriority.class));
+
+		// t1とt2では、t1が先に実行される
+		ai.set(0);
+		ueb.register(t1);
+		ueb.register(t2);
+		assertEquals(ai.get(), 0);
+		ueb.post(new Object());
+		assertTrue(s1.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertEquals(ai.get(), 2);
+
+		// t3は最後に追加されたが、t2よりも先に実行される
+		ai.set(0);
+		ueb.register(t3);
+		assertEquals(ai.get(), 0);
+		ueb.post(new Object());
+		assertTrue(s1.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s3.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertEquals(ai.get(), 3);
+
+		// t4は最後に追加されたが、t1,t3よりも後に実行される
+		ai.set(0);
+		ueb.register(t4);
+		assertEquals(ai.get(), 0);
+		ueb.post(new Object());
+		assertTrue(s1.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s3.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s4.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertEquals(ai.get(), 4);
+
+		ai.set(0);
+		ueb.unregister(t1);
+		ueb.unregister(t4);
+		assertEquals(ai.get(), 0);
+		ueb.post(new Object());
+		assertTrue(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s3.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertEquals(ai.get(), 3);
+
+		ai.set(1);
+		ueb.unregister(t3);
+		assertEquals(ai.get(), 1);
+		ueb.post(new Object());
+		assertTrue(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertEquals(ai.get(), 2);
 	}
 
 	/**
@@ -209,6 +300,13 @@ public class UiEventBusTestCase extends EmptyActivityInstrumentationTestCase2 {
 		assertFalse(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
 		assertEquals(ai1.get(), 2);
 		assertEquals(ai2.get(), 1);
-	}
 
+		// 一度unregisterしてももう一度registerでイベントが渡る
+		ueb.register(t2);
+		ueb.post(new Object());
+		assertTrue(s1.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertTrue(s2.tryAcquire(500, TimeUnit.MILLISECONDS));
+		assertEquals(ai1.get(), 3);
+		assertEquals(ai2.get(), 2);
+	}
 }
