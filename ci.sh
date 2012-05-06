460b2926585a0cbb0accc710615619d76ff96d8f
Isamu Mogi <test@example.com>
2012-05-07 03:01:38 +0900

    antでも車載器アプリと通信部分のソースを共通化

--- ci.sh
+++ ci.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+git clone -b master git@github.com:odt/InVehicleDevice.git
+dir=src/com/kogasoftware/odt/invehicledevice/logic/
+mkdir -p $dir
+cp InVehicleDevice/$dir/SharedPreferencesKey.java $dir
+
 . AndroidCommon/ci_setup.sh $*
 
 cd `dirname $0`
