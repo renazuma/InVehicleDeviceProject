1d82de7aedd2538ec737cd976701272e4bee9a8c
Isamu Mogi <test@example.com>
2012-05-06 14:52:58 +0900

    VTextViewテストのスタブ追加。目検必要

--- tests/src/jp/tomorrowkey/android/vtextviewer/test/unit/VTextViewTestCase.java
+++ tests/src/jp/tomorrowkey/android/vtextviewer/test/unit/VTextViewTestCase.java
@@ -1,22 +1,25 @@
 package jp.tomorrowkey.android.vtextviewer.test.unit;
 
-import com.jayway.android.robotium.solo.Solo;
+import jp.tomorrowkey.android.vtextviewer.VTextView;
+
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
 
 public class VTextViewTestCase extends EmptyActivityInstrumentationTestCase2 {
+	VTextView vtv;
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		solo = new Solo(getInstrumentation(), getActivity());
+		vtv = (VTextView) inflateAndAddTestLayout(com.kogasoftware.odt.invehicledevice.test.R.layout.test_vtext_view);
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		solo.finishOpenedActivities();
 		super.tearDown();
 	}
 
 	public void test1() throws Exception {
+		vtv.setText("吾輩は猫である。名前はまだ無い。");
 		fail("stub! / physical test required");
 	}
 }
