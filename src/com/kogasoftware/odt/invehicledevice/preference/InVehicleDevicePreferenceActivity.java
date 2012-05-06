f53c1313c894a08b990b017c8a75d4dda6de9977
Isamu Mogi <test@example.com>
2012-05-07 01:56:17 +0900

    Bundleのキーを定数にした

--- src/com/kogasoftware/odt/invehicledevice/preference/InVehicleDevicePreferenceActivity.java
+++ src/com/kogasoftware/odt/invehicledevice/preference/InVehicleDevicePreferenceActivity.java
@@ -2,25 +2,24 @@ package com.kogasoftware.odt.invehicledevice.preference;
 
 import org.json.JSONException;
 
-import com.kogasoftware.odt.webapi.WebAPI;
-import com.kogasoftware.odt.webapi.WebAPI.WebAPICallback;
-import com.kogasoftware.odt.webapi.WebAPIException;
-import com.kogasoftware.odt.webapi.model.InVehicleDevice;
-
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Looper;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.kogasoftware.odt.invehicledevice.logic.SharedPreferencesKey;
+import com.kogasoftware.odt.webapi.WebAPI;
+import com.kogasoftware.odt.webapi.WebAPI.WebAPICallback;
+import com.kogasoftware.odt.webapi.WebAPIException;
+import com.kogasoftware.odt.webapi.model.InVehicleDevice;
 
 public class InVehicleDevicePreferenceActivity extends PreferenceActivity
 		implements WebAPICallback<InVehicleDevice> {
@@ -67,36 +66,6 @@ public class InVehicleDevicePreferenceActivity extends PreferenceActivity
 	}
 
 	@Override
-	public void onSucceed(int reqKey, int statusCode, InVehicleDevice result) {
-		if (reqKey != latestReqKey) {
-			return;
-		}
-		try {
-			dismissDialog(CONNECTING_DIALOG_ID);
-		} catch (IllegalArgumentException e) {
-		}
-		if (!result.getAuthenticationToken().isPresent()) {
-			Toast.makeText(this, "token not found", Toast.LENGTH_LONG).show();
-			finish();
-		}
-		String token = result.getAuthenticationToken().get();
-		String message = "reqKey=" + reqKey + " token=" + token;
-		SharedPreferences preference = PreferenceManager
-				.getDefaultSharedPreferences(InVehicleDevicePreferenceActivity.this);
-		String url = preference.getString("connection_url", "");
-
-		Intent intent = new Intent();
-		intent.setAction(Intent.ACTION_SEND);
-		intent.putExtra("url", url);
-		intent.putExtra("token", token);
-		String packageName = "com.kogasoftware.odt.invehicledevice";
-		intent.setClassName(packageName, packageName
-				+ ".ui.activity.SavePreferencesActivity");
-		startActivity(intent);
-		finish();
-	}
-
-	@Override
 	protected Dialog onCreateDialog(int id) {
 		switch (id) {
 		case CONNECTING_DIALOG_ID: {
@@ -110,13 +79,15 @@ public class InVehicleDevicePreferenceActivity extends PreferenceActivity
 	}
 
 	@Override
-	public void onFailed(int reqKey, int statusCode, String response) {
+	public void onException(int reqKey, WebAPIException ex) {
 		if (reqKey != latestReqKey) {
 			return;
 		}
-		final String message = "onFailed: reqKey=" + reqKey + ", statusCode="
-				+ statusCode + " response=" + response;
+		final String message = "onException: reqKey=" + reqKey + ", exception="
+				+ ex;
+
 		runOnUiThread(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					dismissDialog(CONNECTING_DIALOG_ID);
@@ -129,14 +100,14 @@ public class InVehicleDevicePreferenceActivity extends PreferenceActivity
 	}
 
 	@Override
-	public void onException(int reqKey, WebAPIException ex) {
+	public void onFailed(int reqKey, int statusCode, String response) {
 		if (reqKey != latestReqKey) {
 			return;
 		}
-		final String message = "onException: reqKey=" + reqKey + ", exception="
-				+ ex;
-
+		final String message = "onFailed: reqKey=" + reqKey + ", statusCode="
+				+ statusCode + " response=" + response;
 		runOnUiThread(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					dismissDialog(CONNECTING_DIALOG_ID);
@@ -146,5 +117,35 @@ public class InVehicleDevicePreferenceActivity extends PreferenceActivity
 						Toast.LENGTH_LONG).show();
 			}
 		});
+	}
+
+	@Override
+	public void onSucceed(int reqKey, int statusCode, InVehicleDevice result) {
+		if (reqKey != latestReqKey) {
+			return;
+		}
+		try {
+			dismissDialog(CONNECTING_DIALOG_ID);
+		} catch (IllegalArgumentException e) {
+		}
+		if (!result.getAuthenticationToken().isPresent()) {
+			Toast.makeText(this, "token not found", Toast.LENGTH_LONG).show();
+			finish();
+		}
+		String token = result.getAuthenticationToken().get();
+		SharedPreferences preference = PreferenceManager
+				.getDefaultSharedPreferences(InVehicleDevicePreferenceActivity.this);
+		String url = preference.getString("connection_url", "");
+
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_SEND);
+		intent.putExtra(SharedPreferencesKey.SERVER_URL, url);
+		intent.putExtra(SharedPreferencesKey.SERVER_IN_VEHICLE_DEVICE_TOKEN,
+				token);
+		String packageName = "com.kogasoftware.odt.invehicledevice";
+		intent.setClassName(packageName, packageName
+				+ ".ui.activity.SavePreferencesActivity");
+		startActivity(intent);
+		finish();
 	}
 }
