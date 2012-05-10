7350581153d9af7ee70fa552ba3596e3a171b54e
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-10 14:52:49 +0900

    テストリファクタリング

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/OperationScheduleReceiveThreadTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/OperationScheduleReceiveThreadTestCase.java
@@ -1,11 +1,9 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.backgroundtask;
 
 import java.text.ParseException;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,16 +14,14 @@ import com.google.common.base.Function;
 import com.google.common.eventbus.Subscribe;
 import com.kogasoftware.odt.invehicledevice.backgroundtask.OperationScheduleReceiveThread;
 import com.kogasoftware.odt.invehicledevice.logic.CommonLogic;
-import com.kogasoftware.odt.invehicledevice.logic.VehicleNotifications;
-import com.kogasoftware.odt.invehicledevice.logic.event.PauseEvent;
 import com.kogasoftware.odt.invehicledevice.logic.event.UpdatedOperationScheduleReceiveStartEvent;
 import com.kogasoftware.odt.invehicledevice.logic.event.UpdatedOperationScheduleReceivedEvent;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
+import com.kogasoftware.odt.invehicledevice.test.util.Subscriber;
 import com.kogasoftware.odt.invehicledevice.test.util.TestUtil;
 import com.kogasoftware.odt.invehicledevice.test.util.datasource.DummyDataSource;
 import com.kogasoftware.odt.webapi.WebAPIException;
 import com.kogasoftware.odt.webapi.model.OperationSchedule;
-import com.kogasoftware.odt.webapi.model.VehicleNotification;
 
 public class OperationScheduleReceiveThreadTestCase extends
 		EmptyActivityInstrumentationTestCase2 {
@@ -116,24 +112,6 @@ public class OperationScheduleReceiveThreadTestCase extends
 		return l;
 	}
 
-	class TestEventSubscriber {
-		final Class<?> c;
-		CountDownLatch cdl = new CountDownLatch(1);
-		Semaphore s = new Semaphore(0);
-
-		public TestEventSubscriber(Class<?> c) {
-			this.c = c;
-		}
-
-		@Subscribe
-		public void handle(Object object) {
-			if (c.isInstance(object)) {
-				cdl.countDown();
-				s.release();
-			}
-		}
-	}
-
 	public void testUpdatedOperationScheduleReceiveStartEvent通知が無い場合二度めのOperationScheduleを受信はしない()
 			throws Exception {
 		final AtomicInteger seq = new AtomicInteger(0);
@@ -148,14 +126,13 @@ public class OperationScheduleReceiveThreadTestCase extends
 		TestUtil.setDataSource(new TestDataSource());
 		cl = newCommonLogic();
 		osrt = new OperationScheduleReceiveThread(cl);
-		TestEventSubscriber tes = new TestEventSubscriber(
-				UpdatedOperationScheduleReceivedEvent.class);
-		cl.registerEventListener(tes);
+		Subscriber<UpdatedOperationScheduleReceivedEvent> s = Subscriber.of(
+				UpdatedOperationScheduleReceivedEvent.class, cl);
 		cl.registerEventListener(osrt);
 		osrt.start();
 		Thread.sleep(2 * 1000);
 		assertEquals(1, seq.get());
-		assertEquals(1, tes.s.availablePermits());
+		assertEquals(1, s.s.availablePermits());
 	}
 
 	public void testUpdatedOperationScheduleReceiveStartEvent通知で二度めのOperationScheduleを受信()
@@ -173,15 +150,14 @@ public class OperationScheduleReceiveThreadTestCase extends
 		TestUtil.setDataSource(new TestDataSource());
 		cl = newCommonLogic();
 		osrt = new OperationScheduleReceiveThread(cl);
-		TestEventSubscriber tes = new TestEventSubscriber(
-				UpdatedOperationScheduleReceivedEvent.class);
-		cl.registerEventListener(tes);
+		Subscriber<UpdatedOperationScheduleReceivedEvent> s = Subscriber.of(
+				UpdatedOperationScheduleReceivedEvent.class, cl);
 		cl.registerEventListener(osrt);
 		osrt.start();
 		cl.postEvent(new UpdatedOperationScheduleReceiveStartEvent());
 		Thread.sleep(2 * 1000);
 		assertEquals(2, seq.get());
-		assertEquals(2, tes.s.availablePermits());
+		assertEquals(2, s.s.availablePermits());
 	}
 
 	public void testUpdatedOperationScheduleReceiveStartEvent二回通知で三度めのOperationScheduleを受信()
@@ -199,14 +175,14 @@ public class OperationScheduleReceiveThreadTestCase extends
 		cl = newCommonLogic();
 		osrt = new OperationScheduleReceiveThread(cl);
 		osrt.start();
-		TestEventSubscriber tes = new TestEventSubscriber(
-				UpdatedOperationScheduleReceivedEvent.class);
-		cl.registerEventListener(tes);
+		Subscriber<UpdatedOperationScheduleReceivedEvent> s = Subscriber.of(
+				UpdatedOperationScheduleReceivedEvent.class, cl);
+		cl.registerEventListener(s);
 		cl.registerEventListener(osrt);
 		cl.postEvent(new UpdatedOperationScheduleReceiveStartEvent());
 		cl.postEvent(new UpdatedOperationScheduleReceiveStartEvent());
 		Thread.sleep(2 * 1000);
 		assertEquals(3, seq.get());
-		assertEquals(3, tes.s.availablePermits());
+		assertEquals(3, s.s.availablePermits());
 	}
 }
