e20346eb659f37456c52f93ca138e864853c5251
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-10 14:53:11 +0900

    VehicleNotificationReceiverのテスト

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/VehicleNotificationReceiverTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/VehicleNotificationReceiverTestCase.java
@@ -1,25 +1,27 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.backgroundtask;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.kogasoftware.odt.invehicledevice.backgroundtask.VehicleNotificationReceiver;
 import com.kogasoftware.odt.invehicledevice.logic.CommonLogic;
-import com.kogasoftware.odt.invehicledevice.logic.datasource.DataSourceFactory;
+import com.kogasoftware.odt.invehicledevice.logic.event.VehicleNotificationReceivedEvent;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
+import com.kogasoftware.odt.invehicledevice.test.util.Subscriber;
+import com.kogasoftware.odt.invehicledevice.test.util.TestUtil;
 import com.kogasoftware.odt.invehicledevice.test.util.datasource.DummyDataSource;
+import com.kogasoftware.odt.webapi.WebAPIException;
+import com.kogasoftware.odt.webapi.model.VehicleNotification;
 
 public class VehicleNotificationReceiverTestCase extends
 		EmptyActivityInstrumentationTestCase2 {
 
 	CommonLogic cl;
-	DummyDataSource dds;
-	VehicleNotificationReceiver vnr;
+	
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		dds = new DummyDataSource();
-		DataSourceFactory.setInstance(dds);
-		cl = newCommonLogic();
-		vnr = new VehicleNotificationReceiver(cl);
 	}
 
 	@Override
@@ -30,8 +32,61 @@ public class VehicleNotificationReceiverTestCase extends
 		super.tearDown();
 	}
 
-	public void xtestVehicleNotificationReceiver_1() throws Exception {
-		// 引数のクラスがnewできないのでstub
-		fail("stub!");
+	public void testRun_新しいVNがあればVehicleNotificationReceivedEvent通知() throws Exception {
+		final VehicleNotification vn0 = new VehicleNotification();
+		final VehicleNotification vn1 = new VehicleNotification();
+		class TestDataSource extends DummyDataSource {
+			@Override
+			public List<VehicleNotification> getVehicleNotifications()
+					throws WebAPIException {
+				List<VehicleNotification> l = new LinkedList<VehicleNotification>();
+				l.add(vn0);
+				l.add(vn1);
+				return l;
+			}
+		}
+		TestUtil.setDataSource(new TestDataSource());
+		cl = newCommonLogic();
+		VehicleNotificationReceiver vnr = new VehicleNotificationReceiver(cl);
+		Subscriber<VehicleNotificationReceivedEvent> s = Subscriber.of(VehicleNotificationReceivedEvent.class, cl);
+		vnr.run();
+		Thread.sleep(1000);
+		assertEquals(1, s.l.size());
+		assertEquals(vn0, s.l.get(0).vehicleNotifications.get(0));
+		assertEquals(vn1, s.l.get(0).vehicleNotifications.get(1));
+	}
+	
+	public void testRun_新しいVNがなければVehicleNotificationReceivedEvent通知は起きない_1() throws Exception {
+		class TestDataSource extends DummyDataSource {
+			@Override
+			public List<VehicleNotification> getVehicleNotifications()
+					throws WebAPIException {
+				return new LinkedList<VehicleNotification>();
+			}
+		}
+		TestUtil.setDataSource(new TestDataSource());
+		cl = newCommonLogic();
+		VehicleNotificationReceiver vnr = new VehicleNotificationReceiver(cl);
+		Subscriber<VehicleNotificationReceivedEvent> s = Subscriber.of(VehicleNotificationReceivedEvent.class, cl);
+		vnr.run();
+		Thread.sleep(1000);
+		assertTrue(s.l.isEmpty());
+	}
+	
+	public void testRun_新しいVNがなければVehicleNotificationReceivedEvent通知は起きない_2() throws Exception {
+		class TestDataSource extends DummyDataSource {
+			@Override
+			public List<VehicleNotification> getVehicleNotifications()
+					throws WebAPIException {
+				throw new WebAPIException(false, "not found");
+			}
+		}
+		TestUtil.setDataSource(new TestDataSource());
+		cl = newCommonLogic();
+		VehicleNotificationReceiver vnr = new VehicleNotificationReceiver(cl);
+		Subscriber<VehicleNotificationReceivedEvent> s = Subscriber.of(VehicleNotificationReceivedEvent.class, cl);
+		vnr.run();
+		Thread.sleep(1000);
+		assertTrue(s.l.isEmpty());
 	}
 }
