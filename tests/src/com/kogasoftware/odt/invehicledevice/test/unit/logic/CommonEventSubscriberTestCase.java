ecf80af6f428ae253af20f7c8ed54f8e34836f51
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-10 16:05:23 +0900

    通知の受信関連のテスト追加

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/logic/CommonEventSubscriberTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/logic/CommonEventSubscriberTestCase.java
@@ -1,5 +1,7 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.logic;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -13,10 +15,12 @@ import com.kogasoftware.odt.invehicledevice.logic.StatusAccess;
 import com.kogasoftware.odt.invehicledevice.logic.StatusAccess.Reader;
 import com.kogasoftware.odt.invehicledevice.logic.StatusAccess.Writer;
 import com.kogasoftware.odt.invehicledevice.logic.StatusAccess.VoidReader;
+import com.kogasoftware.odt.invehicledevice.logic.VehicleNotifications;
 import com.kogasoftware.odt.invehicledevice.logic.event.OrientationChangedEvent;
 import com.kogasoftware.odt.invehicledevice.logic.event.PauseEvent;
 import com.kogasoftware.odt.invehicledevice.logic.event.StopEvent;
 import com.kogasoftware.odt.invehicledevice.logic.event.TemperatureChangedEvent;
+import com.kogasoftware.odt.invehicledevice.logic.event.VehicleNotificationReceivedEvent;
 import com.kogasoftware.odt.invehicledevice.logic.event.VehicleNotificationRepliedEvent;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
 import com.kogasoftware.odt.invehicledevice.ui.modalview.NotificationModalView;
@@ -32,6 +36,16 @@ public class CommonEventSubscriberTestCase extends
 	protected void setUp() throws Exception {
 		super.setUp();
 		sa = new StatusAccess(getActivity());
+		sa.write(new Writer() {
+			@Override
+			public void write(Status status) {
+				status.vehicleNotifications.clear();
+				status.sendLists.repliedVehicleNotifications.clear();
+				status.receivingOperationScheduleChangedVehicleNotifications.clear();
+				status.receivedOperationScheduleChangedVehicleNotifications.clear();
+			}
+		});
+
 		cl = new CommonLogic(getActivity(), getActivityHandler(), sa);
 
 		assertEquals(cl.countRegisteredClass(CommonEventSubscriber.class)
@@ -137,9 +151,7 @@ public class CommonEventSubscriberTestCase extends
 		sa.write(new Writer() {
 			@Override
 			public void write(Status status) {
-				status.vehicleNotifications.clear();
 				status.vehicleNotifications.add(vn1);
-				status.sendLists.repliedVehicleNotifications.clear();
 				status.sendLists.repliedVehicleNotifications.add(vn2);
 			}
 		});
@@ -170,10 +182,8 @@ public class CommonEventSubscriberTestCase extends
 		sa.write(new Writer() {
 			@Override
 			public void write(Status status) {
-				status.vehicleNotifications.clear();
 				status.vehicleNotifications.add(vn1);
 				status.vehicleNotifications.add(vn2);
-				status.sendLists.repliedVehicleNotifications.clear();
 			}
 		});
 		final CountDownLatch cdl = new CountDownLatch(1);
@@ -198,6 +208,272 @@ public class CommonEventSubscriberTestCase extends
 				assertEquals(
 						status.sendLists.repliedVehicleNotifications.get(0),
 						vn1);
+			}
+		});
+	}
+	
+	public void testMergeVehicleNotification_一般notification追加() throws Exception {
+		final VehicleNotification vn = new VehicleNotification();
+		vn.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn);
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(1, status.vehicleNotifications.size());
+				assertEquals(vn, status.vehicleNotifications.get(0));
+				assertTrue(status.sendLists.repliedVehicleNotifications.isEmpty());
+				assertTrue(status.receivingOperationScheduleChangedVehicleNotifications.isEmpty());
+			}
+		});
+	}
+	
+	public void testMergeVehicleNotification_一般notification同一IDは追加されない() throws Exception {
+		final VehicleNotification vn0 = new VehicleNotification();
+		final VehicleNotification vn1a = new VehicleNotification();
+		final VehicleNotification vn1b = new VehicleNotification();
+		final VehicleNotification vn2 = new VehicleNotification();
+		vn0.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn1a.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn1b.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn2.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn0.setId(100);
+		vn1a.setId(101);
+		vn1b.setId(101);
+		vn2.setId(102);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn0);
+		vns.add(vn1a);
+		vns.add(vn2);
+		sa.write(new Writer() {
+			@Override
+			public void write(Status status) {
+				status.vehicleNotifications.add(vn1b);
+			}
+		});
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(3, status.vehicleNotifications.size());
+				assertEquals(vn1b, status.vehicleNotifications.get(0));
+				assertEquals(vn0, status.vehicleNotifications.get(1));
+				assertEquals(vn2, status.vehicleNotifications.get(2));
+				assertTrue(status.sendLists.repliedVehicleNotifications.isEmpty());
+				assertTrue(status.receivingOperationScheduleChangedVehicleNotifications.isEmpty());
+			}
+		});
+	}
+
+	public void testMergeVehicleNotification_一般notification同一IDがsendListにある場合追加されない() throws Exception {
+		final VehicleNotification vn0 = new VehicleNotification();
+		final VehicleNotification vn1 = new VehicleNotification();
+		final VehicleNotification vn2a = new VehicleNotification();
+		final VehicleNotification vn2b = new VehicleNotification();
+		vn0.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn1.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn2a.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn2b.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn0.setId(10);
+		vn1.setId(11);
+		vn2a.setId(12);
+		vn2b.setId(12);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn0);
+		vns.add(vn1);
+		vns.add(vn2a);
+		sa.write(new Writer() {
+			@Override
+			public void write(Status status) {
+				status.sendLists.repliedVehicleNotifications.add(vn2b);
+			}
+		});
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(2, status.vehicleNotifications.size());
+				assertEquals(vn0, status.vehicleNotifications.get(0));
+				assertEquals(vn1, status.vehicleNotifications.get(1));
+				assertEquals(1, status.sendLists.repliedVehicleNotifications.size());
+				assertEquals(vn2b, status.sendLists.repliedVehicleNotifications.get(0));
+				assertTrue(status.receivingOperationScheduleChangedVehicleNotifications.isEmpty());
+			}
+		});
+	}
+	
+	public void testMergeVehicleNotification_スケジュールnotification追加() throws Exception {
+		final VehicleNotification vn = new VehicleNotification();
+		vn.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn);
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(1, status.receivingOperationScheduleChangedVehicleNotifications.size());
+				assertEquals(vn, status.receivingOperationScheduleChangedVehicleNotifications.get(0));
+				assertTrue(status.vehicleNotifications.isEmpty());
+			}
+		});
+	}
+	
+	public void testMergeVehicleNotification_スケジュールnotification同一IDは追加されない() throws Exception {
+		final VehicleNotification vn0 = new VehicleNotification();
+		final VehicleNotification vn1a = new VehicleNotification();
+		final VehicleNotification vn1b = new VehicleNotification();
+		final VehicleNotification vn2 = new VehicleNotification();
+		vn0.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn1a.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn1b.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn2.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn0.setId(20);
+		vn1a.setId(21);
+		vn1b.setId(21);
+		vn2.setId(22);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn0);
+		vns.add(vn1a);
+		vns.add(vn2);
+		sa.write(new Writer() {
+			@Override
+			public void write(Status status) {
+				status.receivingOperationScheduleChangedVehicleNotifications.add(vn1b);
+			}
+		});
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(3, status.receivingOperationScheduleChangedVehicleNotifications.size());
+				assertEquals(vn1b, status.receivingOperationScheduleChangedVehicleNotifications.get(0));
+				assertEquals(vn0, status.receivingOperationScheduleChangedVehicleNotifications.get(1));
+				assertEquals(vn2, status.receivingOperationScheduleChangedVehicleNotifications.get(2));
+				assertTrue(status.sendLists.repliedVehicleNotifications.isEmpty());
+				assertTrue(status.vehicleNotifications.isEmpty());
+			}
+		});
+	}
+
+	public void testMergeVehicleNotification_スケジュールnotification同一IDがsendListにある場合追加されない() throws Exception {
+		final VehicleNotification vn0 = new VehicleNotification();
+		final VehicleNotification vn1 = new VehicleNotification();
+		final VehicleNotification vn2a = new VehicleNotification();
+		final VehicleNotification vn2b = new VehicleNotification();
+		vn0.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn1.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn2a.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn2b.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn0.setId(200);
+		vn1.setId(201);
+		vn2a.setId(202);
+		vn2b.setId(202);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn0);
+		vns.add(vn1);
+		vns.add(vn2a);
+		sa.write(new Writer() {
+			@Override
+			public void write(Status status) {
+				status.sendLists.repliedVehicleNotifications.add(vn2b);
+			}
+		});
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(2, status.receivingOperationScheduleChangedVehicleNotifications.size());
+				assertEquals(vn0, status.receivingOperationScheduleChangedVehicleNotifications.get(0));
+				assertEquals(vn1, status.receivingOperationScheduleChangedVehicleNotifications.get(1));
+
+				assertEquals(1, status.sendLists.repliedVehicleNotifications.size());
+				assertEquals(vn2b, status.sendLists.repliedVehicleNotifications.get(0));
+
+				assertTrue(status.vehicleNotifications.isEmpty());
+			}
+		});
+	}
+	
+	public void testMergeVehicleNotification_スケジュールnotification同一IDがreceivedにある場合追加されない() throws Exception {
+		final VehicleNotification vn0a = new VehicleNotification();
+		final VehicleNotification vn0b = new VehicleNotification();
+		final VehicleNotification vn1 = new VehicleNotification();
+		final VehicleNotification vn2 = new VehicleNotification();
+		vn0a.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn0b.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn1.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn2.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn0a.setId(1000);
+		vn0b.setId(1000);
+		vn1.setId(1001);
+		vn2.setId(1002);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn0a);
+		vns.add(vn1);
+		vns.add(vn2);
+		sa.write(new Writer() {
+			@Override
+			public void write(Status status) {
+				status.receivedOperationScheduleChangedVehicleNotifications.add(vn0b);
+			}
+		});
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(2, status.receivingOperationScheduleChangedVehicleNotifications.size());
+				assertEquals(vn1, status.receivingOperationScheduleChangedVehicleNotifications.get(0));
+				assertEquals(vn2, status.receivingOperationScheduleChangedVehicleNotifications.get(1));
+
+				assertEquals(1, status.receivedOperationScheduleChangedVehicleNotifications.size());
+				assertEquals(vn0b, status.receivedOperationScheduleChangedVehicleNotifications.get(0));
+				
+				assertTrue(status.vehicleNotifications.isEmpty());
+			}
+		});
+	}
+	
+	public void testMergeVehicleNotification_スケジュールと一般で振り分けが起きる() throws Exception {
+		final VehicleNotification vn0 = new VehicleNotification();
+		final VehicleNotification vn1 = new VehicleNotification();
+		final VehicleNotification vn2 = new VehicleNotification();
+		final VehicleNotification vn3 = new VehicleNotification();
+		vn0.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn1.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn2.setNotificationType(VehicleNotifications.NotificationType.SCHEDULE_CHANGED);
+		vn3.setNotificationType(VehicleNotifications.NotificationType.NORMAL);
+		vn0.setId(200);
+		vn1.setId(201);
+		vn2.setId(202);
+		vn3.setId(203);
+		List<VehicleNotification> vns = new LinkedList<VehicleNotification>();
+		vns.add(vn0);
+		vns.add(vn1);
+		vns.add(vn2);
+		vns.add(vn3);
+		cl.postEvent(new VehicleNotificationReceivedEvent(vns));
+		getInstrumentation().waitForIdleSync();
+		sa.read(new VoidReader() {
+			@Override
+			public void read(Status status) {
+				assertEquals(2, status.vehicleNotifications.size());
+				assertEquals(vn1, status.vehicleNotifications.get(0));
+				assertEquals(vn3, status.vehicleNotifications.get(1));
+
+				assertEquals(2, status.receivingOperationScheduleChangedVehicleNotifications.size());
+				assertEquals(vn0, status.receivingOperationScheduleChangedVehicleNotifications.get(0));
+				assertEquals(vn2, status.receivingOperationScheduleChangedVehicleNotifications.get(1));
+				
+				assertTrue(status.sendLists.repliedVehicleNotifications.isEmpty());
+				assertTrue(status.receivedOperationScheduleChangedVehicleNotifications.isEmpty());
 			}
 		});
 	}
