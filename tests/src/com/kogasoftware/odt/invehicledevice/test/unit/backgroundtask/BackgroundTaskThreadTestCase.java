6207ed2e68f2d54910336f3c89eae8b981a32298
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-11 18:31:50 +0900

    BackgroundTaskの負荷テスト

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/BackgroundTaskThreadTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/BackgroundTaskThreadTestCase.java
@@ -1,11 +1,18 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.backgroundtask;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.kogasoftware.odt.invehicledevice.backgroundtask.BackgroundTaskThread;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
 
 public class BackgroundTaskThreadTestCase extends
 		EmptyActivityInstrumentationTestCase2 {
-	
+
 	public void setUp() throws Exception {
 		super.setUp();
 		Thread.sleep(10 * 1000);
@@ -22,6 +29,67 @@ public class BackgroundTaskThreadTestCase extends
 			t.interrupt();
 			t.join(10000);
 			assertFalse(t.isAlive());
+		}
+	}
+
+	/**
+	 * BackgroundTaskを大量に発生させても致命的なエラーは発生しない
+	 */
+	public void testMultipleInstances() throws Exception {
+		final Queue<Thread> ts = new ConcurrentLinkedQueue<Thread>();
+		for (int i = 0; i < 100; ++i) {
+			final int fi = i;
+			Thread t = new Thread() {
+				@Override
+				public void run() {
+					Uninterruptibles.sleepUninterruptibly(fi * 50,
+							TimeUnit.MILLISECONDS);
+					BackgroundTaskThread bt = new BackgroundTaskThread(
+							getActivity());
+					bt.start();
+					ts.add(bt);
+				}
+			};
+			t.start();
+			ts.add(t);
+		}
+		int l = 0;
+		for (Thread t : new LinkedList<Thread>(ts)) {
+			if (++l % 2 == 0) {
+				continue;
+			}
+			t.interrupt();
+		}
+		for (int i = 0; i < 100; ++i) {
+			final int fi = i;
+			Thread t = new Thread() {
+				@Override
+				public void run() {
+					Uninterruptibles.sleepUninterruptibly(fi * 50,
+							TimeUnit.MILLISECONDS);
+					BackgroundTaskThread bt = new BackgroundTaskThread(
+							getActivity());
+					bt.start();
+					ts.add(bt);
+				}
+			};
+			t.start();
+			ts.add(t);
+		}
+		while (true) {
+			Boolean alive = false;
+			for (Thread t : new LinkedList<Thread>(ts)) {
+				alive |= t.isAlive();
+			}
+			if (!alive) {
+				break;
+			}
+			for (Thread t : new LinkedList<Thread>(ts)) {
+				t.interrupt();
+			}
+			for (Thread t : new LinkedList<Thread>(ts)) {
+				t.join();
+			}
 		}
 	}
 
