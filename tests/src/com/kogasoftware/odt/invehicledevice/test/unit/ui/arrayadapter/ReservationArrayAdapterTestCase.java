d16473eed1a015eb72cfcddc5152b6092f64810d
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-16 18:56:35 +0900

    テスト対象クラス名変更に追従

---
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/arrayadapter/ReservationArrayAdapterTestCase.java
@@ -0,0 +1,82 @@
+package com.kogasoftware.odt.invehicledevice.test.unit.ui.arrayadapter;
+
+import com.kogasoftware.odt.invehicledevice.logic.CommonLogic;
+import com.kogasoftware.odt.invehicledevice.logic.Status;
+import com.kogasoftware.odt.invehicledevice.logic.StatusAccess;
+import com.kogasoftware.odt.invehicledevice.logic.StatusAccess.Writer;
+import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
+import com.kogasoftware.odt.invehicledevice.ui.arrayadapter.ReservationArrayAdapter;
+import com.kogasoftware.odt.webapi.model.PassengerRecord;
+import com.kogasoftware.odt.webapi.model.Reservation;
+import com.kogasoftware.odt.webapi.model.User;
+
+public class ReservationArrayAdapterTestCase extends
+		EmptyActivityInstrumentationTestCase2 {
+
+	CommonLogic cl;
+	ReservationArrayAdapter raa;
+	StatusAccess sa;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		sa = new StatusAccess(getActivity());
+		cl = new CommonLogic(getActivity(), getActivityHandler(), sa);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		if (cl != null) {
+			cl.dispose();
+		}
+	}
+
+	public void testReservationが表示される() throws Exception {
+		final String userName0 = "上野駅前";
+		final String userName1 = "御徒町駅前";
+		sa.write(new Writer() {
+			@Override
+			public void write(Status status) {
+				status.reservations.clear();
+				{
+					PassengerRecord pr = new PassengerRecord();
+					Reservation r = new Reservation();
+					User u = new User();
+					u.setLastName(userName0);
+					r.setUser(u);
+					r.setPassengerRecord(pr);
+					status.reservations.add(r);
+				}
+
+				{
+					PassengerRecord pr = new PassengerRecord();
+					Reservation r = new Reservation();
+					User u = new User();
+					u.setLastName(userName1);
+					r.setUser(u);
+					r.setPassengerRecord(pr);
+					status.reservations.add(r);
+				}
+			}
+		});
+
+		raa = new ReservationArrayAdapter(getInstrumentation()
+				.getTargetContext(), cl);
+		runOnUiThreadSync(new Runnable() {
+			@Override
+			public void run() {
+				getActivity().setContentView(raa.getView(0, null, null));
+			}
+		});
+		solo.searchText(userName0);
+
+		runOnUiThreadSync(new Runnable() {
+			@Override
+			public void run() {
+				getActivity().setContentView(raa.getView(1, null, null));
+			}
+		});
+		solo.searchText(userName1);
+	}
+}
