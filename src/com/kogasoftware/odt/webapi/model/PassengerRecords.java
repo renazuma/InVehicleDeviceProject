8ae107b2fc0d9a219f954d433815c0d60fea2e99
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-16 13:46:49 +0900

    refs #4208 共通ロジック追加

--- src/com/kogasoftware/odt/webapi/model/PassengerRecords.java
+++ src/com/kogasoftware/odt/webapi/model/PassengerRecords.java
@@ -2,8 +2,26 @@ package com.kogasoftware.odt.webapi.model;
 
 public class PassengerRecords {
 	public static class Status {
-		public static final Integer UNHANDLED = 1;
-		public static final Integer RIDING = 2;
-		public static final Integer GOT_OFF = 3;
+		public static final Integer UNHANDLED = 0;
+		public static final Integer RIDING = 1;
+		public static final Integer GOT_OFF = 2;
+	}
+
+	public static Boolean isGotOff(Reservation reservation) {
+		return isStatus(reservation, Status.UNHANDLED);
+	}
+
+	public static Boolean isRiding(Reservation reservation) {
+		return isStatus(reservation, Status.UNHANDLED);
+	}
+
+	private static Boolean isStatus(Reservation reservation, Integer status) {
+		return reservation.getPassengerRecord().isPresent()
+				&& reservation.getPassengerRecord().get().getStatus()
+						.equals(status);
+	}
+
+	public static Boolean isUnhandled(Reservation reservation) {
+		return isStatus(reservation, Status.UNHANDLED);
 	}
 }
