1661a44f74b29a0256304fcfd92aabd50ef0c84f
HIDAKA Takahiro <takahr@gmail.com>
2012-05-17 15:42:03 +0900

    operation_records アクセス追加

---
+++ app/controllers/operation_records_controller.rb
@@ -0,0 +1,43 @@
+class OperationRecordsController < ApplicationController
+  def index
+    @operation_records = OperationRecord.all
+    respond_to do | format |
+      format.json do
+        render json: @operation_records.to_json
+      end
+    end
+  end
+
+  def show
+    @operation_record = OperationRecorde.find(params[:id])
+
+    respond_to do |format|
+      format.json do
+        render json: @operation_record.to_json
+      end
+    end
+  end
+
+  def create
+    @operation_record = OperationRecord.new(params[:operation_record])
+
+    respond_to do |format|
+      if @operation_record.save
+        format.json { render json: @operation_record, status: :created}
+      else
+        format.json { render json: @operation_record.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @operation_record = OperationRecord.find(params[:id])
+    @operation_record.destroy
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ WebAPITest::Application.routes.draw do
   resources :operators
   resources :service_providers
   resources :operation_schedules
+  resources :operation_records
   resources :passenger_records
   resources :platforms
   resources :reservations
