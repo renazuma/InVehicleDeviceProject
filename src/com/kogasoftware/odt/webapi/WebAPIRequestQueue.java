2d391142a7227c73501e0610df89f8dbaa324f11
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-22 20:03:20 +0900

    refs #4329 通信のグループ化を実装するために内部データ構造をMultimapで置き換え

--- src/com/kogasoftware/odt/webapi/WebAPIRequestQueue.java
+++ src/com/kogasoftware/odt/webapi/WebAPIRequestQueue.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 
 import org.apache.commons.lang3.SerializationException;
@@ -15,6 +18,9 @@ import org.apache.commons.lang3.SerializationUtils;
 import android.util.Log;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimap;
 
 /**
  * WebAPIRequestを管理するクラス
@@ -22,15 +28,23 @@ import com.google.common.base.Optional;
 public class WebAPIRequestQueue {
 	private static final String TAG = WebAPIRequestQueue.class.getSimpleName();
 	private static final Object FILE_ACCESS_LOCK = new Object(); // ファイルアクセス中のスレッドを一つに制限するためのロック。将来的にはロックの粒度をファイル毎にする必要があるかもしれない。
-	private static class SerializationList extends LinkedList<WebAPIRequest<?>> {
-		private static final long serialVersionUID = 6728979449299498099L;
+	public static final String DEFAULT_GROUP = "default_group";
+
+	private static class InstanceState implements Serializable {
+		private static final long serialVersionUID = 672897944999498099L;
+		private final LinkedListMultimap<String, WebAPIRequest<?>> requests;
+
+		public InstanceState(Multimap<String, WebAPIRequest<?>> requests) {
+			this.requests = LinkedListMultimap.create(requests);
+		}
 	}
 
-	// 作業中リクエスト
-	protected final Queue<WebAPIRequest<?>> processingQueue = new LinkedList<WebAPIRequest<?>>();
-	// 作業待ちリクエスト
-	protected final Queue<WebAPIRequest<?>> waitingQueue = new LinkedList<WebAPIRequest<?>>();
-	// 作業待ちリクエストの追加待ち処理を実装するためのセマフォ。パーミットの数は必ずwaitingQueueのサイズと同じか多くなるようにする。
+	// 作業中のグループ
+	protected final Set<String> processingGroups = new HashSet<String>();
+	// グループ毎作業待ちリクエスト
+	protected final ListMultimap<String, WebAPIRequest<?>> requestsByGroup = LinkedListMultimap
+			.create();
+	// グループ毎作業待ちリクエストの追加待ち処理を実装するためのセマフォ。パーミットの数は必ずwaitingQueueのサイズと同じか多くなるようにする。
 	protected final Semaphore waitingQueuePollPermissions = new Semaphore(0);
 	// 各キューの整合性を保つためのロック
 	protected final Object queueLock = new Object();
@@ -54,33 +68,38 @@ public class WebAPIRequestQueue {
 		}
 		synchronized (FILE_ACCESS_LOCK) {
 			try {
-				Object object = SerializationUtils.deserialize(new FileInputStream(backupFile));
-				if (!(object instanceof SerializationList)) {
-					Log.w(TAG, "!(" + object + " instanceof SerializationList)");
+				Object object = SerializationUtils
+						.deserialize(new FileInputStream(backupFile));
+				if (!(object instanceof InstanceState)) {
+					Log.w(TAG, "!(" + object + " instanceof InstanceState)");
 					return;
 				}
-				waitingQueue.addAll((SerializationList) object);
+				requestsByGroup.putAll(((InstanceState) object).requests);
 			} catch (IOException e) {
 				Log.w(TAG, e);
 			} catch (SerializationException e) {
 				Log.e(TAG, e.toString(), e);
 			} finally {
-				waitingQueuePollPermissions.release(waitingQueue.size());
+				waitingQueuePollPermissions.release(requestsByGroup.size());
 			}
 		}
 	}
 
 	/**
-	 * 作業待ちリクエストを追加し、作業待ちリクエストの追加待ちをしているスレッドを一つ起こせるようにする
+	 * リクエストを追加し、作業待ちリクエストの追加待ちをしているスレッドを一つ起こせるようにする
 	 * 
 	 * @param request
 	 */
-	public void add(WebAPIRequest<?> request) {
+	public void add(WebAPIRequest<?> request, String group) {
 		synchronized (queueLock) {
-			waitingQueue.add(request);
-			waitingQueuePollPermissions.release();
+			requestsByGroup.put(group, request);
 			backup();
+			waitingQueuePollPermissions.release();
 		}
+	}
+
+	public void add(WebAPIRequest<?> request) {
+		add(request, DEFAULT_GROUP);
 	}
 
 	/**
@@ -93,15 +112,16 @@ public class WebAPIRequestQueue {
 	}
 
 	protected void backup(File backupFile) {
-		SerializationList list = new SerializationList();
+		Multimap<String, WebAPIRequest<?>> backupRequests = LinkedListMultimap
+				.create();
 		synchronized (queueLock) {
-			list.addAll(waitingQueue);
-			list.addAll(processingQueue);
+			backupRequests.putAll(requestsByGroup);
 		}
 
 		synchronized (FILE_ACCESS_LOCK) {
 			try {
-				SerializationUtils.serialize(list, new FileOutputStream(backupFile));
+				SerializationUtils.serialize(new InstanceState(backupRequests),
+						new FileOutputStream(backupFile));
 			} catch (SerializationException e) {
 				Log.e(TAG, e.toString(), e);
 			} catch (IOException e) {
@@ -117,28 +137,49 @@ public class WebAPIRequestQueue {
 	 */
 	public void remove(WebAPIRequest<?> request) {
 		synchronized (queueLock) {
-			waitingQueue.remove(request);
-			processingQueue.remove(request);
+			for (Entry<String, WebAPIRequest<?>> entry : LinkedListMultimap
+					.create(requestsByGroup).entries()) {
+				if (entry.getValue() != request) {
+					continue;
+				}
+				String group = entry.getKey();
+				processingGroups.remove(group);
+				requestsByGroup.removeAll(group);
+			}
 			backup();
 		}
 	}
 
 	/**
-	 * 作業中リクエストを作業待ちリクエストに移動
+	 * 指定したリクエストに対応するグループを作業中から削除し、グループ全体の処理順を最後に移動。
+	 * 指定したリクエストがデフォルトグループの場合、さらにグループ内の処理順を最後に移動。
 	 * 
 	 * @param request
 	 *            リトライ対象のリクエスト
 	 */
 	public void retry(WebAPIRequest<?> request) {
 		synchronized (queueLock) {
-			processingQueue.remove(request);
-			add(request);
+			for (Entry<String, WebAPIRequest<?>> entry : LinkedListMultimap
+					.create(requestsByGroup).entries()) {
+				if (entry.getValue() != request) {
+					continue;
+				}
+				String group = entry.getKey();
+				processingGroups.remove(group);
+				requestsByGroup.putAll(group, requestsByGroup.removeAll(group));
+				if (group.equals(DEFAULT_GROUP)) {
+					requestsByGroup.remove(group, request);
+					requestsByGroup.put(group, request);
+				}
+				backup();
+				waitingQueuePollPermissions.release();
+				break;
+			}
 		}
 	}
 
 	/**
-	 * 作業待ちリクエストからリクエストを一つ取得し、そのリクエストを作業中リクエストに保存
-	 * 作業待ちリクエストがない場合は、作業待ちリクエストが追加されるまで待つ
+	 * 作業中でないグループからリクエストを一つ取得し返す。またそのグループを作業中とする。 該当グループが存在しない場合は、存在するようになるまで待つ
 	 * 
 	 * @return リクエスト
 	 * @throws InterruptedException
@@ -147,13 +188,22 @@ public class WebAPIRequestQueue {
 		while (true) {
 			waitingQueuePollPermissions.acquire(); // synchronizedの外で待つ
 			synchronized (queueLock) {
-				WebAPIRequest<?> request = waitingQueue.poll();
-				if (request == null) {
-					continue;
+				for (String group : new LinkedList<String>(
+						requestsByGroup.keys())) {
+					if (!group.equals(DEFAULT_GROUP)) {
+						if (processingGroups.contains(group)) {
+							continue;
+						}
+						processingGroups.add(group);
+					}
+					List<WebAPIRequest<?>> requests = requestsByGroup
+							.get(group);
+					if (requests == null || requests.isEmpty()) {
+						requestsByGroup.removeAll(group);
+						continue;
+					}
+					return requests.get(0);
 				}
-				backup();
-				processingQueue.add(request);
-				return request;
 			}
 		}
 	}
