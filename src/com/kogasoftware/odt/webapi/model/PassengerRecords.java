5898236d8802b9653b899efce2b6cc67f40b2739
茂木 勇 <isamu_mogi@kogasoftware.com>
2012-05-23 11:25:42 +0900

    乗客の乗り降り状態の取得方法を修正

--- src/com/kogasoftware/odt/webapi/model/PassengerRecords.java
+++ src/com/kogasoftware/odt/webapi/model/PassengerRecords.java
@@ -1,43 +1,62 @@
 package com.kogasoftware.odt.webapi.model;
 
+import org.json.JSONException;
+
+import android.util.Log;
+
 public class PassengerRecords {
-	public static class Status {
-		public static final Integer UNHANDLED = 0;
-		public static final Integer RIDING = 1;
-		public static final Integer GOT_OFF = 2;
-	}
+	private static final String TAG = PassengerRecords.class.getSimpleName();
 
+	/**
+	 * 降車済みかどうかを調べる
+	 */
 	public static Boolean isGotOff(Reservation reservation) {
-		return isStatus(reservation, Status.GOT_OFF);
-	}
-
-	public static Boolean isRiding(Reservation reservation) {
-		return isStatus(reservation, Status.RIDING);
+		return reservation.getPassengerRecord().isPresent()
+				&& isGotOff(reservation.getPassengerRecord().get());
 	}
 
 	public static Boolean isGotOff(PassengerRecord passengerRecord) {
-		return isStatus(passengerRecord, Status.GOT_OFF);
+		return passengerRecord.getGetOnTime().isPresent()
+				&& passengerRecord.getGetOffTime().isPresent();
+	}
+
+	/**
+	 * 乗車中かどうかを調べる
+	 */
+	public static Boolean isRiding(Reservation reservation) {
+		return reservation.getPassengerRecord().isPresent()
+				&& isRiding(reservation.getPassengerRecord().get());
 	}
 
 	public static Boolean isRiding(PassengerRecord passengerRecord) {
-		return isStatus(passengerRecord, Status.RIDING);
-	}
-	
-	private static Boolean isStatus(Reservation reservation, Integer status) {
-		return reservation.getPassengerRecord().isPresent()
-				&& isStatus(reservation.getPassengerRecord().get(), status);
+		return passengerRecord.getGetOnTime().isPresent()
+				&& !passengerRecord.getGetOffTime().isPresent();
 	}
 
-	private static Boolean isStatus(PassengerRecord passengerRecord,
-			Integer status) {
-		return passengerRecord.getStatus().equals(status);
-	}
-
+	/**
+	 * 未乗車かどうかを調べる エラー状態。降車済みとして扱うと現在の車載器のUIで操作が不可能なため、未乗車として扱う。
+	 */
 	public static Boolean isUnhandled(Reservation reservation) {
-		return isStatus(reservation, Status.UNHANDLED);
+		return reservation.getPassengerRecord().isPresent()
+				&& isUnhandled(reservation.getPassengerRecord().get());
 	}
-	
+
 	public static Boolean isUnhandled(PassengerRecord passengerRecord) {
-		return isStatus(passengerRecord, Status.UNHANDLED);
+		if (passengerRecord.getGetOnTime().isPresent()) {
+			return false;
+		}
+		if (!passengerRecord.getGetOffTime().isPresent()) {
+			return true;
+		}
+		String jsonString = "";
+		try {
+			jsonString = passengerRecord.toJSONObject().toString();
+		} catch (JSONException e) {
+			Log.w(TAG, e);
+		}
+		Log.w(TAG,
+				"PassengerRecord (!getGetOnTime().isPresent() && getGetOffTime().isPresent()) : "
+						+ jsonString);
+		return true;
 	}
 }
