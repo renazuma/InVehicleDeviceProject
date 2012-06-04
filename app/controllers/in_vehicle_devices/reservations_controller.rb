72a6e298d8b175fa28939576ef59503a8e516d0b
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-04 17:23:52 +0900

    テストデータを増やした

--- app/controllers/in_vehicle_devices/reservations_controller.rb
+++ app/controllers/in_vehicle_devices/reservations_controller.rb
@@ -4,9 +4,27 @@ class InVehicleDevices::ReservationsController < ApplicationController
     respond_to do |format|
       format.json do
         render json: [
-          {id: 1, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1, departure_platform_id: 1, arrival_platform_id: 2, created_at: Date.today, updated_at: Date.today},
-          {id: 2, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1, departure_platform_id: 1, arrival_platform_id: 2, created_at: Date.today, updated_at: Date.today},
-          {id: 3, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1, departure_platform_id: 1, arrival_platform_id: 2, created_at: Date.today, updated_at: Date.today},
+          {
+            id: 10, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1, created_at: Date.today, updated_at: Date.today,
+            departure_platform_id: 100,
+            departure_platform: {id: 100},
+            arrival_platform_id: 200,
+            arrival_platform: {id: 200},
+          },
+          {
+            id: 11, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1, created_at: Date.today, updated_at: Date.today,
+            departure_platform_id: 200,
+            departure_platform: {id: 200},
+            arrival_platform_id: 300,
+            arrival_platform: {id: 300},
+          },
+          {
+            id: 12, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1, created_at: Date.today, updated_at: Date.today,
+            departure_platform_id: 300,
+            departure_platform: {id: 300},
+            arrival_platform_id: 400,
+            arrival_platform: {id: 400},
+          },
         ]
       end
     end
@@ -16,8 +34,14 @@ class InVehicleDevices::ReservationsController < ApplicationController
   def create
     respond_to do |format|
       format.json do
-        render json:
-          {id: 1, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1, departure_platform_id: 1, arrival_platform_id: 2, created_at: Date.today, updated_at: Date.today}.to_json
+        render json: {
+          id: 1, departure_time: Date.today, arrival_time: Date.today, passenger_count: 1,
+          created_at: Date.today, updated_at: Date.today,
+          departure_platform_id: 1,
+          departure_platform: {id: 1},
+          arrival_platform_id: 2,
+          arrival_platform: {id: 2},
+        }.to_json
       end
     end
   end
