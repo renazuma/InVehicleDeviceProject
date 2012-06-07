87ddd6465631b22722057936282691f6b347be82
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-07 18:39:13 +0900

    音声合成と音声再生の処理がまざっていたのを修正

--- src/com/kogasoftware/odt/invehicledevice/backgroundtask/VoiceThread.java
+++ src/com/kogasoftware/odt/invehicledevice/backgroundtask/VoiceThread.java
@@ -19,6 +19,7 @@ import android.util.Log;
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import com.google.common.eventbus.Subscribe;
+import com.kogasoftware.odt.invehicledevice.logic.empty.EmptyThread;
 import com.kogasoftware.odt.invehicledevice.ui.activity.InVehicleDeviceActivity;
 
 public class VoiceThread extends Thread {
@@ -30,10 +31,29 @@ public class VoiceThread extends Thread {
 		}
 	}
 
+	private static class VoiceEntry {
+		private final String string;
+		private final File file;
+
+		public VoiceEntry(String string, File file) {
+			this.string = string;
+			this.file = file;
+		}
+
+		public String getString() {
+			return string;
+		}
+
+		public File getFile() {
+			return file;
+		}
+	}
+
 	private static final String TAG = VoiceThread.class.getSimpleName();
 	private static final Integer MAX_CACHE_BYTES = 100 * 1024 * 1024;
 	private static final Integer MAX_MESSAGE_LENGTH = 200;
 	private final BlockingQueue<String> voices = new LinkedBlockingQueue<String>();
+	private final BlockingQueue<VoiceEntry> voiceEntries = new LinkedBlockingQueue<VoiceEntry>();
 	private final Context context;
 	private volatile CountDownLatch speakableLatch = new CountDownLatch(0); // 参照書き換え可能な状態で複数スレッドから読まれるためvolatileをつける
 
@@ -41,22 +61,24 @@ public class VoiceThread extends Thread {
 		this.context = context;
 	}
 
-	public static List<String> split(String message, Integer maxLineLength) {
+	public static List<String> split(String message, Integer maxLength) {
 		List<String> result = new LinkedList<String>();
 		for (String line : Splitter.on(CharMatcher.anyOf("\r\n"))
 				.omitEmptyStrings().split(message)) {
-			if (line.length() <= maxLineLength) {
+			if (line.length() <= maxLength) {
 				result.add(line);
 				continue;
 			}
 			for (String word : Splitter
 					.on(CharMatcher.BREAKING_WHITESPACE.or(CharMatcher
-							.anyOf("。、　"))).omitEmptyStrings().split(line)) {
-				if (word.length() <= maxLineLength) {
+							.anyOf("。、　()（）「」"))).omitEmptyStrings()
+					.split(line)) {
+				if (word.length() <= maxLength) {
 					result.add(word);
 					continue;
 				}
-				for (String broken : Splitter.fixedLength(maxLineLength).split(word)) {
+				for (String broken : Splitter.fixedLength(maxLength)
+						.split(word)) {
 					result.add(broken);
 				}
 			}
@@ -73,15 +95,33 @@ public class VoiceThread extends Thread {
 
 	@Override
 	public void run() {
+		Thread speakThread = new EmptyThread();
 		try {
-			VoiceCache voiceCache = new VoiceCache(context, MAX_CACHE_BYTES);
+			final VoiceCache voiceCache = new VoiceCache(context,
+					MAX_CACHE_BYTES);
+			speakThread = new Thread() {
+				@Override
+				public void run() {
+					try {
+						while (true) {
+							VoiceEntry voiceEntry = voiceEntries.take();
+							speak(voiceCache, voiceEntry.getString(),
+									voiceEntry.getFile());
+						}
+					} catch (InterruptedException e) {
+					}
+				}
+			};
+			speakThread.start();
 			while (true) {
 				String voice = voices.take();
-				speak(voiceCache, voice);
+				synthesis(voiceCache, voice);
 			}
 		} catch (IOException e) {
 			Log.e(TAG, "IOException", e);
 		} catch (InterruptedException e) {
+		} finally {
+			speakThread.interrupt();
 		}
 	}
 
@@ -96,8 +136,18 @@ public class VoiceThread extends Thread {
 		speakableLatch.countDown();
 	}
 
-	private void speak(final VoiceCache voiceCache, final String voice)
+	private void synthesis(final VoiceCache voiceCache, final String voice)
 			throws InterruptedException {
+		try {
+			voiceEntries.add(new VoiceEntry(voice, voiceCache.get(voice)));
+		} catch (ExecutionException e) {
+			Log.w(TAG, "voice=" + voice, e);
+			voiceCache.invalidate(voice);
+		}
+	}
+
+	private void speak(final VoiceCache voiceCache, final String voiceString,
+			final File voiceFile) throws InterruptedException {
 		speakableLatch.await();
 
 		final Semaphore semaphore = new Semaphore(0);
@@ -112,25 +162,21 @@ public class VoiceThread extends Thread {
 			@Override
 			public boolean onError(MediaPlayer mp, int what, int extra) {
 				Log.w(TAG, "onError(" + mp + ", " + what + ", " + extra
-						+ ") voice=\"" + voice + "\"");
-				voiceCache.invalidate(voice);
+						+ ") voice=\"" + voiceString + "\" at " + voiceFile);
+				voiceCache.invalidate(voiceString);
 				semaphore.release();
 				return false;
 			}
 		});
 		mediaPlayer.setVolume(1.0f, 1.0f);
 		try {
-			File voicePath = voiceCache.get(voice);
-			mediaPlayer.setDataSource(voicePath.getAbsolutePath());
+			mediaPlayer.setDataSource(voiceFile.getAbsolutePath());
 			mediaPlayer.prepare();
 			mediaPlayer.start();
 			semaphore.acquire();
-		} catch (ExecutionException e) {
-			Log.w(TAG, "voice=" + voice, e);
-			voiceCache.invalidate(voice);
 		} catch (IOException e) {
-			Log.w(TAG, "voice=" + voice, e);
-			voiceCache.invalidate(voice);
+			Log.w(TAG, "voice=" + voiceString, e);
+			voiceCache.invalidate(voiceString);
 		} finally {
 			mediaPlayer.release();
 		}
