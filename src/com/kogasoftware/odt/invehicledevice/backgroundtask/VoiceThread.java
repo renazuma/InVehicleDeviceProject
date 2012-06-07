69fbd9b8ea1a23cc228a929e129c126a6906a821
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-07 17:35:34 +0900

    音声が長くなるとライブラリがエラーになるようなので、発音する文字列を短く分割するようにした

--- src/com/kogasoftware/odt/invehicledevice/backgroundtask/VoiceThread.java
+++ src/com/kogasoftware/odt/invehicledevice/backgroundtask/VoiceThread.java
@@ -2,6 +2,8 @@ package com.kogasoftware.odt.invehicledevice.backgroundtask;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
@@ -14,6 +16,8 @@ import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
 import android.util.Log;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
 import com.google.common.eventbus.Subscribe;
 import com.kogasoftware.odt.invehicledevice.ui.activity.InVehicleDeviceActivity;
 
@@ -28,6 +32,7 @@ public class VoiceThread extends Thread {
 
 	private static final String TAG = VoiceThread.class.getSimpleName();
 	private static final Integer MAX_CACHE_BYTES = 100 * 1024 * 1024;
+	private static final Integer MAX_MESSAGE_LENGTH = 200;
 	private final BlockingQueue<String> voices = new LinkedBlockingQueue<String>();
 	private final Context context;
 	private volatile CountDownLatch speakableLatch = new CountDownLatch(0); // 参照書き換え可能な状態で複数スレッドから読まれるためvolatileをつける
@@ -36,9 +41,34 @@ public class VoiceThread extends Thread {
 		this.context = context;
 	}
 
+	public static List<String> split(String message, Integer maxLineLength) {
+		List<String> result = new LinkedList<String>();
+		for (String line : Splitter.on(CharMatcher.anyOf("\r\n"))
+				.omitEmptyStrings().split(message)) {
+			if (line.length() <= maxLineLength) {
+				result.add(line);
+				continue;
+			}
+			for (String word : Splitter
+					.on(CharMatcher.BREAKING_WHITESPACE.or(CharMatcher
+							.anyOf("。、　"))).omitEmptyStrings().split(line)) {
+				if (word.length() <= maxLineLength) {
+					result.add(word);
+					continue;
+				}
+				for (String broken : Splitter.fixedLength(maxLineLength).split(word)) {
+					result.add(broken);
+				}
+			}
+		}
+		return result;
+	}
+
 	@Subscribe
 	public void enqueue(SpeakEvent event) {
-		voices.add(event.message);
+		for (String message : split(event.message, MAX_MESSAGE_LENGTH)) {
+			voices.add(message);
+		}
 	}
 
 	@Override
@@ -66,40 +96,41 @@ public class VoiceThread extends Thread {
 		speakableLatch.countDown();
 	}
 
-	private void speak(VoiceCache voiceCache, String voice)
+	private void speak(final VoiceCache voiceCache, final String voice)
 			throws InterruptedException {
 		speakableLatch.await();
 
+		final Semaphore semaphore = new Semaphore(0);
 		MediaPlayer mediaPlayer = new MediaPlayer();
-		try {
-			try {
-				File voicePath = voiceCache.get(voice);
-				mediaPlayer.setDataSource(voicePath.getAbsolutePath());
-			} catch (ExecutionException e) {
-				Log.w(TAG, e);
-				return;
+		mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				semaphore.release();
 			}
+		});
+		mediaPlayer.setOnErrorListener(new OnErrorListener() {
+			@Override
+			public boolean onError(MediaPlayer mp, int what, int extra) {
+				Log.w(TAG, "onError(" + mp + ", " + what + ", " + extra
+						+ ") voice=\"" + voice + "\"");
+				voiceCache.invalidate(voice);
+				semaphore.release();
+				return false;
+			}
+		});
+		mediaPlayer.setVolume(1.0f, 1.0f);
+		try {
+			File voicePath = voiceCache.get(voice);
+			mediaPlayer.setDataSource(voicePath.getAbsolutePath());
 			mediaPlayer.prepare();
-
-			final Semaphore semaphore = new Semaphore(0);
-			mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
-				@Override
-				public void onCompletion(MediaPlayer mp) {
-					semaphore.release();
-				}
-			});
-			mediaPlayer.setOnErrorListener(new OnErrorListener() {
-				@Override
-				public boolean onError(MediaPlayer mp, int what, int extra) {
-					semaphore.release();
-					return false;
-				}
-			});
 			mediaPlayer.start();
 			semaphore.acquire();
-		} catch (IOException e) {
+		} catch (ExecutionException e) {
+			Log.w(TAG, "voice=" + voice, e);
 			voiceCache.invalidate(voice);
-			Log.w(TAG, e);
+		} catch (IOException e) {
+			Log.w(TAG, "voice=" + voice, e);
+			voiceCache.invalidate(voice);
 		} finally {
 			mediaPlayer.release();
 		}
--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/VoiceThreadTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/backgroundtask/VoiceThreadTestCase.java
@@ -1,5 +1,6 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.backgroundtask;
 
+import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
@@ -55,5 +56,80 @@ public class VoiceThreadTestCase extends EmptyActivityInstrumentationTestCase2 {
 
 	public void xtestVoiceThread_1() throws Exception {
 		fail("stub! / physical test required");
+	}
+	
+	public void testSplit() {
+		// 行分割
+		Iterator<String> l;
+		l = VoiceThread.split("", 3).iterator();
+		assertFalse(l.hasNext());
+		l = VoiceThread.split("\n", 3).iterator();
+		assertFalse(l.hasNext());
+		l = VoiceThread.split("\n\n\r\n", 3).iterator();
+		assertFalse(l.hasNext());
+		
+		l = VoiceThread.split("a", 3).iterator();
+		assertEquals("a", l.next());
+		assertFalse(l.hasNext());
+		
+		l = VoiceThread.split("aa", 3).iterator();
+		assertEquals("aa", l.next());
+		assertFalse(l.hasNext());
+		
+		l = VoiceThread.split("aa\nbb", 3).iterator();
+		assertEquals("aa", l.next());
+		assertEquals("bb", l.next());
+		assertFalse(l.hasNext());
+		
+		l = VoiceThread.split("aa\nbb\rcc", 3).iterator();
+		assertEquals("aa", l.next());
+		assertEquals("bb", l.next());
+		assertEquals("cc", l.next());
+		assertFalse(l.hasNext());
+		
+		l = VoiceThread.split("\raa\nbb\r\ncc\r\n", 3).iterator();
+		assertEquals("aa", l.next());
+		assertEquals("bb", l.next());
+		assertEquals("cc", l.next());
+		assertFalse(l.hasNext());
+		
+		// 句読点分割
+		l = VoiceThread.split("\raaa\nbb、bb\r\nc、c\r\n", 3).iterator();
+		assertEquals("aaa", l.next());
+		assertEquals("bb", l.next());
+		assertEquals("bb", l.next());
+		assertEquals("c、c", l.next());
+		assertFalse(l.hasNext());
+		
+		l = VoiceThread.split("\ra。\nb。b\r\nc　c\r\n", 2).iterator();
+		assertEquals("a。", l.next());
+		assertEquals("b", l.next());
+		assertEquals("b", l.next());
+		assertEquals("c", l.next());
+		assertEquals("c", l.next());
+		assertFalse(l.hasNext());
+		
+		// 句読点で分割しても足りない場合文字数で分割
+		l = VoiceThread.split("\raaaa\nbbb\r\nc、cccc、c\r\n", 3).iterator();
+		assertEquals("aaa", l.next());
+		assertEquals("a", l.next());
+		assertEquals("bbb", l.next());
+		assertEquals("c", l.next());
+		assertEquals("ccc", l.next());
+		assertEquals("c", l.next());
+		assertEquals("c", l.next());
+		assertFalse(l.hasNext());
+
+		l = VoiceThread.split("\raaa a\nbbb\r\nc、cccc、c\r\n", 2).iterator();
+		assertEquals("aa", l.next());
+		assertEquals("a", l.next());
+		assertEquals("a", l.next());
+		assertEquals("bb", l.next());
+		assertEquals("b", l.next());
+		assertEquals("c", l.next());
+		assertEquals("cc", l.next());
+		assertEquals("cc", l.next());
+		assertEquals("c", l.next());
+		assertFalse(l.hasNext());
 	}
 }
