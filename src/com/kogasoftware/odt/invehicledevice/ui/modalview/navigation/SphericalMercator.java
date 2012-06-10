6ebffb55be3dbf140fdf10b48a2cb09dd9957696
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-11 01:40:44 +0900

    sphericalメルカトルの変換処理

---
+++ src/com/kogasoftware/odt/invehicledevice/ui/modalview/navigation/SphericalMercator.java
@@ -0,0 +1,29 @@
+package com.kogasoftware.odt.invehicledevice.ui.modalview.navigation;
+
+/**
+ * @see http://wiki.openstreetmap.org/wiki/Mercator#Java
+ */
+public class SphericalMercator {
+	/**
+	 * SphericalMercatorの、概ね-180から+180の値を、-85から+85程度の緯度へ変換
+	 * 
+	 * @param aY
+	 * @return
+	 */
+	public static double y2lat(double aY) {
+		return Math.toDegrees(2 * Math.atan(Math.exp(Math.toRadians(aY)))
+				- Math.PI / 2);
+	}
+
+	/**
+	 * 緯度（-90から+90）からSphericalMercatorの、概ね-180から+180の値に変換
+	 * ただし、緯度の範囲は-85から+85くらいを指定しないと利用可能な値が得られない
+	 * 
+	 * @param aLat
+	 * @return
+	 */
+	public static double lat2y(double aLat) {
+		return Math.toDegrees(Math.log(Math.tan(Math.PI / 4
+				+ Math.toRadians(aLat) / 2)));
+	}
+}
---
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/navigation/SphericalMercatorTestCase.java
@@ -0,0 +1,16 @@
+package com.kogasoftware.odt.invehicledevice.test.unit.ui.modalview.navigation;
+
+import junit.framework.TestCase;
+import android.util.Log;
+
+import com.kogasoftware.odt.invehicledevice.ui.modalview.navigation.SphericalMercator;
+
+public class SphericalMercatorTestCase extends TestCase {
+	public void test() {
+		double lat = 35.658517; // 渋谷
+		Log.w("SMTC", lat + " -> " + SphericalMercator.lat2y(lat));
+		// for (double i = -90; i < 90; i += 0.5) {
+		// Log.w("SMTC", i + " -> " + SphericalMercator.lat2y(i));
+		// }
+	}
+}
