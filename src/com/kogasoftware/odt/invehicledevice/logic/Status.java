0f42ba516c779f6480594b54f5cc74727a616bc1
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-13 07:01:40 +0900

    serialVersionUID変更

--- src/com/kogasoftware/odt/invehicledevice/logic/Status.java
+++ src/com/kogasoftware/odt/invehicledevice/logic/Status.java
@@ -24,8 +24,8 @@ public class Status implements Serializable {
 		DRIVE, FINISH, INITIAL, PLATFORM
 	}
 
-	private static final long serialVersionUID = 5617948559888181L;
-	
+	private static final long serialVersionUID = 561788559888181L;
+
 	public final Semaphore operationScheduleInitializedSign = new Semaphore(0); // パーミットが0以上の場合は初期化済み。0以上になるまで待つためにacquireしたら必ずreleaseする。CountDownLatchがSerializableではないためこれを使用
 	public Date updatedDate = new Date();
 	public File file = new EmptyFile();
