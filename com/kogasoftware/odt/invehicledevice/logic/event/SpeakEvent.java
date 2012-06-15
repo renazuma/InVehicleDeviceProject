c6919c05be524372f78fa9ec4ea1caaf9a6ef3f8
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-15 09:40:03 +0900

    不要ファイル削除

--- com/kogasoftware/odt/invehicledevice/logic/event/SpeakEvent.java
+++
@@ -1,9 +0,0 @@
-package com.kogasoftware.odt.invehicledevice.logic.event;
-
-public class SpeakEvent {
-	public final String message;
-
-	public SpeakEvent(String message) {
-		this.message = message;
-	}
-}
