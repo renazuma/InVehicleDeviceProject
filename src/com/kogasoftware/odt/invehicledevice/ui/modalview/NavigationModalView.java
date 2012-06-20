c27b40d2c8725c0a625890b577193edddde4bf6a
Hisako Uchida <hisako_uchida@kogasoftware.com>
2012-06-20 16:49:10 +0900

    refs #4606 車載器アプリ 地図画面に次の乗降場、到着時間を表示

--- src/com/kogasoftware/odt/invehicledevice/ui/modalview/NavigationModalView.java
+++ src/com/kogasoftware/odt/invehicledevice/ui/modalview/NavigationModalView.java
@@ -1,24 +1,39 @@
 package com.kogasoftware.odt.invehicledevice.ui.modalview;
 
 import java.lang.ref.WeakReference;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import android.content.Context;
 import android.graphics.Color;
 import android.opengl.GLSurfaceView;
+import android.text.Html;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.FrameLayout;
 import android.widget.ToggleButton;
 
 import com.google.common.eventbus.Subscribe;
 import com.kogasoftware.odt.invehicledevice.R;
+import com.kogasoftware.odt.invehicledevice.logic.CommonLogic;
 import com.kogasoftware.odt.invehicledevice.logic.event.CommonLogicLoadCompleteEvent;
+import com.kogasoftware.odt.invehicledevice.logic.event.EnterDrivePhaseEvent;
+import com.kogasoftware.odt.invehicledevice.logic.event.EnterFinishPhaseEvent;
+import com.kogasoftware.odt.invehicledevice.logic.event.EnterPlatformPhaseEvent;
 import com.kogasoftware.odt.invehicledevice.logic.event.MapZoomLevelChangedEvent;
+import com.kogasoftware.odt.invehicledevice.logic.event.SpeakEvent;
+import com.kogasoftware.odt.invehicledevice.ui.arrayadapter.ReservationArrayAdapter;
 import com.kogasoftware.odt.invehicledevice.ui.modalview.navigation.NavigationRenderer;
 import com.kogasoftware.odt.invehicledevice.ui.modalview.navigation.tilepipeline.TilePipeline;
+import com.kogasoftware.odt.webapi.model.OperationSchedule;
+import com.kogasoftware.odt.webapi.model.Platform;
+import com.kogasoftware.odt.invehicledevice.logic.Status;
 
 public class NavigationModalView extends ModalView {
 	public static class ShowEvent {
@@ -130,6 +145,7 @@ public class NavigationModalView extends ModalView {
 		autoZoomButton.setChecked(true);
 		setAutoZoom(true);
 		updateZoomButtons();
+
 	}
 
 	public void onPauseActivity() {
@@ -146,6 +162,7 @@ public class NavigationModalView extends ModalView {
 
 		FrameLayout navigationSurfaceParent = (FrameLayout) findViewById(R.id.navigation_surface_parent);
 		navigationSurfaceParent.removeAllViews();
+
 	}
 
 	public void onResumeActivity() {
@@ -178,6 +195,7 @@ public class NavigationModalView extends ModalView {
 		getCommonLogic().registerEventListener(navigationRenderer);
 		navigationRenderer.setCommonLogic(getCommonLogic());
 		navigationRenderer.onResumeActivity();
+
 	}
 
 	@Override
@@ -189,6 +207,7 @@ public class NavigationModalView extends ModalView {
 			getCommonLogic().registerEventListener(navigationRenderer);
 			navigationRenderer.setCommonLogic(getCommonLogic());
 		}
+
 	}
 
 	@Subscribe
@@ -205,6 +224,53 @@ public class NavigationModalView extends ModalView {
 		}
 		// TODO:アニメーション
 		// super.show();
+
+		//次の乗降場表示
+		TextView titleTextView = (TextView) findViewById(R.id.next_platform_text_view);
+		CommonLogic commonLogic = getCommonLogic();
+		List<OperationSchedule> operationSchedules = commonLogic
+				.getRemainingOperationSchedules();
+		if (operationSchedules.isEmpty()) {
+			commonLogic.postEvent(new EnterFinishPhaseEvent());
+			titleTextView.setText("");
+		}
+
+		OperationSchedule operationSchedule = operationSchedules.get(0);
+
+		String titleTextFormat = "";
+		String timeTextFormat = "";
+		Date displayDate = new Date();
+		switch (commonLogic.getPhase()) {
+		case DRIVE:
+			titleTextFormat = getResources().getString(R.string.next_platform_is_html);
+			timeTextFormat = getResources().getString(R.string.platform_arrival_time);
+			displayDate = operationSchedule.getArrivalEstimate();
+			break;
+		default:
+			titleTextFormat = getResources().getString(R.string.now_platform_is_html);
+			timeTextFormat = getResources().getString(R.string.platform_depature_time);
+			displayDate = operationSchedule.getDepartureEstimate();
+			break;
+		}
+
+		if (operationSchedules.size() > 0) {
+			OperationSchedule nowOperationSchedule = operationSchedules.get(0);
+			for (Platform platform : nowOperationSchedule.getPlatform()
+					.asSet()) {
+				titleTextView.setText(Html.fromHtml(String.format(
+						titleTextFormat,
+						platform.getName())));
+			}
+		} else {
+			titleTextView.setText("");
+		}
+
+		//到着時刻表示
+		TextView  platformArrivalTimeTextView = (TextView) findViewById(R.id.platform_arrival_time_view);
+		DateFormat dateFormat = new SimpleDateFormat(timeTextFormat);
+
+		platformArrivalTimeTextView.setText(dateFormat.format(displayDate));
+
 	}
 
 	@Override
