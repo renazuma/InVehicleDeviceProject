6e5e9e0b3f83308994c22ff5b8f85e4728a24a9b
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-25 17:56:54 +0900

    定数をオペレーターツールにあわせて修正

--- src/com/kogasoftware/odt/webapi/model/VehicleNotifications.java
+++ src/com/kogasoftware/odt/webapi/model/VehicleNotifications.java
@@ -2,12 +2,12 @@ package com.kogasoftware.odt.webapi.model;
 
 public class VehicleNotifications {
 	public static class NotificationKind {
-		public static final Integer NORMAL = 0;
-		public static final Integer SCHEDULE_CHANGED = 1;
+		public static final Integer FROM_OPERATOR = 0;
+		public static final Integer RESERVATION_CHANGED = 1;
 	}
 
 	public static class Response {
-		public static final Integer NO = 0;
-		public static final Integer YES = 1;
+		public static final Integer YES = 0;
+		public static final Integer NO = 1;
 	}
 }
