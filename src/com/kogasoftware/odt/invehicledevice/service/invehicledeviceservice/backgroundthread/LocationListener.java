d1af503666cfde57ce79f1715145ba48360fdc21
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-06-26 14:43:15 +0900

    地図ロイド作者の記事を参考に、GPSの取得方法を修正

--- src/com/kogasoftware/odt/invehicledevice/service/invehicledeviceservice/backgroundthread/LocationListener.java
+++ src/com/kogasoftware/odt/invehicledevice/service/invehicledeviceservice/backgroundthread/LocationListener.java
@@ -7,14 +7,23 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
 
+/**
+ * 位置情報を取得する
+ * 
+ * @see http://kamoland.com/wiki/wiki.cgi?Desire%A4%CEGPS%BC%E8%C6%C0%A4%C7%A4%CE%BB%EE%B9%D4%BA%F8%B8%ED
+ */
 public class LocationListener implements android.location.LocationListener,
 		InVehicleDeviceService.OnPauseActivityListener,
 		InVehicleDeviceService.OnResumeActivityListener {
 	private final InVehicleDeviceService service;
 	private final LocationManager locationManager;
+	private final PowerManager powerManager;
+	private final WakeLock wakeLock;
 	private final AtomicBoolean started = new AtomicBoolean(false);
 	private final Handler handler = new Handler();
 	private final long handlerThreadId = handler.getLooper().getThread()
@@ -24,6 +33,10 @@ public class LocationListener implements android.location.LocationListener,
 		this.service = service;
 		locationManager = (LocationManager) service
 				.getSystemService(Context.LOCATION_SERVICE);
+		powerManager = (PowerManager) service
+				.getSystemService(Context.POWER_SERVICE);
+		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+				getClass().getName());
 		service.addOnPauseActivityListener(this);
 		service.addOnResumeActivityListener(this);
 	}
@@ -41,7 +54,14 @@ public class LocationListener implements android.location.LocationListener,
 		if (started.getAndSet(true)) {
 			return;
 		}
+
+		// GPS使用開始時．PARTIAL_WAKE_LOCKを取得する
+		wakeLock.acquire();
+
+		// 試しにgetLastKnownLocationしてみる
 		locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+		// 位置検索を始動する
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
 				1000, 1, this);
 	}
@@ -59,7 +79,10 @@ public class LocationListener implements android.location.LocationListener,
 		if (!started.getAndSet(false)) {
 			return;
 		}
+
+		// GPS使用終了時．PARTIAL_WAKE_LOCKを解放する
 		locationManager.removeUpdates(this);
+		wakeLock.release();
 	}
 
 	@Override
