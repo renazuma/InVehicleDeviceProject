1990ba28bc76dd2ce6a76769bc7a87c46cd021d6
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-07-01 08:40:13 +0900

    google playのクラッシュレポートが、ユーザーの操作を必要とするため、 自動で送るライブラリを導入

--- src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceApplication.java
+++ src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceApplication.java
@@ -1,16 +1,21 @@
 package com.kogasoftware.odt.invehicledevice;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
 import android.app.Application;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
 
+@ReportsCrashes(formKey = "dGVjZDhsb3JLVXctSlJXWV9vU1FHSWc6MQ")
 public class InVehicleDeviceApplication extends Application {
 	private static final String TAG = InVehicleDeviceApplication.class
 			.getSimpleName();
 
 	@Override
 	public void onCreate() {
+		ACRA.init(this);
 		super.onCreate();
 		Log.i(TAG, "onCreate()");
 		try {
