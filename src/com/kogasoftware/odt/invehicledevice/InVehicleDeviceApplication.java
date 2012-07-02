9f775eb7f8f77583e16efe0529ea42708b0d8ccc
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-07-02 13:53:18 +0900

    例外が発生する項目を削除

--- src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceApplication.java
+++ src/com/kogasoftware/odt/invehicledevice/InVehicleDeviceApplication.java
@@ -1,6 +1,7 @@
 package com.kogasoftware.odt.invehicledevice;
 
 import org.acra.ACRA;
+import org.acra.ReportField;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
@@ -9,8 +10,22 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
 
-@ReportsCrashes(formKey = "dFp5SnVVbTRuem13WmJ0YlVUb2NjaXc6MQ", mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text, logcatArguments = {
-		"-t", "1000", "-v", "time" }, includeDropBoxSystemTags = true)
+@ReportsCrashes(formKey = "dFp5SnVVbTRuem13WmJ0YlVUb2NjaXc6MQ", mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text, customReportContent = {
+		ReportField.ANDROID_VERSION, ReportField.APP_VERSION_CODE,
+		ReportField.APP_VERSION_NAME, ReportField.AVAILABLE_MEM_SIZE,
+		ReportField.BRAND, ReportField.BUILD, ReportField.CRASH_CONFIGURATION,
+		ReportField.CUSTOM_DATA, ReportField.DEVICE_ID, ReportField.DISPLAY,
+		ReportField.DROPBOX, ReportField.ENVIRONMENT, ReportField.EVENTSLOG,
+		ReportField.FILE_PATH, ReportField.INITIAL_CONFIGURATION,
+		ReportField.INSTALLATION_ID, ReportField.IS_SILENT, ReportField.LOGCAT,
+		ReportField.PACKAGE_NAME, ReportField.PHONE_MODEL, ReportField.PRODUCT,
+		ReportField.RADIOLOG, ReportField.REPORT_ID,
+		ReportField.SETTINGS_SECURE, ReportField.SETTINGS_SYSTEM,
+		ReportField.SHARED_PREFERENCES, ReportField.STACK_TRACE,
+		ReportField.TOTAL_MEM_SIZE, ReportField.USER_APP_START_DATE,
+		ReportField.USER_COMMENT, ReportField.USER_CRASH_DATE,
+		ReportField.USER_EMAIL, }, logcatArguments = { "-t", "1000", "-v",
+		"time" }, includeDropBoxSystemTags = true)
 public class InVehicleDeviceApplication extends Application {
 	private static final String TAG = InVehicleDeviceApplication.class
 			.getSimpleName();
