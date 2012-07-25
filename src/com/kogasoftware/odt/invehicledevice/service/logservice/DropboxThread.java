4c9bf495937b7c7f0400f0f7ce8b45cc97ca2dbf
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-07-26 02:22:22 +0900

    DropBoxManagerのタグを追加

--- src/com/kogasoftware/odt/invehicledevice/service/logservice/DropboxThread.java
+++ src/com/kogasoftware/odt/invehicledevice/service/logservice/DropboxThread.java
@@ -19,6 +19,12 @@ import android.util.Log;
 public class DropBoxThread extends LogCollectorThread {
 	private static final String LAST_CHECKED_DATE_KEY = "last_checked_date_key";
 	private static final String TAG = DropBoxThread.class.getSimpleName();
+	private static final String[] DROPBOX_TAGS = { "APANIC_CONSOLE",
+			"APANIC_THREADS", "BATTERY_DISCHARGE_INFO", "SYSTEM_BOOT",
+			"SYSTEM_LAST_KMSG", "SYSTEM_RECOVERY_LOG", "SYSTEM_RESTART",
+			"SYSTEM_TOMBSTONE", "data_app_anr", "data_app_strictmode",
+			"system_app_anr", "system_app_crash", "system_app_wtf",
+			"system_server_anr", "system_server_crash", "system_server_wtf", };
 	private final DropBoxManager dropBoxManager;
 	private final SharedPreferences sharedPreferences;
 
@@ -43,8 +49,9 @@ public class DropBoxThread extends LogCollectorThread {
 				break;
 			}
 			try {
-				pipedOutputStream.write(("===== TAG: " + entry.getTag() + " =====\n")
-						.getBytes(Charsets.UTF_8));
+				pipedOutputStream
+						.write(("===== TAG: " + entry.getTag() + " =====\n")
+								.getBytes(Charsets.UTF_8));
 				InputStream inputStream = entry.getInputStream();
 				if (inputStream != null) {
 					ByteStreams.copy(inputStream, pipedOutputStream);
@@ -69,13 +76,11 @@ public class DropBoxThread extends LogCollectorThread {
 		try {
 			while (true) {
 				Date nextCheckDate = new Date();
-				for (String tag : new String[] { "system_app_anr",
-						"SYSTEM_RESTART" }) {
+				for (String tag : DROPBOX_TAGS) {
 					save(tag, lastCheckDate, nextCheckDate);
 					Thread.sleep(1000);
 				}
-				pipedOutputStream.flush();
-
+				
 				lastCheckDate = nextCheckDate;
 				flush();
 				sharedPreferences
@@ -85,8 +90,6 @@ public class DropBoxThread extends LogCollectorThread {
 				Thread.sleep(10 * 1000);
 			}
 		} catch (InterruptedException e) {
-		} catch (IOException e) {
-			Log.w(TAG, e);
 		}
 	}
 }
