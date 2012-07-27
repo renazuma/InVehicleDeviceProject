99dfae514aa56b87de2d7eb8c0632a397e4f9ae6
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-07-27 15:07:03 +0900

    アップロードに失敗したら認証情報を読みなおすようにした

--- src/com/kogasoftware/odt/invehicledevice/service/logservice/UploadThread.java
+++ src/com/kogasoftware/odt/invehicledevice/service/logservice/UploadThread.java
@@ -11,6 +11,7 @@ import android.util.Log;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
@@ -21,6 +22,7 @@ public class UploadThread extends Thread {
 	private final Context context;
 	private final BlockingQueue<File> compressedLogFiles;
 	private final String deviceId;
+	private final String bucket = "odt-android";
 
 	public UploadThread(Context context, File dataDirectory,
 			BlockingQueue<File> compressedLogFiles) {
@@ -31,48 +33,62 @@ public class UploadThread extends Thread {
 		deviceId = telephonyManager.getDeviceId(); // TODO
 	}
 
-	@Override
-	public void run() {
-		Log.i(TAG, "start");
+	private AWSCredentials getAWSCredentials() {
 		SharedPreferences preferences = PreferenceManager
 				.getDefaultSharedPreferences(context);
 		String accessKeyId = preferences.getString(
 				SharedPreferencesKey.AWS_ACCESS_KEY_ID, "");
 		String secretAccessKey = preferences.getString(
 				SharedPreferencesKey.AWS_SECRET_ACCESS_KEY, "");
-		String bucket = "odt-android";
-		
-		AmazonS3Client s3Client = new AmazonS3Client(new BasicAWSCredentials(
-				accessKeyId, secretAccessKey));
+		return new BasicAWSCredentials(accessKeyId, secretAccessKey);
+	}
+
+	private void uploadOneFile(AmazonS3Client s3Client)
+			throws InterruptedException {
+		Thread.sleep(5000);
+		File compressedLogFile = compressedLogFiles.take();
+		if (!compressedLogFile.exists()) {
+			Log.w(TAG, "compressedLogFile(" + compressedLogFile + ") not found");
+			return;
+		}
+		Boolean succeed = false;
 		try {
-			while (true) {
-				Thread.sleep(5000);
-				File compressedLogFile = compressedLogFiles.take();
-				if (!compressedLogFile.exists()) {
-					Log.w(TAG, "compressedLogFile(" + compressedLogFile + ") not found" );
-					continue;
-				}
-				Boolean succeed = false;
-				try {
-					PutObjectRequest putObjectRequest = new PutObjectRequest(
-							bucket, "log/" + deviceId + "_" + compressedLogFile.getName(),
-							compressedLogFile);
-					s3Client.putObject(putObjectRequest);
-					succeed = true;
-				} catch (AmazonServiceException e) {
-					Log.w(TAG, e);
-				} catch (AmazonClientException e) {
-					Log.w(TAG, e);
-				}
-				if (succeed) {
-					compressedLogFile.delete();
-				} else {
-					compressedLogFiles.add(compressedLogFile);
-				}
-			}
-		} catch (InterruptedException e) {
+			PutObjectRequest putObjectRequest = new PutObjectRequest(bucket,
+					"log/" + deviceId + "_" + compressedLogFile.getName(),
+					compressedLogFile);
+			s3Client.putObject(putObjectRequest);
+			succeed = true;
 		} finally {
-			s3Client.shutdown();
+			if (succeed) {
+				compressedLogFile.delete();
+			} else {
+				compressedLogFiles.add(compressedLogFile);
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		Log.i(TAG, "start");
+		while (true) {
+			try {
+				Thread.sleep(5000);
+				AmazonS3Client s3Client = new AmazonS3Client(
+						getAWSCredentials());
+				try {
+					while (true) {
+						uploadOneFile(s3Client);
+					}
+				} finally {
+					s3Client.shutdown();
+				}
+			} catch (InterruptedException e) {
+				break;
+			} catch (AmazonServiceException e) {
+				Log.w(TAG, e);
+			} catch (AmazonClientException e) {
+				Log.w(TAG, e);
+			}
 		}
 		Log.i(TAG, "exit");
 	}
