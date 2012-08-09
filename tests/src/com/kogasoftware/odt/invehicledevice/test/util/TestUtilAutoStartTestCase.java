92b61b9ceaacc2ba6f2b54cbf545fdb123d96bb9
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-08-09 14:46:02 +0900

    自動起動と、起動チェックのテストクラスを分割

---
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/util/TestUtilAutoStartTestCase.java
@@ -0,0 +1,69 @@
+package com.kogasoftware.odt.invehicledevice.test.util;
+
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
+
+import com.kogasoftware.odt.invehicledevice.service.startupservice.StartupService;
+import com.kogasoftware.odt.invehicledevice.ui.activity.InVehicleDeviceActivity;
+
+public class TestUtilAutoStartTestCase extends
+		ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
+
+	private static final String TAG = TestUtilAutoStartTestCase.class.getSimpleName();
+	Context c;
+
+	public TestUtilAutoStartTestCase() {
+		super("com.kogasoftware.odt.invehicledevice",
+				InVehicleDeviceActivity.class);
+	}
+
+	public void setUp() throws Exception {
+		super.setUp();
+		c = getInstrumentation().getContext();
+	}
+
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void callTestDisableAutoStart() throws Exception {
+		// ActivityのUI起動後、自動起動をOFFにしてActivityを終了
+		InVehicleDeviceActivity a = getActivity();
+		TestUtil.waitForStartUI(a);
+		TestUtil.disableAutoStart(c);
+		a.finish();
+		a = null;
+		Thread.sleep(5 * 1000);
+
+		// 最前面のActivityが車載器の場合失敗
+		if (TestUtil.getTopActivity(c).getClassName()
+				.equals(InVehicleDeviceActivity.class.getName())) {
+			fail();
+		}
+
+		// 自動起動をON
+		TestUtil.enableAutoStart(c);
+		Thread.sleep(StartupService.CHECK_DEVICE_INTERVAL_MILLIS);
+		Thread.sleep(5 * 1000);
+
+		// 最前面のActivityが車載器の場合成功
+		if (TestUtil.getTopActivity(c).getClassName()
+				.equals(InVehicleDeviceActivity.class.getName())) {
+			TestUtil.disableAutoStart(c);
+			sendKeys(KeyEvent.KEYCODE_HOME);
+			Thread.sleep(10 * 1000);
+			return;
+		}
+
+		fail();
+	}
+
+	public void testDisableAutoStart1() throws Exception {
+		callTestDisableAutoStart();
+	}
+
+	public void testDisableAutoStart2() throws Exception {
+		callTestDisableAutoStart();
+	}
+}
--- tests/src/com/kogasoftware/odt/invehicledevice/test/util/TestUtilTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/util/TestUtilTestCase.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
-import android.view.KeyEvent;
 
 import com.kogasoftware.odt.invehicledevice.datasource.DataSource;
-import com.kogasoftware.odt.invehicledevice.service.startupservice.StartupService;
 import com.kogasoftware.odt.invehicledevice.test.util.datasource.DummyDataSource;
 import com.kogasoftware.odt.invehicledevice.ui.activity.InVehicleDeviceActivity;
 import com.kogasoftware.odt.webapi.WebAPIException;
@@ -17,31 +15,32 @@ public class TestUtilTestCase extends
 		ActivityInstrumentationTestCase2<InVehicleDeviceActivity> {
 
 	private static final String TAG = TestUtilTestCase.class.getSimpleName();
+
 	InVehicleDeviceActivity a;
+	Context c;
+	Context tc;
 
 	public TestUtilTestCase() {
 		super("com.kogasoftware.odt.invehicledevice",
 				InVehicleDeviceActivity.class);
 	}
 
+	@Override
 	public void setUp() throws Exception {
 		super.setUp();
-		TestUtil.disableAutoStart(getInstrumentation().getContext());
-		Thread.sleep(10 * 1000);
+		c = getInstrumentation().getContext();
+		tc = getInstrumentation().getTargetContext();
+		TestUtil.disableAutoStart(c);
 	}
 
+	@Override
 	public void tearDown() throws Exception {
-		try {
-			if (a != null) {
-				a.finish();
-			}
-			a = null;
-			Context tc = getInstrumentation().getTargetContext();
-			TestUtil.disableAutoStart(tc);
-			TestUtil.exitService(tc);
-		} finally {
-			super.tearDown();
+		if (a != null) {
+			a.finish();
+			Thread.sleep(InVehicleDeviceActivity.PAUSE_FINISH_TIMEOUT_MILLIS);
+			Thread.sleep(3 * 1000);
 		}
+		super.tearDown();
 	}
 
 	public void callTestWaitForStartUi(Boolean timeout) throws Exception {
@@ -78,46 +77,19 @@ public class TestUtilTestCase extends
 		callTestWaitForStartUi(true);
 	}
 
-	public void callTestDisableAutoStart() throws Exception {
-		Context c = getInstrumentation().getContext();
-
-		// ActivityのUI起動後、自動起動をOFFにしてActivityを終了
-		a = getActivity();
-		TestUtil.waitForStartUI(a);
-		TestUtil.disableAutoStart(c);
-		a.finish();
-		a.moveTaskToBack(true);
-		a = null;
-		Thread.sleep(5000);
-
-		// 最前面のActivityが車載器の場合失敗
-		if (TestUtil.getTopActivity(c).getClassName()
-				.equals(InVehicleDeviceActivity.class.getName())) {
-			fail();
-		}
-
-		// 自動起動をON
-		TestUtil.enableAutoStart(c);
-		Thread.sleep(StartupService.CHECK_DEVICE_INTERVAL_MILLIS);
-		Thread.sleep(5 * 1000);
-
-		// 最前面のActivityが車載器の場合成功
-		if (TestUtil.getTopActivity(c).getClassName()
-				.equals(InVehicleDeviceActivity.class.getName())) {
-			TestUtil.disableAutoStart(c);
-			sendKeys(KeyEvent.KEYCODE_HOME);
-			Thread.sleep(10 * 1000);
-			return;
-		}
-
-		fail();
+	public void testWaitForStartUi3() throws Exception {
+		callTestWaitForStartUi(false);
 	}
 
-	public void testDisableAutoStart1() throws Exception {
-		callTestDisableAutoStart();
+	public void testWaitForStartUi3Timeout() throws Exception {
+		callTestWaitForStartUi(true);
 	}
 
-	public void testDisableAutoStart2() throws Exception {
-		callTestDisableAutoStart();
+	public void testWaitForStartUi4() throws Exception {
+		callTestWaitForStartUi(false);
+	}
+
+	public void testWaitForStartUi4Timeout() throws Exception {
+		callTestWaitForStartUi(true);
 	}
 }
