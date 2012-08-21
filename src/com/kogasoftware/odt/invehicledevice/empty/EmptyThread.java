3bcc37a2676a9bbf423462f5481e29f5c952b089
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-08-21 18:21:47 +0900

    GCされないことがあったのを修正

--- src/com/kogasoftware/odt/invehicledevice/empty/EmptyThread.java
+++ src/com/kogasoftware/odt/invehicledevice/empty/EmptyThread.java
@@ -2,6 +2,16 @@ package com.kogasoftware.odt.invehicledevice.empty;
 
 /** FindBugs警告避け用クラス */
 public class EmptyThread extends Thread {
+	public EmptyThread() {
+		super();
+		super.start(); // Android2.3ではstart()していないThreadはGCされないようなので、start()しておく
+	}
+	
+	@Override
+	public void start() {
+		// コンストラクタでsuper.start()が呼ばれているため、二重起動を防ぐため親のメソッドを呼ばないようにする
+	}
+	
 	@Override
 	public void run() {
 	}
