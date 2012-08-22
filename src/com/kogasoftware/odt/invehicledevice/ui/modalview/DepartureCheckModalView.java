dd763dd14c50c704984fda2f5d9483a882cae8d6
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-08-22 16:28:47 +0900

    エラー時に、ユーザーがそれに気づかず先に進むのを防止するため、エラー時と通常時で先に進むボタンの位置を変えるようにした

--- src/com/kogasoftware/odt/invehicledevice/ui/modalview/DepartureCheckModalView.java
+++ src/com/kogasoftware/odt/invehicledevice/ui/modalview/DepartureCheckModalView.java
@@ -4,14 +4,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 
 import com.kogasoftware.odt.invehicledevice.R;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
@@ -29,8 +24,6 @@ public class DepartureCheckModalView extends ModalView {
 		}
 	}
 
-	private final AlphaAnimation animation = new AlphaAnimation(1, 0.3f);
-
 	public DepartureCheckModalView(Context context,
 			InVehicleDeviceService service) {
 		super(context, service);
@@ -39,76 +32,62 @@ public class DepartureCheckModalView extends ModalView {
 	}
 
 	public void show() {
-		Button startButton = (Button) findViewById(R.id.departure_button);
-		Button closeButton = (Button) findViewById(R.id.departure_check_close_button);
-		ListView errorReservationListView = ((FlickUnneededListView) findViewById(R.id.error_reservation_list_view))
-				.getListView();
+		Button departureButton = (Button) findViewById(R.id.departure_button);
+		Button departureWithErrorButton = (Button) findViewById(R.id.departure_with_error_button);
+		View departureButtonLayout = findViewById(R.id.departure_button_layout);
+		FlickUnneededListView errorUserListView = (FlickUnneededListView) findViewById(R.id.error_reservation_list_view);
 		List<String> messages = new LinkedList<String>();
 
-		for (PassengerRecord passengerRecord : service.getNoGettingOnPassengerRecords()) {
+		for (PassengerRecord passengerRecord : service
+				.getNoGettingOnPassengerRecords()) {
 			messages.add(" ※ " + getUserName(passengerRecord) + "様が未乗車です");
 		}
 
-		for (PassengerRecord passengerRecord : service.getNoGettingOffPassengerRecords()) {
+		for (PassengerRecord passengerRecord : service
+				.getNoGettingOffPassengerRecords()) {
 			messages.add(" ※ " + getUserName(passengerRecord) + "様が未降車です");
 		}
 
-		for (PassengerRecord passengerRecord : service.getNoPaymentPassengerRecords()) {
+		for (PassengerRecord passengerRecord : service
+				.getNoPaymentPassengerRecords()) {
 			messages.add(" ※ " + getUserName(passengerRecord) + "様が料金未払いです");
 		}
 
-		errorReservationListView.setAdapter(new ArrayAdapter<String>(
-				getContext(), android.R.layout.simple_list_item_1, messages));
+		errorUserListView.getListView().setAdapter(
+				new ArrayAdapter<String>(getContext(),
+						R.layout.error_user_list_row, messages));
 
+		String buttonMessage = "出発する";
 		if (service.getRemainingOperationSchedules().size() <= 1) {
-			startButton.setText("確定する");
-		} else {
-			startButton.setText("出発する");
+			buttonMessage = "確定する";
 		}
 
-		// 警告データが存在するため「やめる」ボタンの形状を変更
-		if (!messages.isEmpty()) {
-			animation.setDuration(1000);
-			animation.setRepeatCount(Animation.INFINITE);
-			animation.setRepeatMode(Animation.REVERSE);
-			closeButton.startAnimation(animation);
-
-			closeButton.setBackgroundColor(Color.parseColor("#66FF66"));
-			closeButton.setTextColor(Color.parseColor("#000000"));
-
-			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-					LinearLayout.LayoutParams.FILL_PARENT,
-					LinearLayout.LayoutParams.FILL_PARENT);
-			lp.weight = 0.5f;
-			lp.setMargins(10, 1, 10, 10);
-			closeButton.setLayoutParams(lp);
-			closeButton.setShadowLayer(1.0f, 1.5f, 1.5f,
-					Color.parseColor("#FFFFFF"));
-
-		} else {
-			closeButton.setBackgroundDrawable(getContext().getResources()
-					.getDrawable(android.R.drawable.btn_default));
-			closeButton.clearAnimation();
-
-			closeButton.setTextColor(Color.BLACK);
-			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-					LinearLayout.LayoutParams.FILL_PARENT,
-					LinearLayout.LayoutParams.FILL_PARENT);
-			lp.setMargins(0, 0, 0, 0);
-
-			lp.weight = 1f;
-			closeButton.setLayoutParams(lp);
-			closeButton.setShadowLayer(0.0f, 0.0f, 0.0f,
-					Color.parseColor("#FFFFFF"));
-		}
-
-		startButton.setOnClickListener(new OnClickListener() {
+		OnClickListener onClickDepartureButtonListener = new OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				service.enterDrivePhase();
 				hide();
 			}
-		});
+		};
+
+		// 警告データが存在するため「やめる」ボタンの形状を変更
+		if (!messages.isEmpty()) {
+			departureButtonLayout.setVisibility(GONE);
+			departureWithErrorButton.setVisibility(VISIBLE);
+			departureWithErrorButton.setEnabled(true);
+			errorUserListView.setVisibility(VISIBLE);
+			departureWithErrorButton
+					.setOnClickListener(onClickDepartureButtonListener);
+			departureWithErrorButton.setText(buttonMessage);
+		} else {
+			departureButtonLayout.setVisibility(VISIBLE);
+			departureWithErrorButton.setText("");
+			departureWithErrorButton.setEnabled(false);
+			errorUserListView.setVisibility(GONE);
+			departureButton.setOnClickListener(onClickDepartureButtonListener);
+			departureButton.setText(buttonMessage);
+		}
+
 		super.show();
 	}
 }
