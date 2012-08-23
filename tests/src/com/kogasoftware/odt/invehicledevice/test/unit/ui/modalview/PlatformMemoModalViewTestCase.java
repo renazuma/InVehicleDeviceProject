348a57a4aefbfb9e0803676dd70518d8f42190ce
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-08-23 16:13:08 +0900

    refs #5118 乗り降り場メモ画面の表示テスト

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/PlatformMemoModalViewTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/PlatformMemoModalViewTestCase.java
@@ -1,33 +1,105 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.ui.modalview;
 
-import static org.mockito.Mockito.mock;
-import android.app.Activity;
+import static org.mockito.Mockito.*;
 
+import com.google.common.base.Optional;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
 import com.kogasoftware.odt.invehicledevice.ui.modalview.PlatformMemoModalView;
+import com.kogasoftware.odt.webapi.model.OperationSchedule;
+import com.kogasoftware.odt.webapi.model.Platform;
 
-public class PlatformMemoModalViewTestCase extends EmptyActivityInstrumentationTestCase2 {
+public class PlatformMemoModalViewTestCase extends
+		EmptyActivityInstrumentationTestCase2 {
 	InVehicleDeviceService s;
 	PlatformMemoModalView mv;
-	Activity a;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		a = getActivity();
 		s = mock(InVehicleDeviceService.class);
 		mv = new PlatformMemoModalView(a, s);
 		runOnUiThreadSync(new Runnable() {
 			@Override
 			public void run() {
-				getActivity().setContentView(mv);
+				a.setContentView(mv);
 			}
 		});
 	}
-	
-	public void test1() {
-		fail("stub!");
+
+	public void testShow_NoOperationSchedule() throws Exception {
+		when(s.getCurrentOperationSchedule()).thenReturn(
+				Optional.<OperationSchedule> absent());
+		runOnUiThreadSync(new Runnable() {
+			@Override
+			public void run() {
+				mv.show();
+			}
+		});
+		assertFalse(mv.isShown());
+	}
+
+	public void testShow_NoPlatform() throws Exception {
+		OperationSchedule os = new OperationSchedule();
+		when(s.getCurrentOperationSchedule()).thenReturn(Optional.of(os));
+		runOnUiThreadSync(new Runnable() {
+			@Override
+			public void run() {
+				mv.show();
+			}
+		});
+		assertFalse(mv.isShown());
+	}
+
+	public void testShow_NoPlatformMemo() throws Exception {
+		OperationSchedule os = new OperationSchedule();
+		os.setPlatform(new Platform());
+		when(s.getCurrentOperationSchedule()).thenReturn(Optional.of(os));
+		runOnUiThreadSync(new Runnable() {
+			@Override
+			public void run() {
+				mv.show();
+			}
+		});
+		assertFalse(mv.isShown());
+	}
+
+	public void testShow_SuccessAndHideButton() throws Exception {
+		callTestShow_Success(true);
+	}
+
+	public void testShow_SuccessAndHideMethod() throws Exception {
+		callTestShow_Success(false);
+	}
+
+	public void callTestShow_Success(Boolean hideButton) throws Exception {
+		OperationSchedule os = new OperationSchedule();
+		Platform p = new Platform();
+		String memo = "こんにちは" + Math.random();
+		p.setMemo(memo);
+		os.setPlatform(p);
+		when(s.getCurrentOperationSchedule()).thenReturn(Optional.of(os));
+		runOnUiThreadSync(new Runnable() {
+			@Override
+			public void run() {
+				mv.show();
+			}
+		});
+		assertTrue(mv.isShown());
+		assertTrue(solo.searchText(memo));
+
+		if (hideButton) {
+			solo.clickOnButton("戻る");
+		} else {
+			runOnUiThreadSync(new Runnable() {
+				@Override
+				public void run() {
+					mv.hide();
+				}
+			});
+		}
+		getInstrumentation().waitForIdleSync();
+		assertFalse(mv.isShown());
 	}
 
 	@Override
