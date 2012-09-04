747d9941bf9edd90241a0dc20eb1ed5e55172cbc
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-09-04 17:28:52 +0900

    ServiceUnitStatusLogSenderテストのひな形追加

---
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/service/invehicledeviceservice/backgroundtask/ServiceUnitStatusLogSenderTestCase.java
@@ -0,0 +1,15 @@
+package com.kogasoftware.odt.invehicledevice.test.unit.service.invehicledeviceservice.backgroundtask;
+
+import android.test.AndroidTestCase;
+
+public class ServiceUnitStatusLogSenderTestCase extends AndroidTestCase {
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+}
