ee877f2c0003c664da8db798ff220f005494f83e
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-09-05 17:03:03 +0900

    ログ処理の結合テストのスタブ

---
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/integration/LogServiceTestCase.java
@@ -0,0 +1,25 @@
+package com.kogasoftware.odt.invehicledevice.test.integration;
+
+import android.test.InstrumentationTestCase;
+
+public class LogServiceTestCase extends InstrumentationTestCase {
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testUploadDropBox() {
+		fail("stub!");
+	}
+	
+	public void testUploadLogcat() {
+		fail("stub!");
+	}
+	
+	public void testUploadACRA() {
+		fail("stub!");
+	}
+}
