2c43af2846ebeacbf23039c066f18ef16d9ad6b7
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-02 11:28:20 +0900

    refs #5595 出発確認画面のテスト

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/DepartureCheckModalViewTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/DepartureCheckModalViewTestCase.java
@@ -1,44 +1,51 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.ui.modalview;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
 
-import android.view.View;
-
+import com.google.common.base.Optional;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.kogasoftware.odt.invehicledevice.R;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalData;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalDataSource;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalDataSource.Writer;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
+import com.kogasoftware.odt.invehicledevice.test.util.TestUtil;
 import com.kogasoftware.odt.invehicledevice.ui.modalview.DepartureCheckModalView;
 import com.kogasoftware.odt.webapi.model.OperationSchedule;
+import com.kogasoftware.odt.webapi.model.PassengerRecord;
 import com.kogasoftware.odt.webapi.model.Platform;
+import com.kogasoftware.odt.webapi.model.User;
 
 public class DepartureCheckModalViewTestCase extends
 		EmptyActivityInstrumentationTestCase2 {
 	InVehicleDeviceService s;
-	LocalDataSource sa;
+	OperationSchedule os;
 	DepartureCheckModalView mv;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		os = new OperationSchedule();
+		Platform p = new Platform();
+		p.setName("乗降場X");
+		os.setPlatform(p);
+
 		s = mock(InVehicleDeviceService.class);
-		sa = new LocalDataSource(a);
+		when(s.getCurrentOperationSchedule()).thenReturn(Optional.of(os));
+
 		mv = new DepartureCheckModalView(a, s);
 
-		sa.withWriteLock(new Writer() { // TODO もっとスマートにする
+		runOnUiThreadSync(new Runnable() {
 			@Override
-			public void write(LocalData status) {
-				OperationSchedule os1 = new OperationSchedule();
-				OperationSchedule os2 = new OperationSchedule();
-				os1.setPlatform(new Platform());
-				os2.setPlatform(new Platform());
-				status.remainingOperationSchedules.clear();
-				status.remainingOperationSchedules.add(os1);
-				status.remainingOperationSchedules.add(os2);
+			public void run() {
+				a.setContentView(mv);
 			}
 		});
 	}
@@ -48,48 +55,130 @@ public class DepartureCheckModalViewTestCase extends
 		super.tearDown();
 	}
 
-	public void xtestEventBusに自動で登録される() throws Exception {
+	private void assertShow() throws Exception {
+		assertFalse(mv.isShown());
+
 		runOnUiThreadSync(new Runnable() {
 			@Override
 			public void run() {
-				a.setContentView(R.layout.in_vehicle_device);
+				mv.show();
 			}
 		});
-		// try {
-		// assertEquals(cl2.countRegisteredClass(DepartureCheckModalView.class)
-		// .intValue(), 1);
-		// } finally {
-		// cl2.dispose();
-		// }
+
+		TestUtil.assertShow(mv);
 	}
 
-	/**
-	 * ShowEventを受け取ると表示される
-	 */
-	public void testShowEvent() throws InterruptedException {
-		assertFalse(mv.isShown());
-		assertNotSame(mv.getVisibility(), View.VISIBLE);
-
-		// cl.postEvent(new DepartureCheckModalView.ShowEvent(
-		// new ReservationArrayAdapter(getInstrumentation()
-		// .getContext(), cl)));
-		getInstrumentation().waitForIdleSync();
-
-		assertTrue(mv.isShown());
-		assertEquals(mv.getVisibility(), View.VISIBLE);
+	public void testShow() throws Exception {
+		assertShow();
 	}
 
-	public void test出発するボタンを押すとEnterDrivePhaseEvent通知() throws Exception {
-		testShowEvent();
-		final CountDownLatch cdl = new CountDownLatch(1);
+	public void testDepart_NoError() throws Exception {
+		assertShow();
+		assertFalse(solo.getView(R.id.departure_with_error_button).isEnabled());
 		solo.clickOnView(solo.getView(R.id.departure_button));
-		cdl.await();
+		TestUtil.assertHide(mv);
+		verify(s, times(1)).enterDrivePhase();
 	}
 
-	public void test戻るボタンを押すと消える() throws Exception {
-		testShowEvent();
+	private void assertDepartHasError(
+			List<PassengerRecord> noGettingOffPassengerRecords,
+			List<PassengerRecord> noGettingOnPassengerRecords,
+			List<PassengerRecord> noPaymentPassengerRecords) throws Exception {
+
+		when(s.getNoGettingOffPassengerRecords()).thenReturn(
+				noGettingOffPassengerRecords);
+		when(s.getNoGettingOnPassengerRecords()).thenReturn(
+				noGettingOnPassengerRecords);
+		when(s.getNoPaymentPassengerRecords()).thenReturn(
+				noPaymentPassengerRecords);
+
+		assertShow();
+
+		for (PassengerRecord pr : Iterables.concat(
+				noGettingOffPassengerRecords, noGettingOnPassengerRecords,
+				noPaymentPassengerRecords)) {
+			for (User u : pr.getUser().asSet()) {
+				if (!u.getFirstName().isEmpty()) {
+					assertTrue(solo.searchText(u.getFirstName(), true));
+				}
+				if (!u.getLastName().isEmpty()) {
+					assertTrue(solo.searchText(u.getLastName(), true));
+				}
+			}
+		}
+
+		assertFalse(solo.getView(R.id.departure_button).isShown());
+		solo.clickOnView(solo.getView(R.id.departure_with_error_button));
+		TestUtil.assertHide(mv);
+		verify(s, times(1)).enterDrivePhase();
+
+	}
+
+	public void testDepart_HasNoGettingOffError() throws Exception {
+		String firstName = "鈴木";
+		String lastName = "三郎";
+
+		PassengerRecord pr1 = new PassengerRecord();
+		User u1 = new User();
+		u1.setFirstName(firstName + UUID.randomUUID());
+		u1.setLastName(lastName + UUID.randomUUID());
+		pr1.setUser(u1);
+
+		PassengerRecord pr2 = new PassengerRecord();
+		User u2 = new User();
+		u2.setFirstName(firstName + UUID.randomUUID());
+		u2.setLastName(lastName + UUID.randomUUID());
+		pr2.setUser(u2);
+
+		assertDepartHasError(Lists.newArrayList(pr1, pr2),
+				new LinkedList<PassengerRecord>(),
+				new LinkedList<PassengerRecord>());
+	}
+
+	public void testDepart_HasNoPaymentError() throws Exception {
+		String firstName = "鈴木";
+		String lastName = "三郎";
+
+		PassengerRecord pr1 = new PassengerRecord();
+		User u1 = new User();
+		u1.setFirstName(firstName + UUID.randomUUID());
+		u1.setLastName(lastName + UUID.randomUUID());
+		pr1.setUser(u1);
+
+		PassengerRecord pr2 = new PassengerRecord();
+		User u2 = new User();
+		u2.setFirstName(firstName + UUID.randomUUID());
+		u2.setLastName(lastName + UUID.randomUUID());
+		pr2.setUser(u2);
+
+		assertDepartHasError(new LinkedList<PassengerRecord>(),
+				new LinkedList<PassengerRecord>(), Lists.newArrayList(pr1, pr2));
+	}
+
+	public void testDepart_HasNoGettingOnError() throws Exception {
+		String firstName = "鈴木";
+		String lastName = "三郎";
+
+		PassengerRecord pr1 = new PassengerRecord();
+		User u1 = new User();
+		u1.setFirstName(firstName + UUID.randomUUID());
+		u1.setLastName(lastName + UUID.randomUUID());
+		pr1.setUser(u1);
+
+		PassengerRecord pr2 = new PassengerRecord();
+		User u2 = new User();
+		u2.setFirstName(firstName + UUID.randomUUID());
+		u2.setLastName(lastName + UUID.randomUUID());
+		pr2.setUser(u2);
+
+		assertDepartHasError(new LinkedList<PassengerRecord>(),
+				Lists.newArrayList(pr1, pr2), new LinkedList<PassengerRecord>());
+	}
+
+	public void testBack() throws Exception {
+		assertShow();
 		solo.clickOnView(solo.getView(R.id.departure_check_close_button));
-		getInstrumentation().waitForIdleSync();
-		assertFalse(mv.isShown());
+		TestUtil.assertHide(mv);
+		verify(s, never()).enterDrivePhase();
 	}
 }
