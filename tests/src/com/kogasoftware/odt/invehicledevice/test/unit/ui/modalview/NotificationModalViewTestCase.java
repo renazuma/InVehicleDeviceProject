4eb1819d4c15c5feafc88d84c38c0796df54a4bf
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-03 17:34:31 +0900

    refs #5667 通知のテスト

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/NotificationModalViewTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/NotificationModalViewTestCase.java
@@ -1,29 +1,69 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.ui.modalview;
 
 import static org.mockito.Mockito.mock;
-import android.view.View;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.google.common.base.Optional;
 import com.kogasoftware.odt.invehicledevice.R;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalData;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalData.VehicleNotificationStatus;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalDataSource;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalDataSource.Writer;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.VehicleNotificationLogic;
 import com.kogasoftware.odt.invehicledevice.test.util.EmptyActivityInstrumentationTestCase2;
+import com.kogasoftware.odt.invehicledevice.test.util.TestUtil;
 import com.kogasoftware.odt.invehicledevice.ui.modalview.NotificationModalView;
 import com.kogasoftware.odt.webapi.model.VehicleNotification;
+import com.kogasoftware.odt.webapi.model.VehicleNotification.NotificationKind;
+import com.kogasoftware.odt.webapi.model.VehicleNotification.Response;
 
 public class NotificationModalViewTestCase extends
 		EmptyActivityInstrumentationTestCase2 {
 	InVehicleDeviceService s;
-	LocalDataSource sa;
 	NotificationModalView mv;
+	VehicleNotificationLogic vnl;
+	LocalDataSource lds;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		lds = new LocalDataSource(a);
+		lds.withWriteLock(new Writer(){
+			@Override
+			public void write(LocalData localData) {
+				localData.vehicleNotifications.clear();
+			}
+		});
 		s = mock(InVehicleDeviceService.class);
-		sa = new LocalDataSource(a);
+		when(s.getLocalDataSource()).thenReturn(lds);
+		vnl = new VehicleNotificationLogic(s);
+		Answer<List<VehicleNotification>> answer = new Answer<List<VehicleNotification>>() {
+			@Override
+			public List<VehicleNotification> answer(InvocationOnMock invocation)
+					throws Throwable {
+				Object[] arguments = invocation.getArguments();
+				return vnl.getVehicleNotifications((Integer) arguments[0],
+						(VehicleNotificationStatus) arguments[1]);
+			}
+		};
+		when(
+				s.getVehicleNotifications(Mockito.anyInt(),
+						Mockito.<VehicleNotificationStatus> any())).thenAnswer(
+				answer);
 		mv = new NotificationModalView(a, s);
+		runOnUiThreadSync(new Runnable() {
+			@Override
+			public void run() {
+				a.setContentView(mv);
+			}
+		});
 	}
 
 	@Override
@@ -31,73 +71,54 @@ public class NotificationModalViewTestCase extends
 		super.tearDown();
 	}
 
-	public void xtestEventBusに自動で登録される() throws Exception {
+	public void assertShow() throws Exception {
+		assertFalse(mv.isShown());
 		runOnUiThreadSync(new Runnable() {
 			@Override
 			public void run() {
-				a.setContentView(R.layout.in_vehicle_device);
+				mv.show();
 			}
 		});
+		TestUtil.assertShow(mv);
 	}
 
-	/**
-	 * ShowEventを受け取ると表示される
-	 */
-	public void testShowEvent_1() throws InterruptedException {
-		final String message = "Hello";
-		sa.withWriteLock(new Writer() { // TODO: イベントで書き直し
-			@Override
-			public void write(LocalData status) {
-				VehicleNotification vn = new VehicleNotification();
-				vn.setBody(message);
-				status.vehicleNotifications.clear();
-				status.vehicleNotifications.add(vn);
-			}
-		});
+	public void testShow1() throws Exception {
+		String body = "Hello!";
+		final VehicleNotification vn = new VehicleNotification();
+		vn.setBody(body);
+		vn.setNotificationKind(NotificationKind.FROM_OPERATOR);
+		vnl.setVehicleNotificationStatus(vn,
+				VehicleNotificationStatus.UNHANDLED);
 
-		assertFalse(mv.isShown());
-		assertNotSame(mv.getVisibility(), View.VISIBLE);
+		assertShow();
 
-//		cl.postEvent(new NotificationModalView.ShowEvent());
-		getInstrumentation().waitForIdleSync();
-
-		assertTrue(mv.isShown());
-		assertEquals(mv.getVisibility(), View.VISIBLE);
-		assertTrue(solo.searchText(message));
+		assertTrue(solo.searchText(body, true));
 	}
 
-	/**
-	 * VehicleNotificationが無い場合ShowEventを受け取っても表示されない
-	 */
-	public void testShowEvent_2() throws InterruptedException {
-		sa.withWriteLock(new Writer() { // TODO: イベントで書き直し
-			@Override
-			public void write(LocalData status) {
-				status.vehicleNotifications.clear();
-			}
-		});
-
-		assertFalse(mv.isShown());
-		assertNotSame(mv.getVisibility(), View.VISIBLE);
-
-//		cl.postEvent(new NotificationModalView.ShowEvent());
-		getInstrumentation().waitForIdleSync();
-
-		assertFalse(mv.isShown());
-		assertNotSame(mv.getVisibility(), View.VISIBLE);
-	}
-
-	public void testいいえボタンを押すと消える() throws Exception {
-		testShowEvent_1();
-		solo.clickOnView(solo.getView(R.id.reply_no_button));
-		getInstrumentation().waitForIdleSync();
-		assertFalse(mv.isShown());
-	}
-
-	public void testはいボタンを押すと消える() throws Exception {
-		testShowEvent_1();
+	public void testReplyYes() throws Exception {
+		String body = "Hello! testReplyYes";
+		final VehicleNotification vn = new VehicleNotification();
+		vn.setBody(body);
+		vn.setNotificationKind(NotificationKind.FROM_OPERATOR);
+		vnl.setVehicleNotificationStatus(vn,
+				VehicleNotificationStatus.UNHANDLED);
+		assertShow();
 		solo.clickOnView(solo.getView(R.id.reply_yes_button));
-		getInstrumentation().waitForIdleSync();
-		assertFalse(mv.isShown());
+		TestUtil.assertHide(mv);
+		assertEquals(Optional.of(Response.YES), vn.getResponse());
+	}
+
+	public void testReplyNo() throws Exception {
+		String body = "Hello! testReplyNo";
+		final VehicleNotification vn = new VehicleNotification();
+		vn.setBody(body);
+		vn.setNotificationKind(NotificationKind.FROM_OPERATOR);
+		vnl.setVehicleNotificationStatus(vn,
+				VehicleNotificationStatus.UNHANDLED);
+		assertShow();
+		solo.clickOnView(solo.getView(R.id.reply_no_button));
+		TestUtil.assertHide(mv);
+		assertEquals(Optional.of(Response.NO), vn.getResponse());
 	}
 }
+
