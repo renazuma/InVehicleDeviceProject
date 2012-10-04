256736c422af74904c13e3bcce0281b05d6dd079
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-04 10:57:20 +0900

    運行終了が考慮されていなかったのを修正

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/DepartureCheckModalViewTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/ui/modalview/DepartureCheckModalViewTestCase.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.UUID;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Iterables;
@@ -26,19 +25,24 @@ import com.kogasoftware.odt.webapi.model.User;
 public class DepartureCheckModalViewTestCase extends
 		EmptyActivityInstrumentationTestCase2 {
 	InVehicleDeviceService s;
-	OperationSchedule os;
+	OperationSchedule os1;
+	OperationSchedule os2;
 	DepartureCheckModalView mv;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		os = new OperationSchedule();
+		os1 = new OperationSchedule();
 		Platform p = new Platform();
 		p.setName("乗降場X");
-		os.setPlatform(p);
+		os1.setPlatform(p);
 
 		s = mock(InVehicleDeviceService.class);
-		when(s.getCurrentOperationSchedule()).thenReturn(Optional.of(os));
+		when(s.getCurrentOperationSchedule()).thenReturn(Optional.of(os1));
+		when(s.getOperationSchedules())
+				.thenReturn(Lists.newArrayList(os1, os2));
+		when(s.getRemainingOperationSchedules()).thenReturn(
+				Lists.newArrayList(os1, os2));
 
 		mv = new DepartureCheckModalView(a, s);
 
@@ -78,6 +82,19 @@ public class DepartureCheckModalViewTestCase extends
 		solo.clickOnView(solo.getView(R.id.departure_button));
 		TestUtil.assertHide(mv);
 		verify(s, times(1)).enterDrivePhase();
+		verify(s, never()).enterFinishPhase();
+	}
+
+	public void testDepart_HasNoError_Finish() throws Exception {
+		when(s.getRemainingOperationSchedules()).thenReturn(
+				Lists.newArrayList(os2));
+
+		assertShow();
+		assertFalse(solo.getView(R.id.departure_with_error_button).isEnabled());
+		solo.clickOnView(solo.getView(R.id.departure_button));
+		TestUtil.assertHide(mv);
+		verify(s, never()).enterDrivePhase();
+		verify(s, times(1)).enterFinishPhase();
 	}
 
 	private void assertDepartHasError(
@@ -110,8 +127,13 @@ public class DepartureCheckModalViewTestCase extends
 		assertFalse(solo.getView(R.id.departure_button).isShown());
 		solo.clickOnView(solo.getView(R.id.departure_with_error_button));
 		TestUtil.assertHide(mv);
-		verify(s, times(1)).enterDrivePhase();
-
+		if (s.getRemainingOperationSchedules().size() > 1) {
+			verify(s, times(1)).enterDrivePhase();
+			verify(s, never()).enterFinishPhase();
+		} else {
+			verify(s, times(1)).enterFinishPhase();
+			verify(s, never()).enterDrivePhase();
+		}
 	}
 
 	public void testDepart_HasNoGettingOffError() throws Exception {
@@ -120,15 +142,39 @@ public class DepartureCheckModalViewTestCase extends
 
 		PassengerRecord pr1 = new PassengerRecord();
 		User u1 = new User();
-		u1.setFirstName(firstName + UUID.randomUUID());
-		u1.setLastName(lastName + UUID.randomUUID());
+		u1.setFirstName(firstName);
+		u1.setLastName(lastName);
 		pr1.setUser(u1);
 
 		PassengerRecord pr2 = new PassengerRecord();
 		User u2 = new User();
-		u2.setFirstName(firstName + UUID.randomUUID());
-		u2.setLastName(lastName + UUID.randomUUID());
+		u2.setFirstName(firstName);
+		u2.setLastName(lastName);
 		pr2.setUser(u2);
+
+		assertDepartHasError(Lists.newArrayList(pr1, pr2),
+				new LinkedList<PassengerRecord>(),
+				new LinkedList<PassengerRecord>());
+	}
+
+	public void testDepart_HasNoGettingOffError_Finish() throws Exception {
+		String firstName = "鈴木";
+		String lastName = "三郎";
+
+		PassengerRecord pr1 = new PassengerRecord();
+		User u1 = new User();
+		u1.setFirstName(firstName);
+		u1.setLastName(lastName);
+		pr1.setUser(u1);
+
+		PassengerRecord pr2 = new PassengerRecord();
+		User u2 = new User();
+		u2.setFirstName(firstName);
+		u2.setLastName(lastName);
+		pr2.setUser(u2);
+
+		when(s.getRemainingOperationSchedules()).thenReturn(
+				Lists.newArrayList(os2));
 
 		assertDepartHasError(Lists.newArrayList(pr1, pr2),
 				new LinkedList<PassengerRecord>(),
@@ -141,14 +187,14 @@ public class DepartureCheckModalViewTestCase extends
 
 		PassengerRecord pr1 = new PassengerRecord();
 		User u1 = new User();
-		u1.setFirstName(firstName + UUID.randomUUID());
-		u1.setLastName(lastName + UUID.randomUUID());
+		u1.setFirstName(firstName);
+		u1.setLastName(lastName);
 		pr1.setUser(u1);
 
 		PassengerRecord pr2 = new PassengerRecord();
 		User u2 = new User();
-		u2.setFirstName(firstName + UUID.randomUUID());
-		u2.setLastName(lastName + UUID.randomUUID());
+		u2.setFirstName(firstName);
+		u2.setLastName(lastName);
 		pr2.setUser(u2);
 
 		assertDepartHasError(new LinkedList<PassengerRecord>(),
@@ -161,24 +207,26 @@ public class DepartureCheckModalViewTestCase extends
 
 		PassengerRecord pr1 = new PassengerRecord();
 		User u1 = new User();
-		u1.setFirstName(firstName + UUID.randomUUID());
-		u1.setLastName(lastName + UUID.randomUUID());
+		u1.setFirstName(firstName);
+		u1.setLastName(lastName);
 		pr1.setUser(u1);
 
 		PassengerRecord pr2 = new PassengerRecord();
 		User u2 = new User();
-		u2.setFirstName(firstName + UUID.randomUUID());
-		u2.setLastName(lastName + UUID.randomUUID());
+		u2.setFirstName(firstName);
+		u2.setLastName(lastName);
 		pr2.setUser(u2);
 
 		assertDepartHasError(new LinkedList<PassengerRecord>(),
 				Lists.newArrayList(pr1, pr2), new LinkedList<PassengerRecord>());
 	}
 
+
 	public void testBack() throws Exception {
 		assertShow();
 		solo.clickOnView(solo.getView(R.id.departure_check_close_button));
 		TestUtil.assertHide(mv);
 		verify(s, never()).enterDrivePhase();
+		verify(s, never()).enterFinishPhase();
 	}
 }
