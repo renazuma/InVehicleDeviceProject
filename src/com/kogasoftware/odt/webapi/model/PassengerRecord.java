bc521764aa0a2b037079d2ec23e52c804e57b37d
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-17 17:37:04 +0900

    未乗車判定を単純化

--- src/com/kogasoftware/odt/webapi/model/PassengerRecord.java
+++ src/com/kogasoftware/odt/webapi/model/PassengerRecord.java
@@ -1,9 +1,5 @@
 package com.kogasoftware.odt.webapi.model;
 
-import org.json.JSONException;
-
-import android.util.Log;
-
 import com.kogasoftware.odt.webapi.model.base.PassengerRecordBase;
 
 public class PassengerRecord extends PassengerRecordBase {
@@ -24,24 +20,9 @@ public class PassengerRecord extends PassengerRecordBase {
 	}
 
 	/**
-	 * 未乗車かどうかを調べる エラー状態。降車済みとして扱うと現在の車載器のUIで操作が不可能なため、未乗車として扱う。
+	 * 未乗車かどうかを調べる。
 	 */
 	public Boolean isUnhandled() {
-		if (getGetOnTime().isPresent()) {
-			return false;
-		}
-		if (!getGetOffTime().isPresent()) {
-			return true;
-		}
-		String jsonString = "";
-		try {
-			jsonString = toJSONObject().toString();
-		} catch (JSONException e) {
-			Log.w(TAG, e);
-		}
-		Log.w(TAG,
-				"PassengerRecord (!getGetOnTime().isPresent() && getGetOffTime().isPresent()) : "
-						+ jsonString);
-		return true;
+		return !getGetOnTime().isPresent();
 	}
 }
