d53f2ea3534478b597d4c5616308c2dc604a5715
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-22 14:28:24 +0900

    外枠の画面のコードを分割

---
+++ src/com/kogasoftware/odt/invehicledevice/ui/fragment/ControlBarFragment.java
@@ -0,0 +1,350 @@
+package com.kogasoftware.odt.invehicledevice.ui.fragment;
+
+import java.io.Serializable;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.common.base.Optional;
+import com.kogasoftware.odt.invehicledevice.R;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.EventDispatcher;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalData;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalData.Phase;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalDataSource.BackgroundReader;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalDataSource.BackgroundWriter;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.OperationScheduleLogic;
+import com.kogasoftware.odt.invehicledevice.ui.ViewDisabler;
+import com.kogasoftware.odt.invehicledevice.ui.fragment.ControlBarFragment.State;
+import com.kogasoftware.odt.webapi.model.OperationSchedule;
+import com.kogasoftware.odt.webapi.model.PassengerRecord;
+
+public class ControlBarFragment extends ApplicationFragment<State> implements
+		EventDispatcher.OnUpdatePhaseListener {
+
+	private static final String TAG = ControlBarFragment.class.getSimpleName();
+
+	@SuppressWarnings("serial")
+	protected static class State implements Serializable {
+	}
+
+	public static ControlBarFragment newInstance() {
+		return newInstance(new ControlBarFragment(), new State());
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.control_bar_fragment, container,
+				false);
+		Button mapButton = (Button) view.findViewById(R.id.map_button);
+		mapButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ViewDisabler.disable(v);
+				showNavigationFragment();
+			}
+		});
+
+		Button operationScheduleListButton = (Button) view
+				.findViewById(R.id.operation_schedule_list_button);
+		operationScheduleListButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ViewDisabler.disable(v);
+				showOperationScheduleListFragment();
+			}
+		});
+
+		getService().getEventDispatcher().addOnUpdatePhaseListener(this);
+		getService().getLocalDataSource().read(
+				new BackgroundReader<Pair<Phase, Boolean>>() {
+					@Override
+					public Pair<Phase, Boolean> readInBackground(
+							LocalData localData) {
+						Phase phase = OperationScheduleLogic.getPhase(
+								localData.operationSchedules,
+								localData.passengerRecords,
+								localData.completeGetOff);
+						Boolean isLast = !OperationSchedule
+								.getRelativeOperationSchedule(
+										localData.operationSchedules, 1)
+								.isPresent();
+						return Pair.create(phase, isLast);
+					}
+
+					@Override
+					public void onRead(Pair<Phase, Boolean> result) {
+						updateView(result.first, result.second);
+					}
+				});
+		return view;
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		getService().getEventDispatcher().removeOnUpdatePhaseListener(this);
+	}
+
+	public void showNavigationFragment() {
+		String tag = "tag:" + NavigationFragment.class.getSimpleName();
+		Fragment old = getFragmentManager().findFragmentByTag(tag);
+		if (old != null) {
+			setCustomAnimation(getFragmentManager().beginTransaction()).remove(
+					old).commit();
+		}
+		getService().getLocalDataSource().read(
+				new BackgroundReader<Pair<Phase, List<OperationSchedule>>>() {
+					@Override
+					public Pair<Phase, List<OperationSchedule>> readInBackground(
+							LocalData localData) {
+						Phase phase = OperationScheduleLogic.getPhase(
+								localData.operationSchedules,
+								localData.passengerRecords,
+								localData.completeGetOff);
+						return Pair.create(phase, localData.operationSchedules);
+					}
+
+					@Override
+					public void onRead(
+							Pair<Phase, List<OperationSchedule>> result) {
+						setCustomAnimation(
+								getFragmentManager().beginTransaction()).add(
+								R.id.modal_fragment_container,
+								NavigationFragment.newInstance(result.first,
+										result.second)).commit();
+					}
+				});
+	}
+
+	public void showOperationScheduleListFragment() {
+		getService().getLocalDataSource().read(
+				new BackgroundReader<List<OperationSchedule>>() {
+					@Override
+					public List<OperationSchedule> readInBackground(
+							LocalData localData) {
+						return localData.operationSchedules;
+					}
+
+					@Override
+					public void onRead(
+							List<OperationSchedule> operationSchedules) {
+						String tag = "tag:"
+								+ OperationScheduleListFragment.class
+										.getSimpleName();
+						Fragment old = getFragmentManager().findFragmentByTag(
+								tag);
+						if (old != null) {
+							setCustomAnimation(
+									getFragmentManager().beginTransaction())
+									.remove(old).commit();
+						}
+						setCustomAnimation(
+								getFragmentManager().beginTransaction()).add(
+								R.id.modal_fragment_container,
+								OperationScheduleListFragment
+										.newInstance(operationSchedules))
+								.commit();
+					}
+				});
+	}
+
+	public void showArrivalCheckFragment() {
+		getService().getLocalDataSource().read(
+				new BackgroundReader<Optional<OperationSchedule>>() {
+					@Override
+					public Optional<OperationSchedule> readInBackground(
+							LocalData localData) {
+						return OperationSchedule
+								.getCurrentOperationSchedule(localData.operationSchedules);
+					}
+
+					@Override
+					public void onRead(Optional<OperationSchedule> result) {
+						for (OperationSchedule operationSchedule : result
+								.asSet()) {
+							String tag = "tag:"
+									+ ArrivalCheckFragment.class
+											.getSimpleName();
+							Fragment old = getFragmentManager()
+									.findFragmentByTag(tag);
+							if (old != null) {
+								setCustomAnimation(
+										getFragmentManager().beginTransaction())
+										.remove(old).commit();
+							}
+							setCustomAnimation(
+									getFragmentManager().beginTransaction())
+									.add(R.id.modal_fragment_container,
+											ArrivalCheckFragment
+													.newInstance(operationSchedule))
+									.commit();
+						}
+					}
+				});
+	}
+
+	public void showDepartureCheckFragment() {
+		getService()
+				.getLocalDataSource()
+				.read(new BackgroundReader<Pair<Phase, Pair<List<OperationSchedule>, List<PassengerRecord>>>>() {
+					@Override
+					public Pair<Phase, Pair<List<OperationSchedule>, List<PassengerRecord>>> readInBackground(
+							LocalData localData) {
+						Phase phase = OperationScheduleLogic.getPhase(
+								localData.operationSchedules,
+								localData.passengerRecords,
+								localData.completeGetOff);
+						return Pair.create(phase, Pair.create(
+								localData.operationSchedules,
+								localData.passengerRecords));
+					}
+
+					@Override
+					public void onRead(
+							Pair<Phase, Pair<List<OperationSchedule>, List<PassengerRecord>>> result) {
+						showDepartureCheckFragment(result.first,
+								result.second.first, result.second.second);
+					}
+				});
+	}
+
+	public void showDepartureCheckFragment(Phase phase,
+			List<OperationSchedule> operationSchedules,
+			List<PassengerRecord> passengerRecords) {
+		if (!OperationSchedule.getCurrentOperationSchedule(operationSchedules)
+				.isPresent()) {
+			Log.e(TAG, "current operation schedule not found");
+			return;
+		}
+		OperationSchedule operationSchedule = OperationSchedule
+				.getCurrentOperationSchedule(operationSchedules).get();
+		final OperationScheduleLogic operationScheduleLogic = new OperationScheduleLogic(
+				getService());
+
+		if (phase == Phase.PLATFORM_GET_OFF
+				&& operationSchedule.getNoGetOffErrorPassengerRecords(
+						passengerRecords).isEmpty()) {
+			if (operationSchedule.getGetOnScheduledPassengerRecords(
+					passengerRecords).isEmpty()) {
+				setCustomAnimation(getFragmentManager().beginTransaction())
+						.add(R.id.modal_fragment_container,
+								DepartureCheckFragment.newInstance(phase,
+										operationSchedules))
+						.commitAllowingStateLoss();
+			} else {
+				getService().getLocalDataSource().write(new BackgroundWriter() {
+					@Override
+					public void writeInBackground(LocalData ld) {
+						ld.completeGetOff = true;
+					}
+
+					@Override
+					public void onWrite() {
+						operationScheduleLogic.requestUpdatePhase();
+					}
+				});
+			}
+			return;
+		} else if (phase == Phase.PLATFORM_GET_ON
+				&& operationSchedule.getNoGetOnErrorPassengerRecords(
+						passengerRecords).isEmpty()) {
+			setCustomAnimation(getFragmentManager().beginTransaction()).add(
+					R.id.modal_fragment_container,
+					DepartureCheckFragment.newInstance(phase,
+							operationSchedules)).commitAllowingStateLoss();
+			return;
+		}
+
+		// エラーがある場合
+		String tag = "tag:"
+				+ PassengerRecordErrorFragment.class.getSimpleName();
+		Fragment old = getFragmentManager().findFragmentByTag(tag);
+		if (old != null) {
+			setCustomAnimation(getFragmentManager().beginTransaction()).remove(
+					old).commitAllowingStateLoss();
+		}
+		setCustomAnimation(getFragmentManager().beginTransaction()).add(
+				R.id.modal_fragment_container,
+				PassengerRecordErrorFragment.newInstance(phase,
+						operationSchedules, passengerRecords))
+				.commitAllowingStateLoss();
+	}
+
+	public void updateView(Phase phase, Boolean isLast) {
+		Button changePhaseButton = (Button) getView().findViewById(
+				R.id.change_phase_button);
+		getView().setBackgroundColor(getPhaseColor(phase));
+		switch (phase) {
+		case DRIVE:
+			changePhaseButton.setEnabled(true);
+			changePhaseButton
+					.setText(getString(R.string.it_arrives_button_text));
+			changePhaseButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					ViewDisabler.disable(v);
+					showArrivalCheckFragment();
+				}
+			});
+			break;
+		case FINISH:
+			changePhaseButton.setEnabled(false);
+			changePhaseButton.setText("");
+			break;
+		case PLATFORM_GET_OFF:
+			changePhaseButton.setEnabled(true);
+			if (isLast) {
+				// changePhaseButton.setText("降車\n完了\n(終)");
+				changePhaseButton.setText("確認\nする");
+				// changePhaseButton
+				// .setText(getString(R.string.it_leaves_last_platform_button_text));
+			} else {
+				changePhaseButton.setText("確認\nする");
+				// changePhaseButton.setText("降車\n完了");
+				// changePhaseButton
+				// .setText(getString(R.string.it_leaves_button_text));
+			}
+			// changePhaseButton
+			// .setText(getString(R.string.it_leaves_last_platform_button_text));
+			changePhaseButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					ViewDisabler.disable(v);
+					showDepartureCheckFragment();
+				}
+			});
+			break;
+		case PLATFORM_GET_ON:
+			changePhaseButton.setEnabled(true);
+			changePhaseButton.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					ViewDisabler.disable(v);
+					showDepartureCheckFragment();
+				}
+			});
+			// changePhaseButton.setText("乗車\n完了");
+			changePhaseButton.setText("確認\nする");
+			break;
+		}
+	}
+
+	@Override
+	public void onUpdatePhase(Phase phase,
+			List<OperationSchedule> operationSchedules,
+			List<PassengerRecord> passengerRecords) {
+		updateView(
+				phase,
+				!OperationSchedule.getRelativeOperationSchedule(
+						operationSchedules, 1).isPresent());
+	}
+}
---
+++ src/com/kogasoftware/odt/invehicledevice/ui/fragment/InformationBarFragment.java
@@ -0,0 +1,280 @@
+package com.kogasoftware.odt.invehicledevice.ui.fragment;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Stopwatch;
+import com.kogasoftware.odt.invehicledevice.R;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.EventDispatcher;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalData.Phase;
+import com.kogasoftware.odt.invehicledevice.ui.ViewDisabler;
+import com.kogasoftware.odt.invehicledevice.ui.fragment.InformationBarFragment.State;
+import com.kogasoftware.odt.webapi.model.OperationSchedule;
+import com.kogasoftware.odt.webapi.model.PassengerRecord;
+import com.kogasoftware.odt.webapi.model.Platform;
+
+public class InformationBarFragment extends ApplicationFragment<State>
+		implements EventDispatcher.OnUpdatePhaseListener,
+		EventDispatcher.OnChangeSignalStrengthListener {
+
+	@SuppressWarnings("serial")
+	static class State implements Serializable {
+		private final Optional<OperationSchedule> operationSchedule;
+		private final Phase phase;
+
+		public State(Phase phase, Optional<OperationSchedule> operationSchedule) {
+			this.phase = phase;
+			this.operationSchedule = operationSchedule;
+		}
+
+		public Phase getPhase() {
+			return phase;
+		}
+
+		public Optional<OperationSchedule> getOperationSchedule() {
+			return operationSchedule;
+		}
+	}
+
+	private static final int UPDATE_TIME_INTERVAL_MILLIS = 3000;
+	public static final int BATTERY_ALERT_BLINK_MILLIS = 500;
+	public static final int CHECK_BATTERY_MILLIS = 10 * 1000;
+	public static final int BATTERY_DISCONNECTED_LIMIT_MILLIS = 10 * 60 * 1000;
+	public static final String BATTERY_ALERT_DIALOG_FRAGMENT_TAG = "BatteryAlertDialogFragmentTag";
+
+	private ImageView networkStrengthImageView;
+	private TextView presentTimeTextView;
+	private Handler handler;
+
+	public static InformationBarFragment newInstance(Phase phase,
+			Optional<OperationSchedule> operationSchedule) {
+		return newInstance(new InformationBarFragment(), new State(phase,
+				operationSchedule));
+	}
+
+	private final Runnable updateTime = new Runnable() {
+		@Override
+		public void run() {
+			Date now = InVehicleDeviceService.getDate();
+			DateFormat f = new SimpleDateFormat(
+					getString(R.string.present_time_format));
+			presentTimeTextView.setText(f.format(now));
+			handler.postDelayed(this, UPDATE_TIME_INTERVAL_MILLIS);
+		}
+	};
+
+	/**
+	 * バッテリー警告ダイアログ
+	 */
+	public static class BatteryAlertDialogFragment extends DialogFragment {
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setIcon(android.R.drawable.ic_dialog_info);
+			builder.setMessage(Html
+					.fromHtml("<big><big>充電ケーブルを接続してください</big></big>"));
+			builder.setPositiveButton(
+					Html.fromHtml("<big><big>確認</big></big>"), null);
+			return builder.create();
+		}
+	}
+
+	/**
+	 * バッテリー状態を監視
+	 */
+	private final Runnable checkBattery = new Runnable() {
+		private final Stopwatch stopwatch = new Stopwatch();
+
+		@Override
+		public void run() {
+			handler.postDelayed(this, BATTERY_ALERT_BLINK_MILLIS);
+			View blinkView = getView().findViewById(
+					R.id.battery_alert_image_view);
+			// "http://developer.android.com/training/monitoring-device-state/battery-monitoring.html"
+			IntentFilter intentFilter = new IntentFilter(
+					Intent.ACTION_BATTERY_CHANGED);
+			Intent batteryStatus = getActivity().registerReceiver(null,
+					intentFilter);
+			// Are we charging / charged?
+			Integer status = batteryStatus.getIntExtra(
+					BatteryManager.EXTRA_STATUS, -1);
+			Boolean isCharging = status
+					.equals(BatteryManager.BATTERY_STATUS_CHARGING)
+					|| status.equals(BatteryManager.BATTERY_STATUS_FULL);
+			if (isCharging) {
+				stopwatch.reset().start();
+				if (blinkView.isShown()) {
+					blinkView.setVisibility(View.INVISIBLE);
+				}
+				return;
+			}
+			if (stopwatch.elapsedMillis() > BATTERY_DISCONNECTED_LIMIT_MILLIS
+					|| !stopwatch.isRunning()) {
+				stopwatch.reset().start();
+				if (getFragmentManager().findFragmentByTag(
+						BATTERY_ALERT_DIALOG_FRAGMENT_TAG) == null) {
+					BatteryAlertDialogFragment batteryAlertDialogFragment = new BatteryAlertDialogFragment();
+					batteryAlertDialogFragment.show(getFragmentManager(),
+							BATTERY_ALERT_DIALOG_FRAGMENT_TAG);
+				}
+			}
+			if (blinkView.isShown()) {
+				blinkView.setVisibility(View.INVISIBLE);
+			} else {
+				blinkView.setVisibility(View.VISIBLE);
+			}
+		}
+	};
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		View view = getView();
+		handler = new Handler();
+		presentTimeTextView = (TextView) view
+				.findViewById(R.id.present_time_text_view);
+		networkStrengthImageView = (ImageView) view
+				.findViewById(R.id.network_strength_image_view);
+		getService().getEventDispatcher().addOnUpdatePhaseListener(this);
+		updateView(view);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.information_bar_fragment, container,
+				false);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		handler.removeCallbacks(updateTime);
+		handler.removeCallbacks(checkBattery);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		handler.post(updateTime);
+		handler.post(checkBattery);
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		getService().getEventDispatcher().removeOnUpdatePhaseListener(this);
+	}
+
+	public void updateView(View view) {
+		View bcwl = view.findViewById(R.id.background_color_workaround_layout);
+		bcwl.setBackgroundColor(getPhaseColor(getState().getPhase()));
+
+		TextView phaseTextView = (TextView) view
+				.findViewById(R.id.phase_text_view);
+		Boolean showPlatformMemo = true;
+		switch (getState().getPhase()) {
+		case DRIVE:
+			phaseTextView.setText("運行中");
+			break;
+		case FINISH:
+			showPlatformMemo = false;
+			phaseTextView.setText("運行終了");
+			break;
+		case INITIAL:
+			showPlatformMemo = false;
+			phaseTextView.setText("");
+			break;
+		case PLATFORM_GET_OFF:
+			phaseTextView.setText("降車中");
+			break;
+		case PLATFORM_GET_ON:
+			phaseTextView.setText("乗車中");
+			break;
+		}
+
+		final Button platformMemoButton = (Button) view
+				.findViewById(R.id.platform_memo_button);
+		platformMemoButton.setVisibility(View.INVISIBLE);
+		if (showPlatformMemo) {
+			for (final OperationSchedule operationSchedule : getState()
+					.getOperationSchedule().asSet()) {
+				for (Platform platform : operationSchedule.getPlatform()
+						.asSet()) {
+					if (!platform.getMemo().isEmpty()) {
+						platformMemoButton.setVisibility(View.VISIBLE);
+						platformMemoButton
+								.setOnClickListener(new OnClickListener() {
+									@Override
+									public void onClick(View v) {
+										ViewDisabler.disable(v);
+										showPlatformMemoFragment(operationSchedule);
+									}
+								});
+					}
+				}
+			}
+		}
+	}
+
+	public void showPlatformMemoFragment(OperationSchedule operationSchedule) {
+		String tag = "tag:" + PlatformMemoFragment.class.getSimpleName();
+		Fragment old = getFragmentManager().findFragmentByTag(tag);
+		if (old != null) {
+			setCustomAnimation(getFragmentManager().beginTransaction()).remove(
+					old).commit();
+		}
+		setCustomAnimation(getFragmentManager().beginTransaction()).add(
+				R.id.modal_fragment_container,
+				PlatformMemoFragment.newInstance(operationSchedule)).commit();
+
+	}
+
+	@Override
+	public void onUpdatePhase(Phase phase,
+			List<OperationSchedule> operationSchedules,
+			List<PassengerRecord> passengerRecords) {
+		setState(new State(phase,
+				OperationSchedule
+						.getCurrentOperationSchedule(operationSchedules)));
+		updateView(getView());
+	}
+
+	@Override
+	public void onChangeSignalStrength(Integer signalStrengthPercentage) {
+		int imageResourceId = R.drawable.network_strength_4;
+		if (signalStrengthPercentage.intValue() == 0) {
+			imageResourceId = R.drawable.network_strength_0;
+		} else if (signalStrengthPercentage.intValue() <= 25) {
+			imageResourceId = R.drawable.network_strength_1;
+		} else if (signalStrengthPercentage.intValue() <= 50) {
+			imageResourceId = R.drawable.network_strength_2;
+		} else if (signalStrengthPercentage.intValue() <= 75) {
+			imageResourceId = R.drawable.network_strength_3;
+		}
+		networkStrengthImageView.setImageResource(imageResourceId);
+	}
+}
