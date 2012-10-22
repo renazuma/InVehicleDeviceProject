1fc24a7aa8a78585023809c2c1e7e0e453625d15
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-22 16:02:45 +0900

    delete unused import

--- tests/src/com/kogasoftware/odt/invehicledevice/test/unit/service/invehicledeviceservice/InVehicleDeviceServiceTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/test/unit/service/invehicledeviceservice/InVehicleDeviceServiceTestCase.java
@@ -1,35 +1,8 @@
 package com.kogasoftware.odt.invehicledevice.test.unit.service.invehicledeviceservice;
 
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.verify;
-
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
-
-import org.mockito.verification.VerificationWithTimeout;
-
-import android.location.GpsStatus;
-import android.location.Location;
-import android.os.HandlerThread;
 import android.test.ServiceTestCase;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.LocalData.VehicleNotificationStatus;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.OperationScheduleLogic;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.PassengerRecordLogic;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.ServiceUnitStatusLogLogic;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.VehicleNotificationLogic;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.VoiceServiceConnector;
-import com.kogasoftware.odt.webapi.model.OperationSchedule;
-import com.kogasoftware.odt.webapi.model.PassengerRecord;
-import com.kogasoftware.odt.webapi.model.VehicleNotification;
 
 public class InVehicleDeviceServiceTestCase extends
 		ServiceTestCase<InVehicleDeviceService> {
