f841a21ca0fa56a61544e6ca9231cbfbf5a874c3
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-25 14:17:13 +0900

    不要な内部データの削除

--- src/com/kogasoftware/odt/invehicledevice/service/invehicledeviceservice/LocalData.java
+++ src/com/kogasoftware/odt/invehicledevice/service/invehicledeviceservice/LocalData.java
@@ -4,18 +4,14 @@ import java.io.File;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.Semaphore;
 
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
-import com.kogasoftware.odt.invehicledevice.apiclient.model.InVehicleDevice;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.OperationSchedule;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.PassengerRecord;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.ServiceProvider;
-import com.kogasoftware.odt.invehicledevice.apiclient.model.ServiceUnit;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.ServiceUnitStatusLog;
-import com.kogasoftware.odt.invehicledevice.apiclient.model.UnitAssignment;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.VehicleNotification;
 import com.kogasoftware.odt.invehicledevice.empty.EmptyFile;
 
@@ -27,22 +23,17 @@ public class LocalData implements Serializable {
 		DRIVE, FINISH, PLATFORM_GET_ON, PLATFORM_GET_OFF,
 	}
 
-	private static final long serialVersionUID = 1797801788123L;
+	private static final long serialVersionUID = 1797801788124L;
 
-	@Deprecated
-	public final Semaphore operationScheduleInitializedSign = new Semaphore(0); // パーミットが0以上の場合は初期化済み。0以上になるまで待つためにacquireしたら必ずreleaseする。CountDownLatchがSerializableではないためこれを使用
-	@Deprecated
-	public final Semaphore serviceProviderInitializedSign = new Semaphore(0); // パーミットが0以上の場合は初期化済み。0以上になるまで待つためにacquireしたら必ずreleaseする。CountDownLatchがSerializableではないためこれを使用
+	public Boolean operationScheduleInitialized = false;
+	public Boolean serviceProviderInitialized = false;
 
 	public Date updatedDate = InVehicleDeviceService.getDate();
 	public File file = new EmptyFile();
 
 	public String token = "";
 	public String url = "";
-	public InVehicleDevice inVehicleDevice = new InVehicleDevice();
 	public ServiceProvider serviceProvider = new ServiceProvider();
-	public UnitAssignment unitAssignment = new UnitAssignment();
-	public ServiceUnit serviceUnit = new ServiceUnit();
 	public ServiceUnitStatusLog serviceUnitStatusLog = new ServiceUnitStatusLog();
 
 	public Boolean completeGetOff = false;
--- src/com/kogasoftware/odt/invehicledevice/ui/activity/SavePreferencesActivity.java
+++ src/com/kogasoftware/odt/invehicledevice/ui/activity/SavePreferencesActivity.java
@@ -1,10 +1,6 @@
 package com.kogasoftware.odt.invehicledevice.ui.activity;
 
-import java.io.InvalidClassException;
-import java.io.Serializable;
-
 import org.apache.commons.lang3.tuple.Pair;
-import org.json.JSONException;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,17 +8,16 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Strings;
-import com.kogasoftware.odt.invehicledevice.apiclient.model.InVehicleDevice;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.InVehicleDeviceService;
 import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.SharedPreferencesKeys;
-import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.backgroundtask.Broadcasts;
+import com.kogasoftware.odt.invehicledevice.service.invehicledeviceservice.broadcast.Broadcasts;
 
 public class SavePreferencesActivity extends Activity {
+	@SuppressWarnings("unused")
 	private static final String TAG = SavePreferencesActivity.class
 			.getSimpleName();
 
@@ -31,29 +26,6 @@ public class SavePreferencesActivity extends Activity {
 		SharedPreferences preferences = PreferenceManager
 				.getDefaultSharedPreferences(this);
 		Bundle bundle = Objects.firstNonNull(intent.getExtras(), new Bundle());
-		InVehicleDevice inVehicleDevice = new InVehicleDevice();
-
-		Serializable maybeInVehicleDevice = null;
-		try {
-			maybeInVehicleDevice = bundle
-					.getSerializable(SharedPreferencesKeys.IN_VEHICLE_DEVICE);
-		} catch (RuntimeException e) {
-			Log.e(TAG, e.toString(), e);
-			String message = "";
-			if (e.getCause() instanceof InvalidClassException) {
-				message = "エラーが発生しました。設定アプリケーションのバージョンと車載器アプリケーションのバージョンが適合しません。";
-			} else {
-				message = "不明なエラーが発生しました。デバイスのログを参照してください。";
-			}
-			return Pair.of(false, message);
-		}
-
-		if (maybeInVehicleDevice instanceof InVehicleDevice) {
-			inVehicleDevice = (InVehicleDevice) maybeInVehicleDevice;
-		} else {
-			String message = "!(bundle.getSerializable(SharedPreferencesKey.IN_VEHICLE_DEVICE) instanceof InVehicleDevice)";
-			Log.e(TAG, message);
-		}
 
 		SharedPreferences.Editor editor = preferences.edit();
 		editor.clear();
@@ -66,12 +38,6 @@ public class SavePreferencesActivity extends Activity {
 				SharedPreferencesKeys.SERVER_IN_VEHICLE_DEVICE_TOKEN,
 				Strings.nullToEmpty(bundle
 						.getString(SharedPreferencesKeys.SERVER_IN_VEHICLE_DEVICE_TOKEN)));
-		try {
-			editor.putString(SharedPreferencesKeys.IN_VEHICLE_DEVICE,
-					inVehicleDevice.toJSONObject().toString());
-		} catch (JSONException e) {
-			Log.e(TAG, "toJSONObject() failed", e);
-		}
 
 		editor.putInt(
 				SharedPreferencesKeys.LOCATION_RECEIVE_MIN_DISTANCE,
