304a09039bed348ea8fd8bf9b305411743520f15
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-10-26 11:40:14 +0900

    不要コード整理

--- src/com/kogasoftware/odt/invehicledevice/apiclient/model/ServiceProvider.java
+++ src/com/kogasoftware/odt/invehicledevice/apiclient/model/ServiceProvider.java
@@ -2,25 +2,23 @@ package com.kogasoftware.odt.invehicledevice.apiclient.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.kogasoftware.odt.invehicledevice.apiclient.model.base.Model;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.base.ServiceProviderBase;
 
 public class ServiceProvider extends ServiceProviderBase {
 	private static final long serialVersionUID = 8456722052053624083L;
 
-	@JsonProperty JsonNode reservationTimeLimit = Model.getObjectMapper().createObjectNode();
-	@JsonProperty JsonNode timeBufferRatio = Model.getObjectMapper().createObjectNode();
+	@JsonProperty JsonNode reservationTimeLimit = getObjectMapper().createObjectNode();
+	@JsonProperty JsonNode timeBufferRatio = getObjectMapper().createObjectNode();
 
 	public static class ReservationTimeLimit {
-		protected Integer operatorWeb = 0;
 		protected Integer androidApp = 0;
 
 		public Integer getAndroidApp() {
-			return Model.wrapNull(androidApp);
+			return wrapNull(androidApp);
 		}
 
 		public void setAndroidApp(Integer androidApp) {
-			this.androidApp = Model.wrapNull(androidApp);
+			this.androidApp = wrapNull(androidApp);
 		}
 	}
 }
