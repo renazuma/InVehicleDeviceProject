9db34841ba8a8aa545011f931d88209a82ab6141
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-11-21 17:56:04 +0900

    TypeOfUserの定数追加

--- src/com/kogasoftware/odt/invehicledevice/apiclient/model/User.java
+++ src/com/kogasoftware/odt/invehicledevice/apiclient/model/User.java
@@ -8,6 +8,12 @@ import com.kogasoftware.odt.invehicledevice.apiclient.model.base.UserBase;
 public class User extends UserBase {
 	private static final long serialVersionUID = 8982613020419131854L;
 
+	public static class TypeOfUser {
+		public static final Integer RESIDENT = 0; // 在住者
+		public static final Integer VISITOR = 1; // 県内
+		public static final Integer TRAVELER = 2;// 県外
+	}
+
 	public List<String> getNotes() {
 		List<String> notes = new LinkedList<String>();
 		if (getMemo().length() > 0) {
