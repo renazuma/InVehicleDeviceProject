79983dea8bd723d1f3919b47a72070b2fffb8415
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-11-21 19:15:25 +0900

    年齢計算追加

--- src/com/kogasoftware/odt/invehicledevice/apiclient/model/User.java
+++ src/com/kogasoftware/odt/invehicledevice/apiclient/model/User.java
@@ -1,5 +1,7 @@
 package com.kogasoftware.odt.invehicledevice.apiclient.model;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,6 +9,11 @@ import com.kogasoftware.odt.invehicledevice.apiclient.model.base.UserBase;
 
 public class User extends UserBase {
 	private static final long serialVersionUID = 8982613020419131854L;
+
+	public static class Sex {
+		public static final Integer MALE = 1; // 男性
+		public static final Integer FEMALE = 2; // 女性
+	}
 
 	public static class TypeOfUser {
 		public static final Integer RESIDENT = 0; // 在住者
@@ -29,5 +36,24 @@ public class User extends UserBase {
 			notes.add("※要介護");
 		}
 		return notes;
+	}
+
+	public Integer getAge(Date base) {
+		Calendar now = Calendar.getInstance();
+		now.setTime(base);
+		Calendar birthday = Calendar.getInstance();
+		birthday.setTime(getBirthday());
+		if (birthday.get(Calendar.MONTH) < now.get(Calendar.MONTH)
+				|| (birthday.get(Calendar.MONTH) == now.get(Calendar.MONTH) && birthday
+						.get(Calendar.DAY_OF_MONTH) <= now
+						.get(Calendar.DAY_OF_MONTH))) {
+			return now.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
+		} else {
+			return now.get(Calendar.YEAR) - birthday.get(Calendar.YEAR) - 1;
+		}
+	}
+
+	public Integer getAge() {
+		return getAge(new Date());
 	}
 }
--- tests/src/com/kogasoftware/odt/invehicledevice/apiclient/test/model/UserTestCase.java
+++ tests/src/com/kogasoftware/odt/invehicledevice/apiclient/test/model/UserTestCase.java
@@ -1,5 +1,8 @@
 package com.kogasoftware.odt.invehicledevice.apiclient.test.model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import com.kogasoftware.odt.invehicledevice.apiclient.model.User;
 
 import junit.framework.TestCase;
@@ -62,5 +65,28 @@ public class UserTestCase extends TestCase {
 		u.setNeededCare(true);
 		u.setMemo("Hello");
 		assertEquals(4, u.getNotes().size());
+	}
+	
+	public void testGetAge() throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		User u = new User();
+		
+		u.setBirthday(dateFormat.parse("1999-01-01"));
+		assertEquals(0, u.getAge(dateFormat.parse("1999-01-01")).intValue());
+		assertEquals(0, u.getAge(dateFormat.parse("1999-12-31")).intValue());
+		assertEquals(1, u.getAge(dateFormat.parse("2000-01-01")).intValue());
+		assertEquals(1, u.getAge(dateFormat.parse("2000-12-31")).intValue());
+		assertEquals(2, u.getAge(dateFormat.parse("2001-01-01")).intValue());
+		
+		u.setBirthday(dateFormat.parse("2099-01-01"));
+		assertEquals(0, u.getAge(dateFormat.parse("2099-01-01")).intValue());
+		assertEquals(0, u.getAge(dateFormat.parse("2099-12-31")).intValue());
+		assertEquals(1, u.getAge(dateFormat.parse("2100-01-01")).intValue());
+		assertEquals(1, u.getAge(dateFormat.parse("2100-12-31")).intValue());
+		assertEquals(2, u.getAge(dateFormat.parse("2101-01-01")).intValue());
+		
+		u.setBirthday(dateFormat.parse("2000-02-29"));
+		assertEquals(8, u.getAge(dateFormat.parse("2009-02-28")).intValue());
+		assertEquals(9, u.getAge(dateFormat.parse("2009-03-01")).intValue());
 	}
 }
