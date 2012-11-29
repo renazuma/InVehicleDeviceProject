7d8ca2a87e0e1e07127d892f17776e2419d60acd
Isamu Mogi <isamu_mogi@kogasoftware.com>
2012-11-29 12:52:08 +0900

    テスト用モデル出力設定追加

--- start_servers.sh
+++ start_servers.sh
@@ -23,8 +23,15 @@ cp ../database.yml config/database.yml
 bundle install
 bundle exec rake db:migrate:reset
 #bundle exec rake db:migrate
+bundle exec rake "java:model:create[true]"
 bundle exec rails server --daemon --environment=development --port=3334
 cd ..
+
+cp -fr InVehicleDeviceApiClient/tests/* tests
+d=src/com/kogasoftware/odt/invehicledevice/apiclient/model/base
+cp -fr InVehicleDeviceApiClient/$d/* $d
+d=src/com/kogasoftware/odt/invehicledevice/apiclient/model
+yes n | cp -ip InVehicleDeviceApiClient/$d/* $d/ 2> /dev/null
 
 cd InVehicleDeviceTestApiServer
 cp ../database.yml config/database.yml
