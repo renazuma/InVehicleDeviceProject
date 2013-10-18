ede41c6ad3407de0026cb1c1b282e90b536ffc8f
Isamu Mogi <isamu_mogi@kogasoftware.com>
2013-10-18 14:39:08 +0900

    Androidのバージョンによってテスト結果が変わるのを修正

--- InVehicleDevice/src/instrumentTest/java/com/kogasoftware/odt/invehicledevice/service/startupservice/AirplaneModeSettingTestCase.java
+++ InVehicleDevice/src/instrumentTest/java/com/kogasoftware/odt/invehicledevice/service/startupservice/AirplaneModeSettingTestCase.java
@@ -5,16 +5,29 @@ import java.io.IOException;
 import com.kogasoftware.odt.invehicledevice.service.startupservice.AirplaneModeSetting;
 
 import android.content.Context;
+import android.os.Build;
 import android.test.AndroidTestCase;
 
 public class AirplaneModeSettingTestCase extends AndroidTestCase {
 	public void test() throws IOException {
 		Context c = getContext();
-		AirplaneModeSetting.set(c, false);
-		assertFalse(AirplaneModeSetting.get(c));
-		AirplaneModeSetting.set(c, true);
-		assertTrue(AirplaneModeSetting.get(c));
-		AirplaneModeSetting.set(c, false);
-		assertFalse(AirplaneModeSetting.get(c));
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			// 正しく設定変更が反映される
+			AirplaneModeSetting.set(c, false);
+			assertFalse(AirplaneModeSetting.get(c));
+			AirplaneModeSetting.set(c, true);
+			assertTrue(AirplaneModeSetting.get(c));
+			AirplaneModeSetting.set(c, false);
+			assertFalse(AirplaneModeSetting.get(c));
+		} else {
+			// 設定変更がある場合IOException
+			Boolean enable = AirplaneModeSetting.get(c);
+			AirplaneModeSetting.set(c, enable);
+			try {
+				AirplaneModeSetting.set(c, !enable);
+				fail();
+			} catch (IOException e) {
+			}
+		}
 	}
 }
