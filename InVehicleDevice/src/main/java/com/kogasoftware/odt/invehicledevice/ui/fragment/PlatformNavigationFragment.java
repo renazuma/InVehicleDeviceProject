32ec6118966f4d5166278333d3968fc69e07ea08
Isamu Mogi <saturday6c@gmail.com>
2013-10-22 14:08:55 +0900

    google maps のlanguageとregionをリソースで指定した値に書き換えるようにした

--- InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/ui/fragment/PlatformNavigationFragment.java
+++ InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/ui/fragment/PlatformNavigationFragment.java
@@ -1,11 +1,18 @@
 package com.kogasoftware.odt.invehicledevice.ui.fragment;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +20,10 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Optional;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Closer;
 import com.kogasoftware.odt.invehicledevice.R;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.OperationSchedule;
 import com.kogasoftware.odt.invehicledevice.apiclient.model.Platform;
@@ -22,6 +32,9 @@ import com.kogasoftware.odt.invehicledevice.ui.fragment.PlatformNavigationFragme
 
 public class PlatformNavigationFragment extends ApplicationFragment<State>
 		implements OnChangeLocationListener {
+	protected static final String TAG = PlatformNavigationFragment.class
+			.getSimpleName();
+
 	@SuppressWarnings("serial")
 	protected static class State implements Serializable {
 		private final OperationSchedule operationSchedule;
@@ -62,6 +75,31 @@ public class PlatformNavigationFragment extends ApplicationFragment<State>
 				R.id.platform_navigation_close_button);
 	}
 
+	public String getGoogleMapsHtml() throws IOException {
+		Charset charset = Charsets.UTF_8;
+		StringBuilder parameter = new StringBuilder();
+		String language = getString(R.string.google_maps_language);
+		if (!language.isEmpty()) {
+			parameter.append("&amp;language=" + language);
+		}
+		String region = getString(R.string.google_maps_region);
+		if (!region.isEmpty()) {
+			parameter.append("&amp;region=" + region);
+		}
+		Closer closer = Closer.create();
+		try {
+			InputStream inputStream = closer.register(getActivity().getAssets()
+					.open("google_maps.html"));
+			String html = new String(ByteStreams.toByteArray(inputStream),
+					charset);
+			return html.replace("{GOOGLE_MAPS_URL_PARAMETERS}", parameter);
+		} catch (Throwable e) {
+			throw closer.rethrow(e);
+		} finally {
+			closer.close();
+		}
+	}
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
@@ -73,23 +111,44 @@ public class PlatformNavigationFragment extends ApplicationFragment<State>
 			titleTextView.setText(platform.getName());
 			final WebView webView = (WebView) getView().findViewById(
 					R.id.platform_navigation_web_view);
-			final String url = "file:///android_asset/google_maps.html";
+			final AtomicInteger once = new AtomicInteger(1);
+			final String baseUrl = "file:///android_asset/";
 			webView.setWebViewClient(new WebViewClient() {
 				@Override
 				public void onPageFinished(WebView view, String finishedUrl) {
 					super.onPageFinished(view, finishedUrl);
-					if (url.equals(finishedUrl)) { // これは本当にこれで良いのか?
-						webView.loadUrl(String.format(
-								"javascript:initialize(%f, %f, %f, %f)",
-								platform.getLatitude().doubleValue(), platform
-										.getLongitude().doubleValue(),
-								getState().getLatitude(), getState()
-										.getLongitude()));
+					if (!finishedUrl.startsWith(baseUrl)
+							|| !once.compareAndSet(1, 0)) {
+						return;
 					}
+					String script = String.format(Locale.US,
+							"javascript:initialize(%f, %f, %f, %f)", platform
+									.getLatitude().doubleValue(), platform
+									.getLongitude().doubleValue(), getState()
+									.getLatitude(), getState().getLongitude());
+					webView.loadUrl(script);
 				}
 			});
 			webView.getSettings().setJavaScriptEnabled(true);
-			webView.loadUrl(url);
+
+			new AsyncTask<Void, Void, String>() {
+				@Override
+				protected String doInBackground(Void... args) {
+					try {
+						return getGoogleMapsHtml();
+					} catch (IOException e) {
+						Log.w(TAG, e);
+						cancel(false);
+						return null;
+					}
+				}
+
+				@Override
+				protected void onPostExecute(String html) {
+					webView.loadDataWithBaseURL(baseUrl, html, "text/html",
+							"UTF-8", null);
+				}
+			}.execute();
 		}
 	}
 
