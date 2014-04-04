575c85234b8e984c29f2e992f59cd2e8a5d67133
Isamu Mogi <isamu_mogi@kogasoftware.com>
2014-04-04 10:24:33 +0900

    Use GoogleInstrumentationTestRunner

--- InVehicleDevice/src/androidTest/java/com/kogasoftware/odt/invehicledevice/testutil/DexMakerInstrumentationTestRunner.java
+++ InVehicleDevice/src/androidTest/java/com/kogasoftware/odt/invehicledevice/testutil/DexMakerInstrumentationTestRunner.java
@@ -1,10 +1,11 @@
 package com.kogasoftware.odt.invehicledevice.testutil;
 
+import com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner;
+
 import android.os.Bundle;
-import android.test.InstrumentationTestRunner;
 
 public class DexMakerInstrumentationTestRunner extends
-		InstrumentationTestRunner {
+		GoogleInstrumentationTestRunner {
 	@Override
 	public void onCreate(Bundle arguments) {
 		super.onCreate(arguments);
