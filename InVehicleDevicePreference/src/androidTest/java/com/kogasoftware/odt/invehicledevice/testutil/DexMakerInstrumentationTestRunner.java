95c26db217d89082d38b4c0cae2b4e2e31f80b1d
Isamu Mogi <isamu_mogi@kogasoftware.com>
2014-04-04 15:41:31 +0900

    設定アプリのInstrumentationTestRunnerもGoogleInstrumentationTestRunnerベースにした

---
+++ InVehicleDevicePreference/src/androidTest/java/com/kogasoftware/odt/invehicledevice/testutil/DexMakerInstrumentationTestRunner.java
@@ -0,0 +1,18 @@
+package com.kogasoftware.odt.invehicledevice.testutil;
+
+import com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner;
+
+import android.os.Bundle;
+
+/**
+ * "https://code.google.com/p/dexmaker/issues/detail?id=2"
+ */
+public class DexMakerInstrumentationTestRunner extends
+		GoogleInstrumentationTestRunner {
+	@Override
+	public void onCreate(Bundle arguments) {
+		super.onCreate(arguments);
+		System.setProperty("dexmaker.dexcache", getTargetContext()
+				.getCacheDir().toString());
+	}
+}
