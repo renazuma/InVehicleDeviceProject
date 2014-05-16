40b852cca883dc600e06e71b8d2f4763ab9107f0
Isamu Mogi <isamu_mogi@kogasoftware.com>
2014-05-16 10:31:56 +0900

    マージ済みOperationScheduleのスケルトン

---
+++ InVehicleDeviceApiClient/src/main/java/com/kogasoftware/odt/invehicledevice/apiclient/model/OperationSchedule.java
@@ -0,0 +1,7 @@
+package com.kogasoftware.odt.invehicledevice.apiclient.model;
+
+import java.io.Serializable;
+
+public class OperationSchedule implements Serializable {
+	private static final long serialVersionUID = 113833565898783155L;
+}
