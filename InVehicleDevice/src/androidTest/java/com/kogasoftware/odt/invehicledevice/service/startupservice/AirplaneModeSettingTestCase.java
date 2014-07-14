c8090ced79361f02d1d52114e4bb153c9ed07ed5
Isamu Mogi <isamu_mogi@kogasoftware.com>
2014-07-14 18:07:59 +0900

    wifi経由でのデバッグが中断してしまうため、古いAndroidでの試験を削除

--- InVehicleDevice/src/androidTest/java/com/kogasoftware/odt/invehicledevice/service/startupservice/AirplaneModeSettingTestCase.java
+++ InVehicleDevice/src/androidTest/java/com/kogasoftware/odt/invehicledevice/service/startupservice/AirplaneModeSettingTestCase.java
@@ -9,23 +9,16 @@ import android.test.AndroidTestCase;
 public class AirplaneModeSettingTestCase extends AndroidTestCase {
 	public void test() throws IOException {
 		Context c = getContext();
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-			// 正しく設定変更が反映される
-			AirplaneModeSetting.set(c, false);
-			assertFalse(AirplaneModeSetting.get(c));
-			AirplaneModeSetting.set(c, true);
-			assertTrue(AirplaneModeSetting.get(c));
-			AirplaneModeSetting.set(c, false);
-			assertFalse(AirplaneModeSetting.get(c));
-		} else {
-			// 設定変更がある場合IOException
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
 			Boolean enable = AirplaneModeSetting.get(c);
-			AirplaneModeSetting.set(c, enable);
 			try {
+				// 設定を変更しようとするとIOException
 				AirplaneModeSetting.set(c, !enable);
 				fail();
 			} catch (IOException e) {
 			}
+			// 設定変更が発生しない場合は例外は発生しない
+			AirplaneModeSetting.set(c, enable);
 		}
 	}
 }
