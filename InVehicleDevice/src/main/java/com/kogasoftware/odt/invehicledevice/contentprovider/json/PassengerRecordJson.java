e7291cf1f0f408f223948ec4884dcb69f0e89815
Isamu Mogi <isamu_mogi@kogasoftware.com>
2014-07-18 16:07:37 +0900

    乗降時刻がローカルに反映されていなかったのを修正

--- InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/contentprovider/json/PassengerRecordJson.java
+++ InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/contentprovider/json/PassengerRecordJson.java
@@ -5,6 +5,7 @@ import org.joda.time.DateTime;
 import android.content.ContentValues;
 
 import com.kogasoftware.odt.invehicledevice.contentprovider.table.PassengerRecords;
+import com.kogasoftware.odt.invehicledevice.utils.ContentValuesUtils;
 
 public class PassengerRecordJson {
 	public Long id;
@@ -18,6 +19,10 @@ public class PassengerRecordJson {
 		values.put(PassengerRecords.Columns._ID, id);
 		values.put(PassengerRecords.Columns.RESERVATION_ID, reservationId);
 		values.put(PassengerRecords.Columns.USER_ID, userId);
+		ContentValuesUtils.putDateTime(values,
+				PassengerRecords.Columns.GET_ON_TIME, getOnTime);
+		ContentValuesUtils.putDateTime(values,
+				PassengerRecords.Columns.GET_OFF_TIME, getOffTime);
 		return values;
 	}
 }
