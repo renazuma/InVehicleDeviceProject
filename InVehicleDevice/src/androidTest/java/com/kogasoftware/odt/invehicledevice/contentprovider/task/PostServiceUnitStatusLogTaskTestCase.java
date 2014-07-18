1b1a2cfeedee2ba4e4574284df4f17de6aee995d
Isamu Mogi <isamu_mogi@kogasoftware.com>
2014-07-18 16:29:33 +0900

    時刻制御の整理

--- InVehicleDevice/src/androidTest/java/com/kogasoftware/odt/invehicledevice/contentprovider/task/PostServiceUnitStatusLogTaskTestCase.java
+++ InVehicleDevice/src/androidTest/java/com/kogasoftware/odt/invehicledevice/contentprovider/task/PostServiceUnitStatusLogTaskTestCase.java
@@ -6,8 +6,6 @@ import org.joda.time.DateTimeUtils;
 import android.content.ContentValues;
 
 import com.kogasoftware.odt.invehicledevice.contentprovider.table.ServiceUnitStatusLogs;
-import com.kogasoftware.odt.invehicledevice.contentprovider.task.InsertServiceUnitStatusLogTask;
-import com.kogasoftware.odt.invehicledevice.contentprovider.task.PostServiceUnitStatusLogTask;
 
 public class PostServiceUnitStatusLogTaskTestCase
 		extends
@@ -19,24 +17,26 @@ public class PostServiceUnitStatusLogTaskTestCase
 	}
 
 	public void testRun() throws Exception {
-		DateTimeUtils.setCurrentMillisFixed(12345678);
-		DateTime now = DateTime.now();
+		Long now = 12345678L;
+		DateTimeUtils.setCurrentMillisFixed(now);
+
+		Long t1 = now - InsertServiceUnitStatusLogTask.INTERVAL_MILLIS / 2;
+		Long t2 = now - (InsertServiceUnitStatusLogTask.INTERVAL_MILLIS + 100);
+		Long t3 = now;
 
 		ContentValues values1 = new ContentValues();
 		values1.put(ServiceUnitStatusLogs.Columns._ID, 1);
-		values1.put(ServiceUnitStatusLogs.Columns.CREATED_AT, now.getMillis()
-				- InsertServiceUnitStatusLogTask.INTERVAL_MILLIS / 2);
+		values1.put(ServiceUnitStatusLogs.Columns.CREATED_AT, t1);
 		database.insertOrThrow(ServiceUnitStatusLogs.TABLE_NAME, null, values1);
 
 		ContentValues values2 = new ContentValues();
 		values2.put(ServiceUnitStatusLogs.Columns._ID, 2);
-		values2.put(ServiceUnitStatusLogs.Columns.CREATED_AT, now.getMillis()
-				- InsertServiceUnitStatusLogTask.INTERVAL_MILLIS - 100);
+		values2.put(ServiceUnitStatusLogs.Columns.CREATED_AT, t2);
 		database.insertOrThrow(ServiceUnitStatusLogs.TABLE_NAME, null, values2);
 
 		ContentValues values3 = new ContentValues();
 		values3.put(ServiceUnitStatusLogs.Columns._ID, 3);
-		values3.put(ServiceUnitStatusLogs.Columns.CREATED_AT, now.getMillis());
+		values3.put(ServiceUnitStatusLogs.Columns.CREATED_AT, t3);
 		database.insertOrThrow(ServiceUnitStatusLogs.TABLE_NAME, null, values3);
 
 		Runnable task = new PostServiceUnitStatusLogTask(mContext, database,
