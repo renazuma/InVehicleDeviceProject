3f62a5f0ed7cb1b2567374b50674a6f7b12a465f
k_matsuura <k_matsuura@kogasoftware.com>
2018-12-25 12:19:37 +0900

    choisoko公開用パッケージ名変更

--- misc/choisoko/bin/choisoko_converter.sh
+++ misc/choisoko/bin/choisoko_converter.sh
@@ -6,17 +6,17 @@ privacy_policy_url="http://www.aisin.co.jp/privacy/"
 app_name="車載器"
 
 function convert_strings () {
-  find ./InVehicleDevice -name \*.java -o -name \*.xml -o -name \*.txt -o -name build.gradle| xargs sed -i s/kogasoftware/choisoko/g
+  find ./InVehicleDevice -name \*.java -o -name \*.xml -o -name \*.txt -o -name build.gradle| xargs sed -i s/kogasoftware/choisoko2/g
   sed -i -r "s#(<string name=\"privacy_policy_url\">).*(</string>)#\1${privacy_policy_url}\2#g" ./InVehicleDevice/src/main/res/values-ja/strings.xml
   sed -i -r "s#(<string name=\"app_name\">).*(</string>)#\1${app_name}\2#g" ./InVehicleDevice/src/main/res/values-ja/strings.xml
 }
 
 function rename_dirs {
   if [ -e ./InvehicleDevice/src/androidTest/java/com/kogasoftware ]; then
-    mv ./InVehicleDevice/src/androidTest/java/com/{kogasoftware,choisoko}
+    mv ./InVehicleDevice/src/androidTest/java/com/{kogasoftware,choisoko2}
   fi
   if [ -e ./InvehicleDevice/src/main/java/com/kogasoftware ]; then
-    mv ./InVehicleDevice/src/main/java/com/{kogasoftware,choisoko}
+    mv ./InVehicleDevice/src/main/java/com/{kogasoftware,choisoko2}
   fi
 }
 
