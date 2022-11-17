8c0e5c1af14411745fe3a2c57281158e5bc2afba
k-matsuura <k_matsuura@kogasoftware.com>
2022-11-18 02:18:04 +0900

    地図画面の画像URLを、住宅地図APIから取得する様にする
    緯度経度の取得は後で実装するため固定値を使う

---
+++ InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/infra/api/MapApi.kt
@@ -0,0 +1,78 @@
+package com.kogasoftware.odt.invehicledevice.infra.api
+
+import android.util.Log
+import kotlin.Throws
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import java.io.IOException
+
+class MapApi {
+    var USER_ID = "G1W9OKqx"
+    var PASSWORD = "7dQhnH6O"
+    var SERVICE_ID = "50000001"
+    var DEVICE_FLAG = "1"
+
+    @Throws(InterruptedException::class, IOException::class)
+    fun imageUrl(width: Int, height: Int, zoom: Int, center: String): String {
+
+        val (aid: String, kid: String, zisLmtinf) = getAuthInfo()
+
+        val url =  "https://test-api.zip-site.com/api/zips/general/map?width=$width&height=$height&center=$center&zoom=$zoom&zis_authtype=aid&zis_lmtinf=$zisLmtinf&zis_zips_authkey=$kid&zis_aid=$aid"
+        Log.i(TAG, "url: $url")
+        return url
+    }
+
+    private fun getAuthInfo(): Triple<String, String, String> {
+
+        val mapApi = mapApiInterface()
+
+        // HACK: 同期処理をするとAndroidのメイン処理自体にその間待ちが入る（多分）ので、出来ればAPIの実行は非同期にしたい
+        val authInfo = mapApi.login(USER_ID, PASSWORD, SERVICE_ID, DEVICE_FLAG).execute()
+        val aid: String = authInfo.body()!!.result["aid"].toString()
+        val kid: String = authInfo.body()!!.result["kid"].toString()
+        Log.i(TAG, "aid: $aid")
+        Log.i(TAG, "kid: $kid")
+
+        val items: Map<*, *> = authInfo.body()!!.result["items"] as Map<*, *>
+        val funcList = items["func"] as List<Map<*, *>>
+        val mapFunc = funcList.filter { func ->
+            func["id"] == "0007" && func["subid"] == "0001"
+        }.first()
+
+        val areaCode = mapFunc["areaCode"]
+        val funcInfo = mapFunc["funcInfo"]
+
+        val zisLmtinf = "$areaCode,$funcInfo"
+
+        Log.i(TAG, "areaCode: $areaCode")
+        Log.i(TAG, "funcInfo: $funcInfo")
+        Log.i(TAG, "zisLmtinf: $zisLmtinf")
+
+        logout(mapApi, aid)
+
+        return Triple(aid, kid, zisLmtinf)
+    }
+
+    private fun logout(mapApi: MapApiInterface, aid: String) {
+        val logout = mapApi.logout(aid)
+        val logoutInfo = logout.execute()
+        Log.i(TAG, "logout: " + logoutInfo.body()!!.status["text"])
+    }
+
+    private fun mapApiInterface(): MapApiInterface {
+        val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://test-api.zip-site.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        return retrofit.create(MapApiInterface::class.java)
+    }
+
+    companion object {
+        val TAG = MapApi::class.java.simpleName
+    }
+}
