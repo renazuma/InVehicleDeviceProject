c6e6ca022cfcab771d22f04704b0d9cadedc492d
k-matsuura <k_matsuura@kogasoftware.com>
2022-11-22 18:24:27 +0900

    リファクタリング：クラス名を意味が取りやすい様に変更
    呼び出されるのはアプリ全体のメイン画面と言うわけではなく、たまたまサインイン後の最初の画面に割り振られているだけなので、それが分かる名前に変更

--- InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/presenter/MainUiPresenter.java
+++ InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/presenter/MainUiPresenter.java
@@ -1,7 +1,7 @@
 package com.kogasoftware.odt.invehicledevice.presenter;
 
 import com.kogasoftware.odt.invehicledevice.infra.loader.DefaultChargeLoader;
-import com.kogasoftware.odt.invehicledevice.infra.loader.MainViewLoader;
+import com.kogasoftware.odt.invehicledevice.infra.loader.TopViewLoader;
 import com.kogasoftware.odt.invehicledevice.infra.loader.onCreateSignInLoader;
 import com.kogasoftware.odt.invehicledevice.presenter.mainui.OnCreateDataSync;
 import com.kogasoftware.odt.invehicledevice.view.activity.InVehicleDeviceActivity;
@@ -13,13 +13,13 @@ import com.kogasoftware.odt.invehicledevice.view.activity.InVehicleDeviceActivit
 public class MainUiPresenter {
 
     private final onCreateSignInLoader onCreateSignInLoader;
-    private final MainViewLoader mainViewLoader;
+    private final TopViewLoader topViewLoader;
     private final DefaultChargeLoader defaultChargeLoader;
     private final OnCreateDataSync onCreateDataSync;
 
     public MainUiPresenter(InVehicleDeviceActivity inVehicleDeviceActivity) {
         this.onCreateSignInLoader = new onCreateSignInLoader(inVehicleDeviceActivity);
-        this.mainViewLoader = new MainViewLoader(inVehicleDeviceActivity);
+        this.topViewLoader = new TopViewLoader(inVehicleDeviceActivity);
         this.defaultChargeLoader = new DefaultChargeLoader(inVehicleDeviceActivity);
         this.onCreateDataSync = new OnCreateDataSync(inVehicleDeviceActivity);
     }
@@ -32,13 +32,13 @@ public class MainUiPresenter {
         //   * SignIn情報をDBに登録後、それらの情報を元にSPを取得。SPのDBへの登録を契機に、mainViewLoaderが画面表示。
         // * アプリ起動時にDB内にSPがある場合、そのままメイン画面を表示（この場合既に車載器データはある前提）
         onCreateSignInLoader.initLoader();
-        mainViewLoader.initLoader();
+        topViewLoader.initLoader();
         defaultChargeLoader.initLoader();
     }
 
     public void onDestroy() {
         onCreateSignInLoader.destroyLoader();
-        mainViewLoader.destroyLoader();
+        topViewLoader.destroyLoader();
         defaultChargeLoader.destroyLoader();
     }
 }
