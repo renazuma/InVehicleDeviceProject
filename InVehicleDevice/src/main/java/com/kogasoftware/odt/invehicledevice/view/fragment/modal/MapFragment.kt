b9e074083fee949eed34fbda212a9f798c5426a7
k-matsuura <k_matsuura@kogasoftware.com>
2023-01-10 14:19:07 +0900

    地図画像をURLで取得出来る様に修正

--- InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/view/fragment/modal/MapFragment.kt
+++ InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/view/fragment/modal/MapFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.*
 import android.widget.*
 import com.kogasoftware.odt.invehicledevice.R
+import com.bumptech.glide.Glide
+import java.io.IOException
 
 /**
  * 地図表示画面
@@ -13,6 +15,16 @@ import com.kogasoftware.odt.invehicledevice.R
 class MapFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val imageView = view!!.findViewById<ImageView>(R.id.map_image)
+
+        try {
+            var mapUrl: String = "https://www.kogasoftware.com/wordpress/wp-content/themes/kogasoftware/images/cmn/logo.png"
+            Glide.with(view!!).load(mapUrl).into(imageView)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
 
         view!!.findViewById<Button>(R.id.quit_map_button).setOnClickListener { v: View? ->
             hideModal(this@MapFragment)
