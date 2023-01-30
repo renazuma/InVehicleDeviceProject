73d5011fec4dcf6ca675e46f0a636b2d5f05969c
k-matsuura <k_matsuura@kogasoftware.com>
2023-01-30 19:34:52 +0900

    住宅地図のログイン/ログアウト処理を、コルーチンを利用して非同期にする
    処理の高速化のためと、Androidがメインスレッドでの通信処理を拒否する様になった事に対応する別スレッド化のため

--- InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/infra/api/MapApi.kt
+++ InVehicleDevice/src/main/java/com/kogasoftware/odt/invehicledevice/infra/api/MapApi.kt
@@ -1,12 +1,15 @@
 package com.kogasoftware.odt.invehicledevice.infra.api
 
 import android.util.Log
-import kotlin.Throws
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import com.squareup.moshi.Moshi
-import org.json.JSONObject
 import java.io.IOException
 
 class MapApi(val userId: String, val password: String, val serviceId: String) {
@@ -73,40 +76,42 @@ class MapApi(val userId: String, val password: String, val serviceId: String) {
     }
 
     private fun getAuthInfo(): Triple<String, String, String> {
-
         val mapApi = mapApiInterface()
 
-        // HACK: 同期処理をするとAndroidのメイン処理自体にその間待ちが入る（多分）ので、出来ればAPIの実行は非同期にしたい
-        val authInfo = mapApi.login(userId, password, serviceId, DEVICE_FLAG).execute()
-        val aid: String = authInfo.body()!!.result["aid"].toString()
-        val kid: String = authInfo.body()!!.result["kid"].toString()
+        val loginInfo = runBlocking {
+            withContext(CoroutineScope(Dispatchers.Default).coroutineContext) {
+                mapApi.login(userId, password, serviceId, DEVICE_FLAG).result
+            }
+        }
+
+        val aid: String = loginInfo!!["aid"].toString()
         Log.i(TAG, "aid: $aid")
-        Log.i(TAG, "kid: $kid")
 
-        val items: Map<*, *> = authInfo.body()!!.result["items"] as Map<*, *>
-        val funcList = items["func"] as List<Map<*, *>>
-        val mapFunc = funcList.filter { func ->
-            func["id"] == "0007" && func["subid"] == "0001"
-        }.first()
+        runBlocking {
+            withContext(CoroutineScope(Dispatchers.Default).coroutineContext) {
+                mapApi.logout(aid).status
+            }
+        }
 
-        val areaCode = mapFunc["areaCode"]
-        val funcInfo = mapFunc["funcInfo"]
+          val kid: String = loginInfo!!["kid"].toString()
+          Log.i(TAG, "kid: $kid")
 
-        val zisLmtinf = "$areaCode,$funcInfo"
+          val items: Map<*, *> = loginInfo!!["items"] as Map<*, *>
+          val funcList = items["func"] as List<Map<*, *>>
+          val mapFunc = funcList.filter { func ->
+              func["id"] == "0007" && func["subid"] == "0001"
+          }.first()
 
-        Log.i(TAG, "areaCode: $areaCode")
-        Log.i(TAG, "funcInfo: $funcInfo")
-        Log.i(TAG, "zisLmtinf: $zisLmtinf")
+          val areaCode = mapFunc["areaCode"]
+          val funcInfo = mapFunc["funcInfo"]
 
-        logout(mapApi, aid)
+          val zisLmtinf = "$areaCode,$funcInfo"
+
+          Log.i(TAG, "areaCode: $areaCode")
+          Log.i(TAG, "funcInfo: $funcInfo")
+          Log.i(TAG, "zisLmtinf: $zisLmtinf")
 
         return Triple(aid, kid, zisLmtinf)
-    }
-
-    private fun logout(mapApi: MapApiInterface, aid: String) {
-        val logout = mapApi.logout(aid)
-        val logoutInfo = logout.execute()
-        Log.i(TAG, "logout: " + logoutInfo.body()!!.status["text"])
     }
 
     private fun mapApiInterface(): MapApiInterface {
